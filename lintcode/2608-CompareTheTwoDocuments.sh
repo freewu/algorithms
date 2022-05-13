d9c270e79fae543bdaea953573f320fa6daf09d6
bluefrog <bluefrog.wu@gmail.com>
2022-05-13 14:50:44 +0800

    lintcode 2608 · Compare the two documents

---
+++ lintcode/2608-CompareTheTwoDocuments.sh
@@ -0,0 +1,12 @@
+# 2608 · Compare the two documents
+# Description
+# To compare whether two specified files are identical using cmp, follow these three steps as required.
+
+# use the less command to view the contents of file file1
+less file1
+
+# use the less command to view the contents of file file2
+less file2
+
+# use the cmp command to compare the contents of file1 and file2 to see if they are the same
+cmp file1 file2
