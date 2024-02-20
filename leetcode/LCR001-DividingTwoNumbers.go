e81fd8fc67c69c247dd5096da839f5163c0ccf53
bluefrog <bluefrog.wu@gmail.com>
2024-02-20 17:34:31 +0800

    leetcode LCR 001. 两数相除

--- leetcode/LCR001-DividingTwoNumbers.go
+++ leetcode/LCR001-DividingTwoNumbers.go
@@ -29,9 +29,57 @@ package main
 // 		b != 0
 
 import "fmt"
+import "math"
 
 func divide(a int, b int) int {
-	return a / b
+	if a == math.MinInt32 { // 考虑被除数为最小值的情况
+        if b == 1 {
+            return math.MinInt32
+        }
+        if b == -1 {
+            return math.MaxInt32
+        }
+    }
+    if b == math.MinInt32 { // 考虑除数为最小值的情况
+        if a == math.MinInt32 {
+            return 1
+        }
+        return 0
+    }
+    if a == 0 { // 考虑被除数为 0 的情况
+        return 0
+    }
+	// 求它们的除法的商 a/b ，要求不得使用乘号 '*'、除号 '/' 以及求余符号 '%' 
+	// return a / b
+
+	// 一般情况，使用二分查找
+    // 将所有的正数取相反数，这样就只需要考虑一种情况
+    rev := false
+    if a > 0 {
+        a = -a
+        rev = !rev
+    }
+    if b > 0 {
+        b = -b
+        rev = !rev
+    }
+
+    candidates := []int{b}
+    for y := b; y >= a-y; { // 注意溢出
+        y += y
+        candidates = append(candidates, y)
+    }
+    ans := 0
+    for i := len(candidates) - 1; i >= 0; i-- {
+        if candidates[i] >= a {
+            ans |= 1 << i
+            a -= candidates[i]
+        }
+    }
+    if rev {
+        return -ans
+    }
+    return ans
 }
 
 func main() {
@@ -39,4 +87,6 @@ func main() {
 	fmt.Println(divide(7,-3)) // -2
 	fmt.Println(divide(0,1)) // 0
 	fmt.Println(divide(1,1)) // 1
+	fmt.Println(divide(-2147483648,-1)) // 2147483647
+	
 }
