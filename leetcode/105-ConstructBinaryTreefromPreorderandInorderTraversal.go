67c473259978f382968a16a222535c99712bc6e6
bluefrog <bluefrog.wu@gmail.com>
2024-02-20 10:13:28 +0800

    leetcode 105. Construct Binary Tree from Preorder and Inorder Traversal

--- leetcode/105-ConstructBinaryTreefromPreorderandInorderTraversal.go
+++ leetcode/105-ConstructBinaryTreefromPreorderandInorderTraversal.go
@@ -1,38 +1,35 @@
 package main
 
+// 105. Construct Binary Tree from Preorder and Inorder Traversal
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Constraints:
+// 		1 <= preorder.length <= 3000
+// 		inorder.length == preorder.length
+// 		-3000 <= preorder[i], inorder[i] <= 3000
+// 		preorder and inorder consist of unique values.
+// 		Each value of inorder also appears in preorder.
+// 		preorder is guaranteed to be the preorder traversal of the tree.
+// 		inorder is guaranteed to be the inorder traversal of the tree.
+
+// Example 1:
+
+// 	Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// 	Output: [3,9,20,null,null,15,7]
+
+// Example 2:
+
+// 	Input: preorder = [-1], inorder = [-1]
+// 	Output: [-1]
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+
+   
 import "fmt"
-
-/**
-105. Construct Binary Tree from Preorder and Inorder Traversal
-Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
-
-Constraints:
-
-	1 <= preorder.length <= 3000
-	inorder.length == preorder.length
-	-3000 <= preorder[i], inorder[i] <= 3000
-	preorder and inorder consist of unique values.
-	Each value of inorder also appears in preorder.
-	preorder is guaranteed to be the preorder traversal of the tree.
-	inorder is guaranteed to be the inorder traversal of the tree.
-
-Example 1:
-
-	Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
-	Output: [3,9,20,null,null,15,7]
-
-Example 2:
-
-	Input: preorder = [-1], inorder = [-1]
-	Output: [-1]
-
-    3
-   / \
-  9  20
-    /  \
-   15   7
-
- */
 
 type TreeNode struct {
 	Val int
@@ -49,7 +46,7 @@ type TreeNode struct {
  * }
  */
 
-// 解法一, 直接传入需要的 slice 范围作为输入, 可以避免申请对应 inorder 索引的内存
+// 直接传入需要的 slice 范围作为输入, 可以避免申请对应 inorder 索引的内存
 func buildTree(preorder []int, inorder []int) *TreeNode {
 	if len(preorder) == 0 {
 		return nil
@@ -64,7 +61,7 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	return root
 }
 
-// 解法二
+// DFS
 func buildTree1(preorder []int, inorder []int) *TreeNode {
 	inPos := make(map[int]int)
 	for i := 0; i < len(inorder); i++ {
@@ -85,7 +82,30 @@ func buildPreIn2TreeDFS(pre []int, preStart int, preEnd int, inStart int, inPos 
 	return root
 }
 
+// best solution
+func buildTree2(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	if len(preorder) == 1 && len(inorder) == 1 && preorder[0] == inorder[0] {
+		return &TreeNode{Val: preorder[0]}
+	} else {
+		node := &TreeNode{Val: preorder[0]}
+		var index int
+		for index = 0; index < len(inorder); index++ {
+			if inorder[index] == node.Val {
+				break
+			}
+		}
+		node.Left = buildTree2(preorder[1:index+1], inorder[0:index])
+		node.Right = buildTree2(preorder[index+1:], inorder[index+1:])
+		return node
+	}
+}
+
+
 func main() {
 	fmt.Printf("%v\n",buildTree([]int{3,9,20,15,7},[]int{9,3,15,20,7}))
 	fmt.Printf("%v\n",buildTree1([]int{3,9,20,15,7},[]int{9,3,15,20,7}))
+	fmt.Printf("%v\n",buildTree2([]int{3,9,20,15,7},[]int{9,3,15,20,7}))
 }
