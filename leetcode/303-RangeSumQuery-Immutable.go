a3e00299ecdb59985cd609a2cf6bc860163a7eec
bluefrog <bluefrog.wu@gmail.com>
2024-03-18 09:03:09 +0800

    leetcode 303. Range Sum Query - Immutable

--- leetcode/303-RangeSumQuery-Immutable.go
+++ leetcode/303-RangeSumQuery-Immutable.go
@@ -1,52 +1,42 @@
 package main
 
-import "fmt"
+// 303. Range Sum Query - Immutable
+// Given an integer array nums, handle multiple queries of the following type:
+// 	Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
 
-/**
-303. Range Sum Query - Immutable
-Given an integer array nums, handle multiple queries of the following type:
+// Implement the NumArray class:
+// 	NumArray(int[] nums) Initializes the object with the integer array nums.
+// 	int sumRange(int left, int right) Returns the sum of the elements of nums between indices left
+// 	and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
 
-	Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Example 1:
+// 	Input
+// 		["NumArray", "sumRange", "sumRange", "sumRange"]
+// 		[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// 	Output
+// 		[null, 1, -1, -3]
+// 	Explanation
+// 		NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// 		numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// 		numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// 		numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 
-Implement the NumArray class:
+// Constraints:
+// 	1 <= nums.length <= 10^4
+// 	-10^5 <= nums[i] <= 10^5
+// 	0 <= left <= right < nums.length
+// 	At most 104 calls will be made to sumRange.
+	
 
-	NumArray(int[] nums) Initializes the object with the integer array nums.
-
-	int sumRange(int left, int right) Returns the sum of the elements of nums between indices left
-	and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
-
-
-Example 1:
-
-	Input
-		["NumArray", "sumRange", "sumRange", "sumRange"]
-		[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
-
-	Output
-		[null, 1, -1, -3]
-
-	Explanation
-		NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
-		numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
-		numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
-		numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
-
-
-Constraints:
-
-	1 <= nums.length <= 10^4
-	-10^5 <= nums[i] <= 10^5
-	0 <= left <= right < nums.length
-	At most 104 calls will be made to sumRange.
-
-给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点
-*/
+// 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点
 
 /**
  * Your NumArray object will be instantiated and called as such:
  * obj := Constructor(nums);
  * param_1 := obj.SumRange(left,right);
  */
+
+import "fmt"
 
 // NumArray define
 type NumArray struct {
