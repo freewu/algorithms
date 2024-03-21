76d955df3852fdce4edf3b226264b3937fee78d8
bluefrog <bluefrog.wu@gmail.com>
2024-03-21 08:57:44 +0800

    leetcode 206. Reverse Linked List

--- leetcode/206-ReverseLinkedList.go
+++ leetcode/206-ReverseLinkedList.go
@@ -1,25 +1,70 @@
 package main
 
+// 206. Reverse Linked List
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+// <img src="https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg" />
+// Input: head = [1,2,3,4,5] 1->2->3->4->5->NULL
+// Output: [5,4,3,2,1] 5->4->3->2->1->NULL
+
+// Example 2:
+// <img src="https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg" />
+// Input: head = [1,2]  1->2->NULL
+// Output: [2,1] 2->1->NULL
+
+// Example 3:
+// Input: head = []
+// Output: []
+ 
+// Constraints:
+//     The number of nodes in the list is the range [0, 5000].
+//     -5000 <= Node.val <= 5000
+
+// Example:
+// Input: 
+// Output: 
+
+// 1->2->3->4->5
+
 import "fmt"
-
-/**
-206. Reverse Linked List
-
-Reverse a singly linked list.
-
-Example:
-Input: 1->2->3->4->5->NULL
-Output: 5->4->3->2->1->NULL
-
-
-1->2->3->4->5
-
-*/
 
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+// 打印链表
+func printListNode(l *ListNode) {
+    if nil == l {
+        return
+    }
+    for {
+        if nil == l.Next {
+            fmt.Print(l.Val)
+            break
+        } else {
+            fmt.Print(l.Val, " -> ")
+        }
+        l = l.Next
+    }
+    fmt.Println()
+}
+
+// 数组创建链表
+func makeListNode(arr []int) *ListNode {
+    if (len(arr) == 0) {
+        return nil
+    }
+    var l = (len(arr) - 1)
+    var head = &ListNode{arr[l], nil}
+    for i := l - 1; i >= 0; i--  {
+        var n = &ListNode{arr[i], head}
+        head = n
+    }
+    return head
+}
+
 
 /**
  * Definition for singly-linked list.
@@ -38,7 +83,7 @@ func reverseList(head *ListNode) *ListNode {
 	    head.Next = new_head // 更新  head.Next
 	    new_head = head      // 移动 new_head
 	    head = next
-	    fmt.Println("new_head: ",new_head,"head: ",head)
+	    //fmt.Println("new_head: ",new_head,"head: ",head)
     }
 	return new_head
 }
@@ -55,34 +100,44 @@ func reverseList1(head *ListNode) *ListNode {
         prev = cur
         cur = next
     }
-
     return prev
 }
 
-func printListNode(a *ListNode) {
-	head := a
-	for {
-		fmt.Println(head.Val)
-		head = head.Next
-		if nil == head {
-			break
-		}
-	}
-}
-
 func main() {
-	var a, b, c, d, e ListNode
-	a.Val = 1
-	b.Val = 2
-	c.Val = 3
-	d.Val = 4
-	e.Val = 5
-	a.Next = &b
-	b.Next = &c
-	c.Next = &d
-	d.Next = &e
-	e.Next = nil
+    l1 := makeListNode([]int{1,2,3,4,5})
+    fmt.Println("l1: ")
+	printListNode(l1)
+    fmt.Println("reverseList(l1): ")
+	printListNode(reverseList(l1))
 
-	printListNode(&a)
-	printListNode(reverseList(&a))
+    l2 := makeListNode([]int{1,2})
+    fmt.Println("l2: ")
+	printListNode(l2)
+    fmt.Println("reverseList(l2): ")
+	printListNode(reverseList(l2))
+
+    l3 := makeListNode([]int{})
+    fmt.Println("l3: ")
+	printListNode(l3)
+    fmt.Println("reverseList(l3): ")
+	printListNode(reverseList(l3))
+
+
+    l1 = makeListNode([]int{1,2,3,4,5})
+    fmt.Println("l1: ")
+	printListNode(l1)
+    fmt.Println("reverseList1(l1): ")
+	printListNode(reverseList1(l1))
+
+    l2 = makeListNode([]int{1,2})
+    fmt.Println("l2: ")
+	printListNode(l2)
+    fmt.Println("reverseList1(l2): ")
+	printListNode(reverseList1(l2))
+
+    l3 = makeListNode([]int{})
+    fmt.Println("l3: ")
+	printListNode(l3)
+    fmt.Println("reverseList1(l3): ")
+	printListNode(reverseList1(l3))
 }
