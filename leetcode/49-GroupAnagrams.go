b3a139055a0a1f286bb9a9339f910f067acadd0f
bluefrog <bluefrog.wu@gmail.com>
2024-03-22 11:53:55 +0800

    leetcode 49. Group Anagrams

--- leetcode/49-GroupAnagrams.go
+++ leetcode/49-GroupAnagrams.go
@@ -1,78 +1,72 @@
 package main
 
-/**
-49. Group Anagrams
-Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+// 49. Group Anagrams
+// Given an array of strings strs, group the anagrams together. 
+// You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
-Constraints:
+// Constraints:
+//     1 <= strs.length <= 10^4
+//     0 <= strs[i].length <= 100
+//     strs[i] consists of lowercase English letters.
 
-	1 <= strs.length <= 10^4
-	0 <= strs[i].length <= 100
-	strs[i] consists of lowercase English letters.
+// Example 1:
+//     Input: strs = ["eat","tea","tan","ate","nat","bat"]
+//     Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-Example 1:
+// Example 2:
+//     Input: strs = [""]
+//     Output: [[""]]
 
-	Input: strs = ["eat","tea","tan","ate","nat","bat"]
-	Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 3:
+//     Input: strs = ["a"]
+//     Output: [["a"]]
 
-Example 2:
+// 给出一个字符串数组，要求对字符串数组里面有 Anagrams 关系的字符串进行分组。
+// Anagrams 关系是指两个字符串的字符完全相同，顺序不同，两者是由排列组合组成。
 
-	Input: strs = [""]
-	Output: [[""]]
+// 将每个字符串都排序，排序完成以后，
+// 相同 Anagrams 的字符串必然排序结果一样。
+// 把排序以后的字符串当做 key 存入到 map 中。
+// 遍历数组以后，就能得到一个 map，key 是排序以后的字符串，value 对应的是这个排序字符串以后的 Anagrams 字符串集合。
+// 最后再将这些 value 对应的字符串数组输出即可。
 
-Example 3:
 
-	Input: strs = ["a"]
-	Output: [["a"]]
-
-给出一个字符串数组，要求对字符串数组里面有 Anagrams 关系的字符串进行分组。
-Anagrams 关系是指两个字符串的字符完全相同，顺序不同，两者是由排列组合组成。
-
-将每个字符串都排序，排序完成以后，
-相同 Anagrams 的字符串必然排序结果一样。
-把排序以后的字符串当做 key 存入到 map 中。
-遍历数组以后，就能得到一个 map，key 是排序以后的字符串，value 对应的是这个排序字符串以后的 Anagrams 字符串集合。
-最后再将这些 value 对应的字符串数组输出即可。
- */
-
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
+import "sort"
 
 type sortRunes []rune
 
 func (s sortRunes) Less(i, j int) bool {
-	return s[i] < s[j]
+    return s[i] < s[j]
 }
 
 func (s sortRunes) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
+    s[i], s[j] = s[j], s[i]
 }
 
 func (s sortRunes) Len() int {
-	return len(s)
+    return len(s)
 }
 
 func groupAnagrams(strs []string) [][]string {
-	var res [][]string
-	record := map[string][]string{} // key 是排序以后的字符串，value 对应的是这个排序字符串以后的 Anagrams 字符串集合
-	for _, str := range strs {
-		sByte := []rune(str)
-		sort.Sort(sortRunes(sByte)) // 按字符排序
+    var res [][]string
+    record := map[string][]string{} // key 是排序以后的字符串，value 对应的是这个排序字符串以后的 Anagrams 字符串集合
+    for _, str := range strs {
+        sByte := []rune(str)
+        sort.Sort(sortRunes(sByte)) // 按字符排序
 
-		sstrs := record[string(sByte)]
-		sstrs = append(sstrs, str)
-		record[string(sByte)] = sstrs
-	}
-	for _, v := range record {
-		res = append(res, v)
-	}
-	return res
+        sstrs := record[string(sByte)]
+        sstrs = append(sstrs, str)
+        record[string(sByte)] = sstrs
+    }
+    for _, v := range record {
+        res = append(res, v)
+    }
+    return res
 }
 
-// 这边把字符串生成  hash 值
+// 这边把字符串生成 hash 值
 func groupAnagrams1(strs []string) [][]string {
 	m := make(map[int][]string)
 	for _, s := range strs {
@@ -92,21 +86,20 @@ var primeNumbers = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
 const bigPrime = 276906403
 
 func hash(s string) int {
-	fmt.Printf("s = %v\n",s)
-	result := 1
-	for _, c := range s {
-		result = (result * primeNumbers[c - 'a']) % bigPrime
-		fmt.Printf("primeNumbers[c - 'a'] = %v\n",primeNumbers[c - 'a'])
-		fmt.Printf("result :%v\n",result)
-	}
-	return result
+    //fmt.Printf("s = %v\n",s)
+    result := 1
+    for _, c := range s {
+        result = (result * primeNumbers[c - 'a']) % bigPrime
+        // fmt.Printf("primeNumbers[c - 'a'] = %v\n",primeNumbers[c - 'a'])
+        // fmt.Printf("result :%v\n",result)
+    }
+    return result
 }
 
 //  best solution 
 func groupAnagrams2(strs []string) [][]string {
-    // method 1: HashMap 对strs 利的元素在bytes 排序后 就相同了，作为key, values 是个原始切片
-    // method 2: HashMap 用计数作为key, 将计数转化为 [26]int 数组，代表 strs 中每个元素 有几个26字母
-    // METHOD2:
+    // HashMap 对strs 利的元素在bytes 排序后 就相同了，作为key, values 是个原始切片
+    // HashMap 用计数作为key, 将计数转化为 [26]int 数组，代表 strs 中每个元素 有几个26字母
     hmap := make(map[[26]int][]string)
 
     for _, str := range strs {
@@ -119,8 +112,8 @@ func groupAnagrams2(strs []string) [][]string {
         }
         hmap[key] = append(hmap[key], str)
 
-		fmt.Println("key: ",key)
-		fmt.Println("hmap[key]: ",hmap[key])
+        // fmt.Println("key: ",key)
+        // fmt.Println("hmap[key]: ",hmap[key])
     }
     result := make([][]string, 0, len(hmap))
     for _, v := range hmap {
