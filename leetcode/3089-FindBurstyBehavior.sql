b2c9c6dfcecf50a8383fad6a5f461ee4cec065a3
bluefrog <bluefrog.wu@gmail.com>
2024-03-27 17:26:46 +0800

    leetcode 3089. Find Bursty Behavior

--- leetcode/3089-FindBurstyBehavior.sql
+++ leetcode/3089-FindBurstyBehavior.sql
@@ -171,3 +171,71 @@
 --             user_id
 --     )
 -- ) AS t
+
+
+WITH 
+P AS ( -- 用户任何 连续 7 天 的时段发帐数
+    SELECT 
+        p1.user_id AS user_id, 
+        COUNT(1) AS cnt
+    FROM
+        Posts AS p1
+    JOIN 
+        Posts AS p2
+    ON 
+        p1.user_id = p2.user_id AND 
+        -- 任何 连续 7 天 的时段
+        p2.post_date BETWEEN p1.post_date AND DATE_ADD(p1.post_date, INTERVAL 6 DAY)
+    GROUP BY 
+        p1.user_id, p1.post_date
+),
+T AS ( -- 2月平均每周发贴数
+    SELECT 
+        user_id, 
+        COUNT(1) / 4 AS avg_weekly_posts
+    FROM 
+        Posts
+    WHERE 
+        post_date BETWEEN '2024-02-01' AND '2024-02-28'
+    GROUP BY 
+        user_id
+)
+
+SELECT 
+    user_id, 
+    MAX(cnt) AS max_7day_posts, 
+    avg_weekly_posts
+FROM
+    P
+JOIN T USING (user_id)
+GROUP BY 
+    user_id
+HAVING 
+    max_7day_posts >= avg_weekly_posts * 2 -- 任何 连续 7 天 的时段中发帖频率是其 平均 每周发帖频率的 至少两倍。
+ORDER BY 
+    user_id;
+
+# Write your MySQL query statement below
+-- WITH
+--     P AS (
+--         SELECT p1.user_id AS user_id, COUNT(1) AS cnt
+--         FROM
+--             Posts AS p1
+--             JOIN Posts AS p2
+--                 ON p1.user_id = p2.user_id
+--                 AND p2.post_date BETWEEN p1.post_date AND DATE_ADD(p1.post_date, INTERVAL 6 DAY)
+--         GROUP BY p1.user_id, p1.post_id
+--     ),
+--     T AS (
+--         SELECT user_id, COUNT(1) / 4 AS avg_weekly_posts
+--         FROM Posts
+--         WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
+--         GROUP BY user_id
+--     )
+-- SELECT user_id, MAX(cnt) AS max_7day_posts, avg_weekly_posts
+-- FROM
+--     P
+--     JOIN T USING (user_id)
+-- GROUP BY user_id
+-- HAVING max_7day_posts >= avg_weekly_posts * 2
+-- ORDER BY user_id;
