c4aba9ad90a36a99eae79e0ce666af74a0bd2f23
bluefrog <bluefrog.wu@gmail.com>
2024-04-07 01:32:37 +0800

    leetcode 162. Find Peak Element

--- leetcode/410-SplitArrayLargestSum.go
+++ leetcode/410-SplitArrayLargestSum.go
@@ -18,49 +18,48 @@ package main
 // The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
  
 // Constraints:
-//         1 <= nums.length <= 1000
-//         0 <= nums[i] <= 10^6
-//         1 <= k <= min(50, nums.length)
+//     1 <= nums.length <= 1000
+//     0 <= nums[i] <= 10^6
+//     1 <= k <= min(50, nums.length)
 
 import "fmt"
 
 func splitArray(nums []int, m int) int {
-	maxNum, sum := 0, 0
-	for _, num := range nums {
-		sum += num
-		if num > maxNum {
-			maxNum = num
-		}
-	}
-	if m == 1 {
-		return sum
-	}
-	low, high := maxNum, sum
-	for low < high {
-		mid := low + (high-low)>>1
-		if calSum(mid, m, nums) {
-			high = mid
-		} else {
-			low = mid + 1
-		}
-	}
-	return low
-}
-
-func calSum(mid, m int, nums []int) bool {
-	sum, count := 0, 0
-	for _, v := range nums {
-		sum += v
-		if sum > mid {
-			sum = v
-			count++
-			// 分成 m 块，只需要插桩 m -1 个
-			if count > m - 1 {
-				return false
-			}
-		}
-	}
-	return true
+    maxNum, sum := 0, 0
+    for _, num := range nums {
+        sum += num
+        if num > maxNum {
+            maxNum = num
+        }
+    }
+    if m == 1 {
+        return sum
+    }
+    calSum := func (mid, m int, nums []int) bool {
+        sum, count := 0, 0
+        for _, v := range nums {
+            sum += v
+            if sum > mid {
+                sum = v
+                count++
+                // 分成 m 块，只需要插桩 m -1 个
+                if count > m - 1 {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    low, high := maxNum, sum
+    for low < high {
+        mid := low + (high-low)>>1
+        if calSum(mid, m, nums) {
+            high = mid
+        } else {
+            low = mid + 1
+        }
+    }
+    return low
 }
 
 func main() {
