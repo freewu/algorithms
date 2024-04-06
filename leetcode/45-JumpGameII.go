e347207f3d067edf1bb7e9db89cd6f494c8a8433
bluefrog <bluefrog.wu@gmail.com>
2024-04-07 01:22:46 +0800

    leetcode 45. Jump Game II

--- leetcode/45-JumpGameII.go
+++ leetcode/45-JumpGameII.go
@@ -1,89 +1,89 @@
 package main
 
+// 45. Jump Game II
+// You are given a 0-indexed array of integers nums of length n.
+// You are initially positioned at nums[0].
+// Each element nums[i] represents the maximum length of a forward jump from index i. 
+// In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+//     0 <= j <= nums[i] and
+//     i + j < n
+
+// Return the minimum number of jumps to reach nums[n - 1]. 
+// The test cases are generated such that you can reach nums[n - 1].
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+// Example 2:
+// Input: nums = [2,3,0,1,4]
+// Output: 2
+ 
+// Constraints:
+//     1 <= nums.length <= 10^4
+//     0 <= nums[i] <= 1000
+//     It's guaranteed that you can reach nums[n - 1].
+
+// 解题思路:
+//     给定一个非负整数数组，你最初位于数组的第一个位置。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+//     你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+
 import "fmt"
 
-/**
-45. Jump Game II
-Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
-Each element in the array represents your maximum jump length at that position.
-Your goal is to reach the last index in the minimum number of jumps.
-You can assume that you can always reach the last index.
-
-Constraints:
-
-	1 <= nums.length <= 1000
-	0 <= nums[i] <= 10^5
-
-Example 1:
-
-	Input: nums = [2,3,1,1,4]
-	Output: 2
-	Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
-
-Example 2:
-
-	Input: nums = [2,3,0,1,4]
-	Output: 2
-
-解题思路:
-	给定一个非负整数数组，你最初位于数组的第一个位置。数组中的每个元素代表你在该位置可以跳跃的最大长度。
-	你的目标是使用最少的跳跃次数到达数组的最后一个位置。
-
- */
 func jump(nums []int) int {
-	if len(nums) == 1 {
-		return 0
-	}
-	needChoose, canReach, step := 0, 0, 0
-	for i, x := range nums {
-		if i+x > canReach {
-			canReach = i + x
-			if canReach >= len(nums)-1 {
-				return step + 1
-			}
-		}
-		if i == needChoose {
-			needChoose = canReach
-			step++
-		}
-	}
-	return step
+    if len(nums) == 1 {
+        return 0
+    }
+    needChoose, canReach, step := 0, 0, 0
+    for i, x := range nums {
+        if i + x > canReach {
+            canReach = i + x
+            if canReach >= len(nums) - 1 {
+                return step + 1
+            }
+        }
+        if i == needChoose {
+            needChoose = canReach
+            step++
+        }
+    }
+    return step
 }
 
 // best solution
-func jumpBest(nums []int) int {
-	l := len(nums)
-	c := 0
-
-	if l == 1 {
-		return c
-	}
-	for i := 0; i < l; {
-		c++
-		m, mj := 0, 0
-		for j := i + 1; j < i + 1 + nums[i] && j < l; j++ {
-			val := j - i + nums[j]
-			if m <= val {
-				m, mj = val, j
-			}
-			if j == l - 1 {
-				return c
-			}
-		}
-		i = mj
-		if mj >= l - 1 {
-			break
-		} else if mj + nums[mj] >= l - 1{
-			c++
-			break
-		}
-	}
-	return c
+func jump1(nums []int) int {
+    l, res := len(nums), 0
+    if l == 1 {
+        return res
+    }
+    for i := 0; i < l; {
+        res++
+        m, mj := 0, 0
+        for j := i + 1; j < i + 1 + nums[i] && j < l; j++ {
+            val := j - i + nums[j]
+            if m <= val {
+                m, mj = val, j
+            }
+            if j == l - 1 {
+                return res
+            }
+        }
+        i = mj
+        if mj >= l - 1 {
+            break
+        } else if mj + nums[mj] >= l - 1{
+            res++
+            break
+        }
+    }
+    return res
 }
 
 func main() {
-	fmt.Printf("jump([]int{2,3,1,1,4}) = %v\n",jump([]int{2,3,1,1,4}))
-	fmt.Printf("jump([]int{2,3,0,1,4}) = %v\n",jump([]int{2,3,0,1,4}))
-	fmt.Printf("jumpBest([]int{2,3,1,1,4}) = %v\n",jumpBest([]int{2,3,1,1,4}))
-	fmt.Printf("jumpBest([]int{2,3,0,1,4}) = %v\n",jumpBest([]int{2,3,0,1,4}))
+    // Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+	fmt.Printf("jump([]int{2,3,1,1,4}) = %v\n",jump([]int{2,3,1,1,4})) // 2 
+	fmt.Printf("jump([]int{2,3,0,1,4}) = %v\n",jump([]int{2,3,0,1,4})) // 2 
+	fmt.Printf("jump1([]int{2,3,1,1,4}) = %v\n",jump1([]int{2,3,1,1,4})) // 2 
+	fmt.Printf("jump1([]int{2,3,0,1,4}) = %v\n",jump1([]int{2,3,0,1,4})) // 2 
 }
--- leetcode/55-JumpGame.go
+++ leetcode/55-JumpGame.go
@@ -1,84 +1,73 @@
 package main
 
+// 55. Jump Game
+// You are given an integer array nums. 
+// You are initially positioned at the array's first index, 
+// and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+// Example 2:
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: 
+//     You will always arrive at index 3 no matter what. 
+//     Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+// Constraints:
+//     1 <= nums.length <= 10^4
+//     0 <= nums[i] <= 10^5
+
+// 解题思路:
+// 	给出一个非负数组，要求判断从数组 0 下标开始，能否到达数组最后一个位置。
+//     如果某一个作为 起跳点 的格子可以跳跃的距离是 n，那么表示后面 n 个格子都可以作为 起跳点。
+//     可以对每一个能作为 起跳点 的格子都尝试跳一次，把 能跳到最远的距离maxJump 不断更新。
+//     如果可以一直跳到最后，就成功了。如果中间有一个点比 maxJump 还要大，说明在这个点和 maxJump 中间连不上了，有些点不能到达最后一个位置。
+
 import "fmt"
 
-/**
-55. Jump Game
-Given an array of non-negative integers, you are initially positioned at the first index of the array.
-Each element in the array represents your maximum jump length at that position.
-Determine if you are able to reach the last index.
-
-Constraints:
-
-	1 <= nums.length <= 10000
-	0 <= nums[i] <= 100000
-
-Example 1:
-
-	Input: [2,3,1,1,4]
-	Output: true
-	Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-
-Example 2:
-
-	Input: [3,2,1,0,4]
-	Output: false
-	Explanation: You will always arrive at index 3 no matter what. Its maximum
-				 jump length is 0, which makes it impossible to reach the last index.
-
-解题思路:
-	给出一个非负数组，要求判断从数组 0 下标开始，能否到达数组最后一个位置。
-    如果某一个作为 起跳点 的格子可以跳跃的距离是 n，那么表示后面 n 个格子都可以作为 起跳点。
-    可以对每一个能作为 起跳点 的格子都尝试跳一次，把 能跳到最远的距离maxJump 不断更新。
-    如果可以一直跳到最后，就成功了。如果中间有一个点比 maxJump 还要大，说明在这个点和 maxJump 中间连不上了，有些点不能到达最后一个位置。
- */
-
 func canJump(nums []int) bool {
-	n := len(nums)
-	if n == 0 {
-		return false
-	}
-	if n == 1 {
-		return true
-	}
-	maxJump := 0
-	for i, v := range nums {
-		if i > maxJump { // 从 0 开始，所以第一次不会进入   如果可跳的最大值 < 当前位置  说明跳不到本位置 退出 返回 false
-			return false
-		}
-		fmt.Printf("i = %v \n",i)
-		fmt.Printf("v = %v \n",v)
-		fmt.Printf("i + v = %v \n",i +v)
-		fmt.Printf("maxJump = %v \n",maxJump)
-		maxJump = max(maxJump, i+v) // i + v =   当前点 + 在当前点可以跳的最大值
-	}
-	return true
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
+    if len(nums) == 0 {
+        return false
+    }
+    if len(nums) == 1 {
+        return true
+    }
+    res := 0
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    for i, v := range nums {
+        // 从 0 开始，所以第一次不会进入   如果可跳的最大值 < 当前位置  说明跳不到本位置 退出 返回 false
+        if i > res { 
+            return false
+        }
+        res = max(res, i + v) // i + v =   当前点 + 在当前点可以跳的最大值
+    }
+    return true
 }
 
 // best solution
-func canJumpBest(nums []int) bool {
-	steps := 0 // 所需步数
-	// 从最后一个位置倒推
-	for i := len(nums) - 2; i >= 0; i-- { // len(nums) - 2 最后一个位置
-		steps++
-		if nums[i] >= steps { // 因为是 倒推 当前位置可跳步数  >= 需要的步数,说明到了这个位置 肯定能到最后 把 当前所需步数据 设置为0
-			steps = 0
-		}
-	}
-	return steps == 0
+func canJump1(nums []int) bool {
+    steps := 0 // 所需步数
+    // 从最后一个位置倒推
+    for i := len(nums) - 2; i >= 0; i-- { // len(nums) - 2 最后一个位置
+        steps++
+        // 因为是 倒推 当前位置可跳步数  >= 需要的步数,说明到了这个位置 肯定能到最后 把 当前所需步数据 设置为 0
+        if nums[i] >= steps { 
+            steps = 0
+        }
+    }
+    return steps == 0
 }
 
 func main() {
-	fmt.Printf("canJump([]int{2,3,1,1,4} = %v\n",canJump([]int{2,3,1,1,4})) // true
-	fmt.Printf("canJump([]int{3,2,1,0,4} = %v\n",canJump([]int{3,2,1,0,4})) // false
+    fmt.Printf("canJump([]int{2,3,1,1,4} = %v\n",canJump([]int{2,3,1,1,4})) // true
+    fmt.Printf("canJump([]int{3,2,1,0,4} = %v\n",canJump([]int{3,2,1,0,4})) // false
 
-	fmt.Printf("canJumpBest([]int{2,3,1,1,4} = %v\n",canJumpBest([]int{2,3,1,1,4})) // true
-	fmt.Printf("canJumpBest([]int{3,2,1,0,4} = %v\n",canJumpBest([]int{3,2,1,0,4})) // false
+    fmt.Printf("canJump1([]int{2,3,1,1,4} = %v\n",canJump1([]int{2,3,1,1,4})) // true
+    fmt.Printf("canJump1([]int{3,2,1,0,4} = %v\n",canJump1([]int{3,2,1,0,4})) // false
 }
