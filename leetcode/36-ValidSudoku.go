1e074083a558434b671d6413981d4c7ff1ea7da1
bluefrog <bluefrog.wu@gmail.com>
2024-04-22 01:15:06 +0800

    leetcode 36. Valid Sudoku

--- leetcode/36-ValidSudoku.go
+++ leetcode/36-ValidSudoku.go
@@ -1,168 +1,163 @@
 package main
 
-/**
-36. Valid Sudoku
+// 36. Valid Sudoku
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+//     Each row must contain the digits 1-9 without repetition.
+//     Each column must contain the digits 1-9 without repetition.
+//     Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 
-Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
-	Each row must contain the digits 1-9 without repetition.
-	Each column must contain the digits 1-9 without repetition.
-	Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+//     A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+//     Only the filled cells need to be validated according to the mentioned rules.
 
-A partially filled sudoku which is valid.
-The Sudoku board could be partially filled, where empty cells are filled with the character '.'
+// Example 1:
+// Input:
+// [
+//     ["5","3",".",".","7",".",".",".","."],
+//     ["6",".",".","1","9","5",".",".","."],
+//     [".","9","8",".",".",".",".","6","."],
+//     ["8",".",".",".","6",".",".",".","3"],
+//     ["4",".",".","8",".","3",".",".","1"],
+//     ["7",".",".",".","2",".",".",".","6"],
+//     [".","6",".",".",".",".","2","8","."],
+//     [".",".",".","4","1","9",".",".","5"],
+//     [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: true
 
-Example 1:
+// Example 2:
+// Input:
+// [
+//     ["8","3",".",".","7",".",".",".","."],
+//     ["6",".",".","1","9","5",".",".","."],
+//     [".","9","8",".",".",".",".","6","."],
+//     ["8",".",".",".","6",".",".",".","3"],
+//     ["4",".",".","8",".","3",".",".","1"],
+//     ["7",".",".",".","2",".",".",".","6"],
+//     [".","6",".",".",".",".","2","8","."],
+//     [".",".",".","4","1","9",".",".","5"],
+//     [".",".",".",".","8",".",".","7","9"]
+// ]
+// Output: false
+// Explanation: Same as Example 1, except with the 5 in the top left corner being
+// modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
-	Input:
-	[
-	  ["5","3",".",".","7",".",".",".","."],
-	  ["6",".",".","1","9","5",".",".","."],
-	  [".","9","8",".",".",".",".","6","."],
-	  ["8",".",".",".","6",".",".",".","3"],
-	  ["4",".",".","8",".","3",".",".","1"],
-	  ["7",".",".",".","2",".",".",".","6"],
-	  [".","6",".",".",".",".","2","8","."],
-	  [".",".",".","4","1","9",".",".","5"],
-	  [".",".",".",".","8",".",".","7","9"]
-	]
-	Output: true
+// Constraints:
+//     board.length == 9
+//     board[i].length == 9
+//     board[i][j] is a digit 1-9 or '.'.
 
-Example 2:
+import "fmt"
+import "strconv"
 
-	Input:
-	[
-	  ["8","3",".",".","7",".",".",".","."],
-	  ["6",".",".","1","9","5",".",".","."],
-	  [".","9","8",".",".",".",".","6","."],
-	  ["8",".",".",".","6",".",".",".","3"],
-	  ["4",".",".","8",".","3",".",".","1"],
-	  ["7",".",".",".","2",".",".",".","6"],
-	  [".","6",".",".",".",".","2","8","."],
-	  [".",".",".","4","1","9",".",".","5"],
-	  [".",".",".",".","8",".",".","7","9"]
-	]
-	Output: false
-
-Explanation: Same as Example 1, except with the 5 in the top left corner being
-modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
-
- */
-
-import (
-	"fmt"
-	"strconv"
-)
-
-// 解法一 暴力遍历，时间复杂度 O(n^3)
+// 暴力遍历，时间复杂度 O(n^3)
 func isValidSudoku(board [][]byte) bool {
-	// 判断行 row
-	for i := 0; i < 9; i++ {
-		tmp := [10]int{}
-		for j := 0; j < 9; j++ {
-			cellVal := board[i][j : j+1]
-			if string(cellVal) != "." {
-				index, _ := strconv.Atoi(string(cellVal))
-				if index > 9 || index < 1 {
-					return false
-				}
-				if tmp[index] == 1 {
-					return false
-				}
-				tmp[index] = 1
-			}
-		}
-	}
-	// 判断列 column
-	for i := 0; i < 9; i++ {
-		tmp := [10]int{}
-		for j := 0; j < 9; j++ {
-			cellVal := board[j][i]
-			if string(cellVal) != "." {
-				index, _ := strconv.Atoi(string(cellVal))
-				if index > 9 || index < 1 {
-					return false
-				}
-				if tmp[index] == 1 {
-					return false
-				}
-				tmp[index] = 1
-			}
-		}
-	}
-	// 判断 9宫格 3X3 cell
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			tmp := [10]int{}
-			for ii := i * 3; ii < i*3+3; ii++ {
-				for jj := j * 3; jj < j*3+3; jj++ {
-					cellVal := board[ii][jj]
-					if string(cellVal) != "." {
-						index, _ := strconv.Atoi(string(cellVal))
-						if tmp[index] == 1 {
-							return false
-						}
-						tmp[index] = 1
-					}
-				}
-			}
-		}
-	}
-	return true
+    // 判断行 row
+    for i := 0; i < 9; i++ {
+        tmp := [10]int{}
+        for j := 0; j < 9; j++ {
+            cellVal := board[i][j : j+1]
+            if string(cellVal) != "." {
+                index, _ := strconv.Atoi(string(cellVal))
+                if index > 9 || index < 1 {
+                    return false
+                }
+                if tmp[index] == 1 {
+                    return false
+                }
+                tmp[index] = 1
+            }
+        }
+    }
+    // 判断列 column
+    for i := 0; i < 9; i++ {
+        tmp := [10]int{}
+        for j := 0; j < 9; j++ {
+            cellVal := board[j][i]
+            if string(cellVal) != "." {
+                index, _ := strconv.Atoi(string(cellVal))
+                if index > 9 || index < 1 {
+                    return false
+                }
+                if tmp[index] == 1 {
+                    return false
+                }
+                tmp[index] = 1
+            }
+        }
+    }
+    // 判断 9宫格 3X3 cell
+    for i := 0; i < 3; i++ {
+        for j := 0; j < 3; j++ {
+            tmp := [10]int{}
+            for ii := i * 3; ii < i*3+3; ii++ {
+                for jj := j * 3; jj < j*3+3; jj++ {
+                    cellVal := board[ii][jj]
+                    if string(cellVal) != "." {
+                        index, _ := strconv.Atoi(string(cellVal))
+                        if tmp[index] == 1 {
+                            return false
+                        }
+                        tmp[index] = 1
+                    }
+                }
+            }
+        }
+    }
+    return true
 }
 
-// 解法二 添加缓存，时间复杂度 O(n^2) best solution
+//  添加缓存，时间复杂度 O(n^2) best solution
 func isValidSudoku1(board [][]byte) bool {
-	rowbuf, colbuf, boxbuf := make([][]bool, 9), make([][]bool, 9), make([][]bool, 9)
-	for i := 0; i < 9; i++ {
-		rowbuf[i] = make([]bool, 9)
-		colbuf[i] = make([]bool, 9)
-		boxbuf[i] = make([]bool, 9)
-	}
-	// 遍历一次，添加缓存
-	for r := 0; r < 9; r++ {
-		for c := 0; c < 9; c++ {
-			if board[r][c] != '.' {
-				num := board[r][c] - '0' - byte(1)
-				fmt.Printf("num = %v\n",num)
-				fmt.Printf("r/3*3+c/3 = %v\n",(r/3*3+c/3))
-				// 判断列 / 行 / 小范围 是否都出现了 num 值 如果是说明数据独不成立
-				if rowbuf[r][num] || colbuf[c][num] || boxbuf[r/3*3+c/3][num] {
-					return false
-				}
-				rowbuf[r][num] = true
-				colbuf[c][num] = true
-				boxbuf[r/3*3+c/3][num] = true // r,c 转换到box方格中
-			}
-		}
-	}
-	return true
+    rowbuf, colbuf, boxbuf := make([][]bool, 9), make([][]bool, 9), make([][]bool, 9)
+    for i := 0; i < 9; i++ {
+        rowbuf[i] = make([]bool, 9)
+        colbuf[i] = make([]bool, 9)
+        boxbuf[i] = make([]bool, 9)
+    }
+    // 遍历一次，添加缓存
+    for r := 0; r < 9; r++ {
+        for c := 0; c < 9; c++ {
+            if board[r][c] != '.' {
+                num := board[r][c] - '0' - byte(1)
+                // 判断列 / 行 / 小范围 是否都出现了 num 值 如果是说明数据独不成立
+                if rowbuf[r][num] || colbuf[c][num] || boxbuf[r/3*3+c/3][num] {
+                    return false
+                }
+                rowbuf[r][num] = true
+                colbuf[c][num] = true
+                boxbuf[r/3*3+c/3][num] = true // r,c 转换到box方格中
+            }
+        }
+    }
+    return true
 }
 
 func main() {
-	bytes := [][]byte{
-		[]byte{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-		[]byte{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-		[]byte{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-		[]byte{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-		[]byte{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-		[]byte{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-		[]byte{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-		[]byte{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-		[]byte{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
-	}
-	bytes1 := [][]byte{
-		[]byte{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
-		[]byte{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-		[]byte{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-		[]byte{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-		[]byte{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-		[]byte{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-		[]byte{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-		[]byte{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-		[]byte{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
-	}
-	fmt.Printf("isValidSudoku(bytes) = %v\n",isValidSudoku(bytes))
-	fmt.Printf("isValidSudoku(bytes1) = %v\n",isValidSudoku(bytes1))
-	fmt.Printf("isValidSudoku1(bytes) = %v\n",isValidSudoku1(bytes))
-	fmt.Printf("isValidSudoku(bytes1) = %v\n",isValidSudoku(bytes1))
+    bytes := [][]byte{
+        []byte{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        []byte{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        []byte{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        []byte{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        []byte{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        []byte{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        []byte{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        []byte{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        []byte{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+    }
+    bytes1 := [][]byte{
+        []byte{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+        []byte{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        []byte{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        []byte{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        []byte{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        []byte{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        []byte{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        []byte{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        []byte{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+    }
+    fmt.Printf("isValidSudoku(bytes) = %v\n",isValidSudoku(bytes))
+    fmt.Printf("isValidSudoku(bytes1) = %v\n",isValidSudoku(bytes1))
+    fmt.Printf("isValidSudoku1(bytes) = %v\n",isValidSudoku1(bytes))
+    fmt.Printf("isValidSudoku(bytes1) = %v\n",isValidSudoku(bytes1))
 }
