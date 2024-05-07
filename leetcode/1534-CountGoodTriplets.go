0de2a1e9acf404488efd131c02866a0a8f60d544
bluefrog <bluefrog.wu@gmail.com>
2024-05-07 10:22:52 +0800

    leetcode 624. Maximum Distance in Arrays

--- leetcode/1534-CountGoodTriplets.go
+++ leetcode/1534-CountGoodTriplets.go
@@ -30,7 +30,7 @@ import "fmt"
 
 // 暴力解法
 func countGoodTriplets(arr []int, a int, b int, c int) int {
-    abs := func (n int) int { if n < 0 { return -n ; }; return n; }
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
     res := 0
     for i := 0; i < len(arr); i++ {
         for j := i + 1; j < len(arr); j++ {
@@ -49,9 +49,9 @@ func countGoodTriplets(arr []int, a int, b int, c int) int {
 
 // best solution
 func countGoodTriplets1(arr []int, a int, b int, c int) int {
-    abs := func (n int) int { if n < 0 { return -n; }; return n; }
-    min := func (a int, b int) int { if a < b { return a; }; return b; }
-    max := func (a int, b int) int { if a < b { return b; }; return a; }
+    max := func(x, y int) int { if x > y { return x; }; return y; }
+    min:= func(x, y int) int { if x < y { return x; }; return y; }
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
     m := arr[0]
     for _, v := range arr {
         if v > m {
---
+++ leetcode/624-MaximumDistanceInArrays.go
@@ -0,0 +1,56 @@
+package main
+
+// 624. Maximum Distance in Arrays
+// You are given m arrays, where each array is sorted in ascending order.
+// You can pick up two integers from two different arrays (each array picks one) and calculate the distance. 
+// We define the distance between two integers a and b to be their absolute difference |a - b|.
+// Return the maximum distance.
+
+// Example 1:
+// Input: arrays = [[1,2,3],[4,5],[1,2,3]]
+// Output: 4
+// Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+
+// Example 2:
+// Input: arrays = [[1],[1]]
+// Output: 0
+
+// Constraints:
+//     m == arrays.length
+//     2 <= m <= 10^5
+//     1 <= arrays[i].length <= 500
+//     -10^4 <= arrays[i][j] <= 10^4
+//     arrays[i] is sorted in ascending order.
+//     There will be at most 10^5 integers in all the arrays.
+
+import "fmt"
+
+func maxDistance(arrays [][]int) int {
+    if len(arrays) <= 0 {
+        return 0
+    }
+    res, mx, mn := 0, arrays[0][len(arrays[0]) - 1], arrays[0][0]
+    max := func(x, y int) int { if x > y { return x; }; return y; }
+    min:= func(x, y int) int { if x < y { return x; }; return y; }
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
+    for i:=1; i<len(arrays); i++ {
+        n := len(arrays[i])
+        // 因为数据是有序的，每次比较的时候只用比较: 数组里面的第一个和最后一个值
+        res = max(res, max(abs(arrays[i][n-1] - mn), abs(mx - arrays[i][0])))
+        mx = max(mx, arrays[i][n-1])
+        mn = min(mn, arrays[i][0])
+    }
+    return res
+}
+
+func main() {
+// Example 1:
+// Input: arrays = [[1,2,3],[4,5],[1,2,3]]
+// Output: 4
+// Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+fmt.Println(maxDistance([][]int{{1,2,3},{4,5},{1,2,3}})) // 4
+// Example 2:
+// Input: arrays = [[1],[1]]
+// Output: 0
+fmt.Println(maxDistance([][]int{{1},{1}})) // 0
+}
