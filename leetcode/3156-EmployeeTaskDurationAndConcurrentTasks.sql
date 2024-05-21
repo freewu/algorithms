3d2d7822f7a72503005ec94c6ffe0cb1b72ee700
bluefrog <bluefrog.wu@gmail.com>
2024-05-21 12:52:14 +0800

    leetcode 3156. Employee Task Duration and Concurrent Tasks

--- leetcode/3156-EmployeeTaskDurationAndConcurrentTasks.sql
+++ leetcode/3156-EmployeeTaskDurationAndConcurrentTasks.sql
@@ -69,12 +69,11 @@
 -- insert into Tasks (task_id, employee_id, start_time, end_time) values ('5', '1002', '2023-05-01 09:30:00', '2023-05-01 11:30:00');
 -- insert into Tasks (task_id, employee_id, start_time, end_time) values ('6', '1003', '2023-05-01 14:00:00', '2023-05-01 16:00:00');
 
--- Write your MySQL query statement below
 -- WITH t AS (-- 得到一张能有时间关联的任务表
 --     SELECT
 --         t1.*,
---         t2.start_time AS t2s,
---         t2.end_time AS t2e,
+--         t2.start_time as t2s,
+--         t2.end_time as t2e,
 --         t2.task_id as t2id
 --     FROM
 --         Tasks AS t1
@@ -83,16 +82,91 @@
 --     ON 
 --         t1.task_id != t2.task_id AND 
 --         t1.employee_id = t2.employee_id AND
---         t1.start_time <= t2.start_time AND t1.end_time >= t2.start_time
+--         t1.start_time <= t2.start_time AND t1.end_time > t2.start_time
 -- )
+-- -- select * from t;
+-- -- SELECT t2id FROM t;
 -- SELECT
 --     employee_id,
---     SUM(
---         DATE_DIFF("hour", IFNULL(t.s2e, t.end_time), t.start_time)
---     ) AS total_task_hours 
+--     FLOOR(
+--         SUM(
+--             (TIMESTAMPDIFF(SECOND, t.start_time, IFNULL(t.t2e, t.end_time)) / 3600)
+--         )
+--     ) AS total_task_hours,
+--     COUNT(DISTINCT t2id) + 1 AS max_concurrent_tasks 
 -- FROM 
 --     t
+-- WHERE 
+--     task_id NOT IN (SELECT t2id FROM t WHERE t2id IS NOT NULL)
 -- GROUP BY 
 --     employee_id 
--- ORDER BY 
---     employee_id 
+
+
+-- Write your MySQL query statement below
+WITH t AS ( -- 取出开始时间&结束时间
+    SELECT DISTINCT employee_id, start_time AS st FROM Tasks
+    UNION DISTINCT
+    SELECT DISTINCT employee_id, end_time AS st   FROM Tasks
+),
+p AS (
+    SELECT
+        *,
+        LEAD(st) OVER ( PARTITION BY employee_id ORDER BY st ) AS ed
+    FROM 
+        t
+),
+s AS (
+    SELECT
+        p.*,
+        COUNT(1) AS concurrent_count
+    FROM
+        p
+    INNER JOIN 
+        Tasks AS t
+    USING (employee_id)
+    WHERE 
+        p.st >= t.start_time AND p.ed <= t.end_time
+    GROUP BY 
+        p.employee_id, p.st , p.ed
+)
+-- -- select * from p;
+-- | employee_id | st                  | ed                  |
+-- | ----------- | ------------------- | ------------------- |
+-- | 1001        | 2023-05-01 08:00:00 | 2023-05-01 08:30:00 |
+-- | 1001        | 2023-05-01 08:30:00 | 2023-05-01 09:00:00 |
+-- | 1001        | 2023-05-01 09:00:00 | 2023-05-01 10:30:00 |
+-- | 1001        | 2023-05-01 10:30:00 | 2023-05-01 11:00:00 |
+-- | 1001        | 2023-05-01 11:00:00 | 2023-05-01 12:00:00 |
+-- | 1001        | 2023-05-01 12:00:00 | 2023-05-01 13:00:00 |
+-- | 1001        | 2023-05-01 13:00:00 | 2023-05-01 15:30:00 |
+-- | 1001        | 2023-05-01 15:30:00 | null                |
+-- | 1002        | 2023-05-01 09:00:00 | 2023-05-01 09:30:00 |
+-- | 1002        | 2023-05-01 09:30:00 | 2023-05-01 10:00:00 |
+-- | 1002        | 2023-05-01 10:00:00 | 2023-05-01 11:30:00 |
+-- | 1002        | 2023-05-01 11:30:00 | null                |
+-- | 1003        | 2023-05-01 14:00:00 | 2023-05-01 16:00:00 |
+-- | 1003        | 2023-05-01 16:00:00 | null                |
+
+--select * from s;
+-- | employee_id | st                  | ed                  | concurrent_count |
+-- | ----------- | ------------------- | ------------------- | ---------------- |
+-- | 1001        | 2023-05-01 08:00:00 | 2023-05-01 08:30:00 | 1                |
+-- | 1001        | 2023-05-01 08:30:00 | 2023-05-01 09:00:00 | 2                |
+-- | 1001        | 2023-05-01 09:00:00 | 2023-05-01 10:30:00 | 1                |
+-- | 1001        | 2023-05-01 11:00:00 | 2023-05-01 12:00:00 | 1                |
+-- | 1001        | 2023-05-01 13:00:00 | 2023-05-01 15:30:00 | 1                |
+-- | 1002        | 2023-05-01 09:00:00 | 2023-05-01 09:30:00 | 1                |
+-- | 1002        | 2023-05-01 09:30:00 | 2023-05-01 10:00:00 | 2                |
+-- | 1002        | 2023-05-01 10:00:00 | 2023-05-01 11:30:00 | 1                |
+-- | 1003        | 2023-05-01 14:00:00 | 2023-05-01 16:00:00 | 1                |
+
+SELECT
+    employee_id,
+    FLOOR(SUM(TIMESTAMPDIFF(SECOND, st, ed) / 3600)) AS total_task_hours, 
+    MAX(concurrent_count) AS max_concurrent_tasks
+FROM 
+    s
+GROUP BY 
+    employee_id
+ORDER BY 
+    employee_id
