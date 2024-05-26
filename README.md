2642d3d5f46af1efa67a3f1648e7a9a70df1cd9b
bluefrog <bluefrog.wu@gmail.com>
2024-05-27 01:37:51 +0800

    leetcode LCR 009. 乘积小于 K 的子数组

--- README.md
+++ README.md
@@ -1481,6 +1481,7 @@ LCR 075. 数组的相对排序
 [LCR 006. 两数之和 II - 输入有序数组](./leetcode/LCR006-TwoSumII.go)   
 [LCR 007. 三数之和](./leetcode/LCR007-ThreeSum.go)   
 [LCR 008. 长度最小的子数组](./leetcode/LCR008-MinimumSizeSubarraySum.go)   
+[LCR 009. 乘积小于 K 的子数组](./leetcode/LCR009-SubarrayProductLessThanK.go)   
 [LCR 038. 每日温度](./leetcode/LCR038-DailyTemperatures.go)   
 [LCR 057. 存在重复元素 III](./leetcode/LCR057-ContainsDuplicateIII.go)   
 [LCR 060. 前 K 个高频元素](./leetcode/LCR060-TopKFrequentElements.go)   
--- leetcode/LCR009-SubarrayProductLessThanK.go
+++ leetcode/LCR009-SubarrayProductLessThanK.go
@@ -1,21 +1,19 @@
 package main
 
-// 713. Subarray Product Less Than K
-// Given an array of integers nums and an integer k, 
-// return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+// LCR 009. 乘积小于 K 的子数组
+// 给定一个正整数数组 nums和整数 k ，请找出该数组内乘积小于 k 的连续的子数组的个数。
 
-// Example 1:
-// Input: nums = [10,5,2,6], k = 100
-// Output: 8
-// Explanation: The 8 subarrays that have product less than 100 are:
-// [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
-// Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+// 示例 1:
+// 输入: nums = [10,5,2,6], k = 100
+// 输出: 8
+// 解释: 8 个乘积小于 100 的子数组分别为: [10], [5], [2], [6], [10,5], [5,2], [2,6], [5,2,6]。
+// 需要注意的是 [10,5,2] 并不是乘积小于100的子数组。
 
-// Example 2:
-// Input: nums = [1,2,3], k = 0
-// Output: 0
- 
-// Constraints:
+// 示例 2:
+// 输入: nums = [1,2,3], k = 0
+// 输出: 0
+
+// 提示: 
 //     1 <= nums.length <= 3 * 10^4
 //     1 <= nums[i] <= 1000
 //     0 <= k <= 10^6
@@ -58,6 +56,20 @@ func numSubarrayProductLessThanK1(nums []int, k int) int {
     return res
 }
 
+func numSubarrayProductLessThanK2(nums []int, k int) int {
+    res, i, j, multiply := 0, 0, 0, 1
+    for j < len(nums) {
+        multiply *= nums[j]
+        for multiply >= k && i <= j {
+            multiply /= nums[i]
+            i++
+        }
+        res += j - i + 1
+        j++
+    }
+    return res
+}
+
 func main() {
     // Explanation: The 8 subarrays that have product less than 100 are:
     // [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
@@ -67,4 +79,7 @@ func main() {
 
     fmt.Println(numSubarrayProductLessThanK1([]int{10,5,2,6},100)) // 8
     fmt.Println(numSubarrayProductLessThanK1([]int{1,2,3},0)) // 0
+
+    fmt.Println(numSubarrayProductLessThanK2([]int{10,5,2,6},100)) // 8
+    fmt.Println(numSubarrayProductLessThanK2([]int{1,2,3},0)) // 0
 }
