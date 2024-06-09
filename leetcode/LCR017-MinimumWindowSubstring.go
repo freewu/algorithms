af0cb0104b0e5618230fdd0767d0e05181be3e74
bluefrog <bluefrog.wu@gmail.com>
2024-06-09 23:09:33 +0800

    leetcode LCR 017. 最小覆盖子串

--- leetcode/LCR017-MinimumWindowSubstring.go
+++ leetcode/LCR017-MinimumWindowSubstring.go
@@ -1,35 +1,30 @@
 package main
 
-// 76. Minimum Window Substring
-// Given two strings s and t of lengths m and n respectively, 
-// return the minimum window substring of s such that every character in t (including duplicates) is included in the window. 
-// If there is no such substring, return the empty string "".
+// LCR 017. 最小覆盖子串
+// 给定两个字符串 s 和 t 。返回 s 中包含 t 的所有字符的最短子字符串。如果 s 中不存在符合条件的子字符串，则返回空字符串 "" 。
+// 如果 s 中存在多个符合条件的子字符串，返回任意一个。
 
-// The testcases will be generated such that the answer is unique.
+// 注意： 对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
 
-// Example 1:
-// Input: s = "ADOBECODEBANC", t = "ABC"
-// Output: "BANC"
-// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+// 示例 1：
+// 输入：s = "ADOBECODEBANC", t = "ABC"
+// 输出："BANC" 
+// 解释：最短子字符串 "BANC" 包含了字符串 t 的所有字符 'A'、'B'、'C'
 
-// Example 2:
-// Input: s = "a", t = "a"
-// Output: "a"
-// Explanation: The entire string s is the minimum window.
+// 示例 2：
+// 输入：s = "a", t = "a"
+// 输出："a"
 
-// Example 3:
-// Input: s = "a", t = "aa"
-// Output: ""
-// Explanation: Both 'a's from t must be included in the window.
-// Since the largest window of s only has one 'a', return empty string.
-
-// Constraints:
-//     m == s.length
-//     n == t.length
-//     1 <= m, n <= 10^5
-//     s and t consist of uppercase and lowercase English letters.
-
-// Follow up: Could you find an algorithm that runs in O(m + n) time?
+// 示例 3：
+// 输入：s = "a", t = "aa"
+// 输出：""
+// 解释：t 中两个字符 'a' 均应包含在 s 的子串中，因此没有符合条件的子字符串，返回空字符串。
+ 
+// 提示：
+//     1 <= s.length, t.length <= 10^5
+//     s 和 t 由英文字母组成
+ 
+// 进阶：你能设计一个在 o(n) 时间内解决此问题的算法吗？
 
 // 解题思路:
 //     滑动窗口
