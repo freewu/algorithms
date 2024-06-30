a16fc794a847e2602510bebf6f7f259635b3e444
bluefrog <bluefrog.wu@gmail.com>
2024-07-01 01:47:11 +0800

    leetcode LCR 039. 柱状图中最大的矩形

--- README.md
+++ README.md
@@ -1822,6 +1822,7 @@ LCR 075. 数组的相对排序
 [LCR 036. 逆波兰表达式求值](./leetcode/LCR036-EvaluateReversePolishNotation.go)   
 [LCR 037. 行星碰撞](./leetcode/LCR037-AsteroidCollision.go)   
 [LCR 038. 每日温度](./leetcode/LCR038-DailyTemperatures.go)   
+[LCR 039. 柱状图中最大的矩形](./leetcode/LCR039-LargestRectangleInHistogram.go)   
 [LCR 040. 最大矩形](./leetcode/LCR040-MaximalRectangle.go)   
 [LCR 057. 存在重复元素 III](./leetcode/LCR057-ContainsDuplicateIII.go)   
 [LCR 060. 前 K 个高频元素](./leetcode/LCR060-TopKFrequentElements.go)   
--- leetcode/LCR039-LargestRectangleInHistogram.go
+++ leetcode/LCR039-LargestRectangleInHistogram.go
@@ -1,22 +1,22 @@
 package main
 
-// 84. Largest Rectangle in Histogram
-// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1,
-// return the area of the largest rectangle in the histogram.
+// LCR 039. 柱状图中最大的矩形
+// 给定非负整数数组 heights ，数组中的数字用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。
 
-// Example 1:
+// 示例 1:
 // <img src="https://assets.leetcode.com/uploads/2021/01/04/histogram.jpg" />
-// Input: heights = [2,1,5,6,2,3]
-// Output: 10
-// Explanation: The above is a histogram where width of each bar is 1.
-// The largest rectangle is shown in the red area, which has an area = 10 units.
+// 输入：heights = [2,1,5,6,2,3]
+// 输出：10
+// 解释：最大的矩形为图中红色区域，面积为 10
 
-// Example 2:
-// Input: heights = [2,4]
-// Output: 4
- 
-// Constraints:
-//     1 <= heights.length <= 10^5
+// 示例 2：
+// <img src="https://assets.leetcode.com/uploads/2021/01/04/histogram-1.jpg" />
+// 输入： heights = [2,4]
+// 输出： 4
+
+// 提示：
+//     1 <= heights.length <=10^5
 //     0 <= heights[i] <= 10^4
 
 import "fmt"
@@ -73,8 +73,6 @@ func largestRectangleArea1(heights []int) int {
     }
     return res
 }
-
-
 
 func main() {
     // Explanation: The above is a histogram where width of each bar is 1.
