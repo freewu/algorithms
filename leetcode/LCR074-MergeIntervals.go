d1970346ffe94e2132cf8a08ed9e1d54288b1ddc
bluefrog <bluefrog.wu@gmail.com>
2024-07-18 10:48:52 +0800

    leetcode LCR 074. 合并区间

--- leetcode/LCR074-MergeIntervals.go
+++ leetcode/LCR074-MergeIntervals.go
@@ -1,20 +1,20 @@
 package main
 
-// 56. Merge Intervals
-// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
-// and return an array of the non-overlapping intervals that cover all the intervals in the input.
+// LCR 074. 合并区间
+// 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+// 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 
-// Example 1:
-// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-// Output: [[1,6],[8,10],[15,18]]
-// Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+// 示例 1：
+// 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+// 输出：[[1,6],[8,10],[15,18]]
+// 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
 
-// Example 2:
-// Input: intervals = [[1,4],[4,5]]
-// Output: [[1,5]]
-// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
- 
-// Constraints:
+// 示例 2：
+// 输入：intervals = [[1,4],[4,5]]
+// 输出：[[1,5]]
+// 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+// 提示：
 //     1 <= intervals.length <= 10^4
 //     intervals[i].length == 2
 //     0 <= starti <= endi <= 10^4
