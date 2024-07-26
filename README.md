830d8491a043e95588ec9c04065c8786d54b66bf
bluefrog <bluefrog.wu@gmail.com>
2024-07-27 00:21:44 +0800

    leetcode LCR 082. 组合总和 II

--- README.md
+++ README.md
@@ -1989,6 +1989,7 @@ LCR 075. 数组的相对排序
 [LCR 079. 子集](./leetcode/LCR079-Subsets.go)   
 [LCR 080. 组合](./leetcode/LCR080-Combinations.go)   
 [LCR 081. 组合总和](./leetcode/LCR081-CombinationSum.go)   
+[LCR 082. 组合总和 II](./leetcode/LCR082-CombinationSumII.go)   
 [LCR 122. 路径加密](./leetcode/LCR122-PathEncryption.go)   
 [LCR 123. 图书整理 I](./leetcode/LCR123-ReverseBookList.go)   
 [LCR 125. 图书整理 II](./leetcode/LCR125-BookQueue.go)   
--- leetcode/LCR082-CombinationSumII.go
+++ leetcode/LCR082-CombinationSumII.go
@@ -1,33 +1,28 @@
 package main
 
-// 40. Combination Sum II
-// Given a collection of candidate numbers (candidates) and a target number (target),
-// find all unique combinations in candidates where the candidate numbers sums to target.
-// Each number in candidates may only be used once in the combination.
+// LCR 082. 组合总和 II
+// 给定一个可能有重复数字的整数数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+// candidates 中的每个数字在每个组合中只能使用一次，解集不能包含重复的组合。 
 
-// Note:
-//     All numbers (including target) will be positive integers.
-//     The solution set must not contain duplicate combinations.
-
-// Example 1:
-// Input: candidates = [10,1,2,7,6,1,5], target = 8,
-// A solution set is:
+// 示例 1:
+// 输入: candidates = [10,1,2,7,6,1,5], target = 8,
+// 输出:
 // [
-//     [1, 7],
-//     [1, 2, 5],
-//     [2, 6],
-//     [1, 1, 6]
+// [1,1,6],
+// [1,2,5],
+// [1,7],
+// [2,6]
 // ]
 
-// Example 2:
-// Input: candidates = [2,5,2,1,2], target = 5,
-// A solution set is:
+// 示例 2:
+// 输入: candidates = [2,5,2,1,2], target = 5,
+// 输出:
 // [
-//     [1,2,2],
-//     [5]
+// [1,2,2],
+// [5]
 // ]
 
-// Constraints:
+// 提示:
 //     1 <= candidates.length <= 100
 //     1 <= candidates[i] <= 50
 //     1 <= target <= 30
