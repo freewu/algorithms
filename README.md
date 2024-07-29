480c80d7d5b940fc0fe07ed81e43d455b13e1143
bluefrog <bluefrog.wu@gmail.com>
2024-07-29 11:00:28 +0800

    leetcode LCR 084. 全排列 II

--- README.md
+++ README.md
@@ -2001,6 +2001,7 @@ LCR 075. 数组的相对排序
 [LCR 081. 组合总和](./leetcode/LCR081-CombinationSum.go)   
 [LCR 082. 组合总和 II](./leetcode/LCR082-CombinationSumII.go)   
 [LCR 083. 全排列](./leetcode/LCR083-Permutations.go)   
+[LCR 084. 全排列 II](./leetcode/LCR084-PermutationsII.go)   
 [LCR 122. 路径加密](./leetcode/LCR122-PathEncryption.go)   
 [LCR 123. 图书整理 I](./leetcode/LCR123-ReverseBookList.go)   
 [LCR 125. 图书整理 II](./leetcode/LCR125-BookQueue.go)   
--- leetcode/LCR084-PermutationsII.go
+++ leetcode/LCR084-PermutationsII.go
@@ -1,18 +1,20 @@
 package main
 
-// 47. Permutations II
-// Given a collection of numbers, nums, that might contain duplicates, 
-// return all possible unique permutations in any order.
+// LCR 084. 全排列 II 
+// 给定一个可包含重复数字的整数集合 nums ，按任意顺序 返回它所有不重复的全排列。
 
-// Example 1:
-// Input: nums = [1,1,2]
-// Output: [[1,1,2], [1,2,1], [2,1,1]]
+// 示例 1：
+// 输入：nums = [1,1,2]
+// 输出：
+// [[1,1,2],
+//  [1,2,1],
+//  [2,1,1]]
 
-// Example 2:
-// Input: nums = [1,2,3]
-// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
- 
-// Constraints:
+// 示例 2：
+// 输入：nums = [1,2,3]
+// 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+// 提示：
 //     1 <= nums.length <= 8
 //     -10 <= nums[i] <= 10
 
