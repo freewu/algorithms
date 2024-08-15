8a900c47dd61dae5f96370c6cd59ea29ae173fcd
bluefrog <bluefrog.wu@gmail.com>
2024-08-16 00:42:53 +0800

    leetcode LCR 103. 零钱兑换

--- README.md
+++ README.md
@@ -2189,6 +2189,7 @@ LCR 075. 数组的相对排序
 [LCR 100. 三角形最小路径和](./leetcode/LCR100-Triangle.go)   
 [LCR 101. 分割等和子集](./leetcode/LCR101-PartitionEqualSubsetSum.go)   
 [LCR 102. 目标和](./leetcode/LCR102-TargetSum.go)   
+[LCR 103. 零钱兑换](./leetcode/LCR103-CoinChange.go)   
 [LCR 120. 寻找文件副本](./leetcode/LCR120-FindRepeatDocument.go)   
 [LCR 121. 寻找目标值 - 二维数组](./leetcode/LCR121-FindTargetInMatrix.go)   
 [LCR 122. 路径加密](./leetcode/LCR122-PathEncryption.go)   
--- leetcode/LCR103-CoinChange.go
+++ leetcode/LCR103-CoinChange.go
@@ -1,25 +1,34 @@
 package main
 
-// 322. Coin Change
-// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
-// Return the fewest number of coins that you need to make up that amount. 
-// If that amount of money cannot be made up by any combination of the coins, return -1.
-// You may assume that you have an infinite number of each kind of coin.
+// LCR 103. 零钱兑换
+// 给定不同面额的硬币 coins 和一个总金额 amount。
+// 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+// 如果没有任何一种硬币组合能组成总金额，返回 -1。
 
-// Example 1:
-// Input: coins = [1,2,5], amount = 11
-// Output: 3
-// Explanation: 11 = 5 + 5 + 1
+// 你可以认为每种硬币的数量是无限的。
 
-// Example 2:
-// Input: coins = [2], amount = 3
-// Output: -1
+// 示例 1：
+// 输入：coins = [1, 2, 5], amount = 11
+// 输出：3 
+// 解释：11 = 5 + 5 + 1
 
-// Example 3:
-// Input: coins = [1], amount = 0
-// Output: 0
- 
-// Constraints:
+// 示例 2：
+// 输入：coins = [2], amount = 3
+// 输出：-1
+
+// 示例 3：
+// 输入：coins = [1], amount = 0
+// 输出：0
+
+// 示例 4：
+// 输入：coins = [1], amount = 1
+// 输出：1
+
+// 示例 5：
+// 输入：coins = [1], amount = 2
+// 输出：2
+
+// 提示：
 //     1 <= coins.length <= 12
 //     1 <= coins[i] <= 2^31 - 1
 //     0 <= amount <= 10^4
