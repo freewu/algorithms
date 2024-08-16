26987e2f31496e600696594215bb0ffadd89bde1
bluefrog <bluefrog.wu@gmail.com>
2024-08-16 11:13:52 +0800

    leetcode 1970. Last Day Where You Can Still Cross

---
+++ leetcode/1970-LastDayWhereYouCanStillCross.go
@@ -0,0 +1,288 @@
+package main
+
+// 1970. Last Day Where You Can Still Cross
+// There is a 1-based binary matrix where 0 represents land and 1 represents water. 
+// You are given integers row and col representing the number of rows and columns in the matrix, respectively.
+
+// Initially on day 0, the entire matrix is land. However, each day a new cell becomes flooded with water. 
+// You are given a 1-based 2D array cells, where cells[i] = [ri, ci] represents that on the ith day, 
+// the cell on the rith row and cith column (1-based coordinates) will be covered with water (i.e., changed to 1).
+
+// You want to find the last day that it is possible to walk from the top to the bottom by only walking on land cells. 
+// You can start from any cell in the top row and end at any cell in the bottom row. 
+// You can only travel in the four cardinal directions (left, right, up, and down).
+
+// Return the last day where it is possible to walk from the top to the bottom by only walking on land cells.
+
+// Example 1:
+// <img src="https://assets.leetcode.com/uploads/2021/07/27/1.png" />
+// Input: row = 2, col = 2, cells = [[1,1],[2,1],[1,2],[2,2]]
+// Output: 2
+// Explanation: The above image depicts how the matrix changes each day starting from day 0.
+// The last day where it is possible to cross from top to bottom is on day 2.
+
+// Example 2:
+// <img src="https://assets.leetcode.com/uploads/2021/07/27/2.png" />
+// Input: row = 2, col = 2, cells = [[1,1],[1,2],[2,1],[2,2]]
+// Output: 1
+// Explanation: The above image depicts how the matrix changes each day starting from day 0.
+// The last day where it is possible to cross from top to bottom is on day 1.
+
+// Example 3:
+// <img src="https://assets.leetcode.com/uploads/2021/07/27/3.png" />
+// Input: row = 3, col = 3, cells = [[1,2],[2,1],[3,3],[2,2],[1,1],[1,3],[2,3],[3,2],[3,1]]
+// Output: 3
+// Explanation: The above image depicts how the matrix changes each day starting from day 0.
+// The last day where it is possible to cross from top to bottom is on day 3.
+
+// Constraints:
+//     2 <= row, col <= 2 * 10^4
+//     4 <= row * col <= 2 * 10^4
+//     cells.length == row * col
+//     1 <= ri <= row
+//     1 <= ci <= col
+//     All the values of cells are unique.
+
+import "fmt"
+
+// bfs
+func latestDayToCross(row int, col int, cells [][]int) int {
+    res, left, right := 0, 0, row * col
+    isPossible := func(m int, n int, t int, cells [][]int) bool {
+        grid := make([][]int, m+1)
+        for i := range grid {
+            grid[i] = make([]int, n+1)
+        }
+        directions := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+        for i := 0; i < t; i++ {
+            grid[cells[i][0]][cells[i][1]] = 1
+        }
+        queue := [][]int{}
+        for i := 1; i <= n; i++ {
+            if grid[1][i] == 0 {
+                queue = append(queue, []int{1, i})
+                grid[1][i] = 1
+            }
+        }
+        for len(queue) > 0 {
+            cell := queue[0]
+            queue = queue[1:]
+            r, c := cell[0], cell[1]
+            for _, dir := range directions {
+                nr, nc := r+dir[0], c+dir[1]
+                if nr > 0 && nc > 0 && nr <= m && nc <= n && grid[nr][nc] == 0 {
+                    grid[nr][nc] = 1
+                    if nr == m {
+                        return true
+                    }
+                    queue = append(queue, []int{nr, nc})
+                }
+            }
+        }
+        return false
+    }
+    for left < right-1 {
+        mid := left + (right-left)/2
+        if isPossible(row, col, mid, cells) {
+            left = mid
+            res = mid
+        } else {
+            right = mid
+        }
+    }
+    return res
+}
+
+
+// dfs
+func latestDayToCross1(row int, col int, cells [][]int) int {
+    l, r := 0, len(cells)-1
+    n, m := row, col
+    directions := []int{0, 1, 0, -1, 0}
+    helper := func(vis [][]int, v [][]int, i int, j int) bool {
+        vis[i][j] = 1
+        stack := [][]int{{i, j}}
+        res := false
+        for len(stack) > 0 {
+            cell := stack[len(stack)-1] // pop
+            stack = stack[:len(stack)-1]
+            r, c := cell[0], cell[1]
+            if r == n - 1 {
+                res = true
+                break
+            }
+            for k := 0; k < 4; k++ {
+                nr, nc := r + directions[k], c + directions[k+1]
+                if nr >= 0 && nc >= 0 && nr < n && nc < m && vis[nr][nc] == 0 && v[nr][nc] == 0 {
+                    vis[nr][nc] = 1
+                    stack = append(stack, []int{nr, nc})
+                }
+            }
+        }
+        return res
+    }
+    for l <= r {
+        mid := (l + r) / 2
+        v := make([][]int, row)
+        for i := range v {
+            v[i] = make([]int, col)
+        }
+        for i := 0; i <= mid; i++ {
+            v[cells[i][0]-1][cells[i][1]-1] = 1
+        }
+        vis := make([][]int, row)
+        for i := range vis {
+            vis[i] = make([]int, col)
+        }
+        flag := false
+        for i := 0; i < col; i++ {
+            if vis[0][i] == 0 && v[0][i] == 0 {
+                flag = flag || helper(vis, v, 0, i)
+            }
+        }
+        if flag {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+    return l
+}
+
+// Union Find
+func latestDayToCross2(row, col int, cells [][]int) int {
+    grid := make([][]int, row)
+    for i := range grid {
+        grid[i] = make([]int, col)
+        for j := range grid[i] {
+            grid[i][j] = 1
+        }
+    }
+    p := make([]int, row*col+3)
+    for i := range p {
+        p[i] = -1
+    }
+    directions := [][]int{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+    var find func (x int) int
+    find = func (x int) int {
+        if p[x] < 0 {
+            return x
+        }
+        p[x] = find(p[x])
+        return p[x]
+    }
+    union := func (x, y int) {
+        x = find(x)
+        y = find(y)
+        if x == y {
+            return
+        }
+        p[y] = x
+    }
+    startG, endG := row*col + 1, row*col + 2
+    for j := 0; j < col; j++ {
+        union(j, startG)
+        union((row-1)*col+j, endG)
+    }
+    for i := row*col - 1; i >= 0; i-- {
+        r := cells[i][0] - 1
+        c := cells[i][1] - 1
+        grid[r][c] = 0
+        key := r*col + c
+        for k := 0; k < 4; k++ {
+            nr := r + directions[k][0]
+            nc := c + directions[k][1]
+            if nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 0 {
+                nKey := nr*col + nc
+                union(key, nKey)
+            }
+            if find(startG) == find(endG) {
+                return i
+            }
+        }
+    }
+    return row * col - 1
+}
+
+func latestDayToCross3(row int, col int, cells [][]int) int {
+    directions := []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+    src := row * col
+    dst := src + 1
+    fa := make([]int, dst+1)
+    for i := range fa {
+        fa[i] = i
+    }
+    find := func(x int) int {
+        res := x
+        for fa[res] != res {
+            res = fa[res]
+        }
+        for fa[x] != res {
+            fa[x], x = res, fa[x]
+        }
+        return res
+    }
+    merge := func(from, to int) { fa[find(from)] = find(to) }
+    same := func(x, y int) bool { return find(x) == find(y) }
+
+    land := make([][]bool, row)
+    for i := range land {
+        land[i] = make([]bool, col)
+    }
+    for day := len(cells) - 1; ; day-- {
+        p := cells[day]
+        r, c := p[0]-1, p[1]-1
+        v := r*col + c
+        for _, dir := range directions {
+            if x, y := r+dir.x, c+dir.y; x >= 0 && x < row && y >= 0 && y < col && land[x][y] {
+                merge(v, x*col+y)
+            }
+        }
+        land[r][c] = true
+        if r == 0 {
+            merge(v, src)
+        }
+        if r == row-1 {
+            merge(v, dst)
+        }
+        if same(src, dst) {
+            return day
+        }
+    }
+}
+
+func main() {
+    // Example 1:
+    // <img src="https://assets.leetcode.com/uploads/2021/07/27/1.png" />
+    // Input: row = 2, col = 2, cells = [[1,1],[2,1],[1,2],[2,2]]
+    // Output: 2
+    // Explanation: The above image depicts how the matrix changes each day starting from day 0.
+    // The last day where it is possible to cross from top to bottom is on day 2.
+    fmt.Println(latestDayToCross(2,2,[][]int{{1,1},{2,1},{1,2},{2,2}})) // 2
+    // Example 2:
+    // <img src="https://assets.leetcode.com/uploads/2021/07/27/2.png" />
+    // Input: row = 2, col = 2, cells = [[1,1],[1,2],[2,1],[2,2]]
+    // Output: 1
+    // Explanation: The above image depicts how the matrix changes each day starting from day 0.
+    // The last day where it is possible to cross from top to bottom is on day 1.
+    fmt.Println(latestDayToCross(2,2,[][]int{{1,1},{1,2},{2,1},{2,2}})) // 1
+    // Example 3:
+    // <img src="https://assets.leetcode.com/uploads/2021/07/27/3.png" />
+    // Input: row = 3, col = 3, cells = [[1,2],[2,1],[3,3],[2,2],[1,1],[1,3],[2,3],[3,2],[3,1]]
+    // Output: 3
+    // Explanation: The above image depicts how the matrix changes each day starting from day 0.
+    // The last day where it is possible to cross from top to bottom is on day 3.
+    fmt.Println(latestDayToCross(3,3,[][]int{{1,2},{2,1},{3,3},{2,2},{1,1},{1,3},{2,3},{3,2},{3,1}})) // 3
+
+    fmt.Println(latestDayToCross1(2,2,[][]int{{1,1},{2,1},{1,2},{2,2}})) // 2
+    fmt.Println(latestDayToCross1(2,2,[][]int{{1,1},{1,2},{2,1},{2,2}})) // 1
+    fmt.Println(latestDayToCross1(3,3,[][]int{{1,2},{2,1},{3,3},{2,2},{1,1},{1,3},{2,3},{3,2},{3,1}})) // 3
+
+    fmt.Println(latestDayToCross2(2,2,[][]int{{1,1},{2,1},{1,2},{2,2}})) // 2
+    fmt.Println(latestDayToCross2(2,2,[][]int{{1,1},{1,2},{2,1},{2,2}})) // 1
+    fmt.Println(latestDayToCross2(3,3,[][]int{{1,2},{2,1},{3,3},{2,2},{1,1},{1,3},{2,3},{3,2},{3,1}})) // 3
+
+    fmt.Println(latestDayToCross3(2,2,[][]int{{1,1},{2,1},{1,2},{2,2}})) // 2
+    fmt.Println(latestDayToCross3(2,2,[][]int{{1,1},{1,2},{2,1},{2,2}})) // 1
+    fmt.Println(latestDayToCross3(3,3,[][]int{{1,2},{2,1},{3,3},{2,2},{1,1},{1,3},{2,3},{3,2},{3,1}})) // 3
+}
--- leetcode/624-MaximumDistanceInArrays.go
+++ leetcode/624-MaximumDistanceInArrays.go
@@ -44,13 +44,13 @@ func maxDistance(arrays [][]int) int {
 }
 
 func main() {
-// Example 1:
-// Input: arrays = [[1,2,3],[4,5],[1,2,3]]
-// Output: 4
-// Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
-fmt.Println(maxDistance([][]int{{1,2,3},{4,5},{1,2,3}})) // 4
-// Example 2:
-// Input: arrays = [[1],[1]]
-// Output: 0
-fmt.Println(maxDistance([][]int{{1},{1}})) // 0
+    // Example 1:
+    // Input: arrays = [[1,2,3],[4,5],[1,2,3]]
+    // Output: 4
+    // Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+    fmt.Println(maxDistance([][]int{{1,2,3},{4,5},{1,2,3}})) // 4
+    // Example 2:
+    // Input: arrays = [[1],[1]]
+    // Output: 0
+    fmt.Println(maxDistance([][]int{{1},{1}})) // 0
 }
