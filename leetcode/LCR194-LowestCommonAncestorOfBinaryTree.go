5df544fb266a1afcab13898f8c4a1928debf3d0c
bluefrog <bluefrog.wu@gmail.com>
2024-08-18 00:26:35 +0800

    leetcode LCR 194. 二叉树的最近公共祖先

--- leetcode/LCR194-LowestCommonAncestorOfBinaryTree.go
+++ leetcode/LCR194-LowestCommonAncestorOfBinaryTree.go
@@ -1,33 +1,24 @@
 package main
 
-// 236. Lowest Common Ancestor of a Binary Tree
-// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
-// According to the definition of LCA on Wikipedia: 
-//     The lowest common ancestor is defined between two nodes p 
-//     and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).
+// LCR 194. 二叉树的最近公共祖先
+// 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+// 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+// <img src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/12/15/binarytree.png" />
 
-// Example 1:
-// <img src="https://assets.leetcode.com/uploads/2018/12/14/binarytree.png" />
-// Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
-// Output: 3
-// Explanation: The LCA of nodes 5 and 1 is 3.
+// 示例 1:
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+// 输出: 3
+// 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
 
-// Example 2:
-// <img src="https://assets.leetcode.com/uploads/2018/12/14/binarytree.png" />
-// Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
-// Output: 5
-// Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+// 示例 2:
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+// 输出: 5
+// 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
 
-// Example 3:
-// Input: root = [1,2], p = 1, q = 2
-// Output: 1
- 
-// Constraints:
-//     The number of nodes in the tree is in the range [2, 10^5].
-//     -10^9 <= Node.val <= 10^9
-//     All Node.val are unique.
-//     p != q
-//     p and q will exist in the tree.
+// 说明:
+//     所有节点的值都是唯一的。
+//     p、q 为不同节点且均存在于给定的二叉树中。
 
 import "fmt"
 
@@ -55,11 +46,7 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
         return root
     }
     // 递归查找 left / right
-    left := lowestCommonAncestor(root.Left, p, q)
-    right := lowestCommonAncestor(root.Right, p, q)
-    // fmt.Println("root: ", root)
-    // fmt.Println("left: ", left)
-    // fmt.Println("right: ", right)
+    left, right := lowestCommonAncestor(root.Left, p, q), lowestCommonAncestor(root.Right, p, q)
     // 如果 left 和 right 都不为 nil 返回 root
     if left != nil && right != nil {
         return root
