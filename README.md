4f0669b38c05b62a30f7c2eeafa050552c4bfbfe
bluefrog <bluefrog.wu@gmail.com>
2024-08-23 00:35:18 +0800

    leetcode LCR 110. 所有可能的路径

--- README.md
+++ README.md
@@ -2255,6 +2255,7 @@ LCR 075. 数组的相对排序
 [LCR 107. 01 矩阵](./leetcode/LCR107-01matrix.go)   
 [LCR 108. 单词接龙](./leetcode/LCR108-WordLadder.go)   
 [LCR 109. 打开转盘锁](./leetcode/LCR109-OpenSwingLock.go)   
+[LCR 110. 所有可能的路径](./leetcode/LCR-110-AllPathsFromSourceToTarget.go)   
 [LCR 120. 寻找文件副本](./leetcode/LCR120-FindRepeatDocument.go)   
 [LCR 121. 寻找目标值 - 二维数组](./leetcode/LCR121-FindTargetInMatrix.go)   
 [LCR 122. 路径加密](./leetcode/LCR122-PathEncryption.go)   
--- leetcode/LCR110-AllPathsFromSourceToTarget.go
+++ leetcode/LCR110-AllPathsFromSourceToTarget.go
@@ -1,29 +1,40 @@
 package main
 
-// 797. All Paths From Source to Target
-// Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, 
-// find all possible paths from node 0 to node n - 1 and return them in any order.
+// LCR 110. 所有可能的路径
+// 给定一个有 n 个节点的有向无环图，用二维数组 graph 表示，请找到所有从 0 到 n-1 的路径并输出（不要求按顺序）。
 
-// The graph is given as follows: graph[i] is a list of all nodes you can visit from node i 
-// (i.e., there is a directed edge from node i to node graph[i][j]).
+// graph 的第 i 个数组中的单元都表示有向图中 i 号节点所能到达的下一些结点（译者注：有向图是有方向的，即规定了 a→b 你就不能从 b→a ），
+// 若为空，就是没有下一个节点了。
 
-// Example 1:
+// 示例 1：
 // <img src="https://assets.leetcode.com/uploads/2020/09/28/all_1.jpg" />
-// Input: graph = [[1,2],[3],[3],[]]
-// Output: [[0,1,3],[0,2,3]]
-// Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+// 输入：graph = [[1,2],[3],[3],[]]
+// 输出：[[0,1,3],[0,2,3]]
+// 解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
 
-// Example 2:
-// Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
-// Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
- 
-// Constraints:
+// 示例 2：
+// <img src="https://assets.leetcode.com/uploads/2020/09/28/all_2.jpg" />
+// 输入：graph = [[4,3,1],[3,2,4],[3],[4],[]]
+// 输出：[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+
+// 示例 3：
+// 输入：graph = [[1],[]]
+// 输出：[[0,1]]
+
+// 示例 4：
+// 输入：graph = [[1,2,3],[2],[3],[]]
+// 输出：[[0,1,2,3],[0,2,3],[0,3]]
+
+// 示例 5：
+// 输入：graph = [[1,3],[2],[3],[]]
+// 输出：[[0,1,2,3],[0,3]]
+
+// 提示：
 //     n == graph.length
 //     2 <= n <= 15
 //     0 <= graph[i][j] < n
-//     graph[i][j] != i (i.e., there will be no self-loops).
-//     All the elements of graph[i] are unique.
-//     The input graph is guaranteed to be a DAG.
+//     graph[i][j] != i 
+//     保证输入为有向无环图 (GAD)
 
 import "fmt"
 
