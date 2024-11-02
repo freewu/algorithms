286b8759a54935535fd7b48bb8d45c8b7b67a745
bluefrog <bluefrog.wu@gmail.com>
2024-11-03 01:35:25 +0800

    leetcode 1723. Find Minimum Time to Finish All Jobs

--- leetcode/1723-FindMinimumTimeToFinishAllJobs.go
+++ leetcode/1723-FindMinimumTimeToFinishAllJobs.go
@@ -29,6 +29,7 @@ package main
 
 import "fmt"
 import "sort"
+import "math/bits"
 
 // backtracking
 func minimumTimeRequired(jobs []int, k int) int {
@@ -55,6 +56,37 @@ func minimumTimeRequired(jobs []int, k int) int {
     return res
 }
 
+// dp
+func minimumTimeRequired1(jobs []int, k int) int {
+    n, inf := len(jobs), 1 << 31
+    // dp(S, k) = min( dp(S, k), dp(S|sub, k-1)+sum(sub) )
+    sum := make([]int, (1 << n))
+    for i := 1; i < (1 << n); i++ {
+        x := bits.TrailingZeros(uint(i))
+        y := i ^ (1 << x)
+        sum[i] = sum[y] + jobs[x]
+    }
+    dp := make([][]int, k)
+    for i := range dp {
+        dp[i] = make([]int, (1 << n))
+    }
+    for i, s := range sum {
+        dp[0][i] = s
+    }
+    min := func (x, y int) int { if x < y { return x; }; return y; }
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    for i := 1; i < k; i++ {
+        for j := 0; j < (1 << n); j++ {
+            tem := inf
+            for x := j; x > 0; x = (x - 1) & j {
+                tem = min(tem, max(dp[i-1][j-x], sum[x]))
+            }
+            dp[i][j] = tem
+        }
+    }
+    return dp[k-1][(1 << n)-1]
+}
+
 func main() {
     // Example 1:
     // Input: jobs = [3,2,3], k = 3
@@ -69,4 +101,7 @@ func main() {
     // Worker 2: 4, 7 (working time = 4 + 7 = 11)
     // The maximum working time is 11.
     fmt.Println(minimumTimeRequired([]int{1,2,4,7,8}, 2)) // 11
+
+    fmt.Println(minimumTimeRequired1([]int{3,2,3}, 3)) // 3
+    fmt.Println(minimumTimeRequired1([]int{1,2,4,7,8}, 2)) // 11
 }
