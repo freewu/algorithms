12715b3533eb9b604628ad0c1fe6a5174713d98c
bluefrog <bluefrog.wu@gmail.com>
2025-01-24 10:19:11 +0800

    leetcode 802. Find Eventual Safe States

--- leetcode/802-FindEventualSafeStates.go
+++ leetcode/802-FindEventualSafeStates.go
@@ -59,17 +59,17 @@ func eventualSafeNodes(graph [][]int) []int {
 // 反向拓扑排序 bfs
 func eventualSafeNodes1(graph [][]int) []int {
     // 存在环的时候，不能完成所有课程
-    numCourses := len(graph)
-    res, outdegree, indegree := []int{}, make([]int, numCourses), make([][]int, numCourses)
+    n := len(graph)
+    res, outdegree, indegree := []int{}, make([]int, n), make([][]int, n)
     for from, toList := range graph {
         outdegree[from] += len(toList)
         for _, to := range toList {
             indegree[to] = append(indegree[to], from) // 所有的入度
         }
     }
-    queue := []int(nil)
-    for i, out := range outdegree { // 不指向任何节点的， 可以入队列
-        if out == 0 { // 终端节点
+    queue := []int{}
+    for i, v := range outdegree { // 不指向任何节点的， 可以入队列
+        if v == 0 { // 终端节点
             queue = append(queue, i)
         }
     }
@@ -83,8 +83,33 @@ func eventualSafeNodes1(graph [][]int) []int {
             }
         } 
     }
-    for i, out := range outdegree {
-        if out == 0 { // 终端节点
+    for i, v := range outdegree {
+        if v == 0 { // 终端节点
+            res = append(res, i)
+        }
+    }
+    return res
+}
+
+func eventualSafeNodes2(graph [][]int) []int {
+    n := len(graph)
+    res, nodes, visited := make([]int, 0, n), make([]bool, n), make([]bool, n)
+    var dfs func(node int) bool
+    dfs = func(node int) bool {
+        if len(graph[node]) == 0 { return true }
+        if visited[node] { return nodes[node] }
+        visited[node] = true
+        for _, next := range graph[node] {
+            if !dfs(next) {
+                nodes[node] = false
+                return false
+            }
+        }
+        nodes[node] = true
+        return true
+    }
+    for i := 0; i < n; i++ {
+        if dfs(i) {
             res = append(res, i)
         }
     }
@@ -98,16 +123,17 @@ func main() {
     // Explanation: The given graph is shown above.
     // Nodes 5 and 6 are terminal nodes as there are no outgoing edges from either of them.
     // Every path starting at nodes 2, 4, 5, and 6 all lead to either node 5 or 6.
-    graph1 := [][]int{{1,2},{2,3},{5},{0},{5},{},{}}
-    fmt.Println(eventualSafeNodes(graph1)) // [2,4,5,6]
+    fmt.Println(eventualSafeNodes([][]int{{1,2},{2,3},{5},{0},{5},{},{}})) // [2,4,5,6]
     // Example 2:
     // Input: graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]
     // Output: [4]
     // Explanation:
     // Only node 4 is a terminal node, and every path starting at node 4 leads to node 4.
-    graph2 := [][]int{{1,2,3,4},{1,2},{3,4},{0,4},{}}
-    fmt.Println(eventualSafeNodes(graph2)) // [4]
+    fmt.Println(eventualSafeNodes([][]int{{1,2,3,4},{1,2},{3,4},{0,4},{}})) // [4]
 
-    fmt.Println(eventualSafeNodes1(graph1)) // [2,4,5,6]
-    fmt.Println(eventualSafeNodes1(graph2)) // [4]
+    fmt.Println(eventualSafeNodes1([][]int{{1,2},{2,3},{5},{0},{5},{},{}})) // [2,4,5,6]
+    fmt.Println(eventualSafeNodes1([][]int{{1,2,3,4},{1,2},{3,4},{0,4},{}})) // [4]
+
+    fmt.Println(eventualSafeNodes2([][]int{{1,2},{2,3},{5},{0},{5},{},{}})) // [2,4,5,6]
+    fmt.Println(eventualSafeNodes2([][]int{{1,2,3,4},{1,2},{3,4},{0,4},{}})) // [4]
 }
