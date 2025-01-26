4716193084af54c997c3480f1e1809da23b6e970
bluefrog <bluefrog.wu@gmail.com>
2025-01-26 22:46:11 +0800

    leetcode 3422. Minimum Operations to Make Subarray Elements Equal

--- leetcode/3422-MinimumOperationsToMakeSubarrayElementsEqual.go
+++ leetcode/3422-MinimumOperationsToMakeSubarrayElementsEqual.go
@@ -28,9 +28,11 @@ package main
 
 import "fmt"
 import "sort"
+import "container/heap"
+import "math"
 
 // 超出时间限制 763 / 773
-func minOperations(nums []int, k int) int64 {
+func minOperations1(nums []int, k int) int64 {
     n := len(nums)
     if n < k {
         return 0
@@ -69,7 +71,7 @@ func minOperations(nums []int, k int) int64 {
             return ops2
         }
     }
-    minOps := computeMinOps(sorted, prefix)
+    res := computeMinOps(sorted, prefix)
     for i := k; i < n; i++ {
         // 移除左边的元素
         leftVal := nums[i-k]
@@ -89,11 +91,147 @@ func minOperations(nums []int, k int) int64 {
         }
         // 计算当前的操作次数
         currentOps := computeMinOps(sorted, prefix)
-        if currentOps < minOps {
-            minOps = currentOps
+        if currentOps < res {
+            res = currentOps
         }
     }
-    return int64(minOps)
+    return int64(res)
+}
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *MaxHeap) Pop() interface{} {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
+}
+
+type MinHeap []int
+
+func (h MinHeap) Len() int            { return len(h) }
+func (h MinHeap) Less(i, j int) bool  { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int))}
+func (h *MinHeap) Pop() interface{} {
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
+}
+
+type MedianMaintainer struct {
+    lower      *MaxHeap
+    higher     *MinHeap
+    lowerSize  int
+    higherSize int
+    lowerSum   int64
+    higherSum  int64
+    removes    map[int]int
+}
+
+func (m *MedianMaintainer) addNum(num int) {
+    if m.lower.Len() == 0 || num <= (*m.lower)[0] {
+        heap.Push(m.lower, num)
+        m.lowerSize++
+        m.lowerSum += int64(num)
+    } else {
+        heap.Push(m.higher, num)
+        m.higherSize++
+        m.higherSum += int64(num)
+    }
+    m.adjustPriorityQueues()
+}
+
+func (m *MedianMaintainer) removeNum(num int) {
+    m.removes[num]++
+    if m.lower.Len() > 0 && num <= (*m.lower)[0] {
+        m.lowerSize--
+        m.lowerSum -= int64(num)
+    } else {
+        m.higherSize--
+        m.higherSum -= int64(num)
+    }
+    m.adjustPriorityQueues()
+}
+
+func (m *MedianMaintainer) adjustPriorityQueues() {
+    for m.lowerSize > m.higherSize+1 {
+        num := heap.Pop(m.lower).(int)
+        m.lowerSize--
+        m.lowerSum -= int64(num)
+        heap.Push(m.higher, num)
+        m.higherSize++
+        m.higherSum += int64(num)
+    }
+    for m.higherSize > m.lowerSize {
+        num := heap.Pop(m.higher).(int)
+        m.higherSize--
+        m.higherSum -= int64(num)
+        heap.Push(m.lower, num)
+        m.lowerSize++
+        m.lowerSum += int64(num)
+    }
+    for m.lower.Len() > 0 {
+        top := (*m.lower)[0]
+        if cnt, ok := m.removes[top]; ok && cnt > 0 {
+            heap.Pop(m.lower)
+            m.removes[top]--
+            if m.removes[top] == 0 {
+                delete(m.removes, top)
+            }
+        } else {
+            break
+        }
+    }
+    for m.higher.Len() > 0 {
+        top := (*m.higher)[0]
+        if cnt, ok := m.removes[top]; ok && cnt > 0 {
+            heap.Pop(m.higher)
+            m.removes[top]--
+            if m.removes[top] == 0 {
+                delete(m.removes, top)
+            }
+        } else {
+            break
+        }
+    }
+}
+
+func (m *MedianMaintainer) findMedian() int {
+    return (*m.lower)[0]
+}
+
+func minOperations(nums []int, k int) int64 {
+    mm := &MedianMaintainer{
+        lower:   &MaxHeap{},
+        higher:  &MinHeap{},
+        removes: make(map[int]int),
+    }
+    heap.Init(mm.lower)
+    heap.Init(mm.higher)
+    res, n := int64(math.MaxInt64), len(nums)
+    for i := 0; i < n; i++ {
+        if i >= k {
+            mm.removeNum(nums[i-k])
+        }
+        mm.addNum(nums[i])
+        if i >= k-1 {
+            median := mm.findMedian()
+            operations := mm.higherSum - mm.lowerSum + int64(median)*int64(mm.lowerSize-mm.higherSize)
+            if operations < res {
+                res = operations
+            }
+        }
+    }
+    if res == math.MaxInt64 { return 0 }
+    return res
 }
 
 func main() {
@@ -114,4 +252,9 @@ func main() {
 
     fmt.Println(minOperations([]int{1,2,3,4,5,6,7,8,9}, 2)) // 1
     fmt.Println(minOperations([]int{9,8,7,6,5,4,3,2,1}, 2)) // 1
+
+    fmt.Println(minOperations1([]int{4,-3,2,1,-4,6}, 3)) // 5
+    fmt.Println(minOperations1([]int{-2,-2,3,1,4}, 2)) // 0
+    fmt.Println(minOperations1([]int{1,2,3,4,5,6,7,8,9}, 2)) // 1
+    fmt.Println(minOperations1([]int{9,8,7,6,5,4,3,2,1}, 2)) // 1
 }
