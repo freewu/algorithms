12e4a4cffb11d9a33d3d4b0a00b2a8b74b12d894
bluefrog <bluefrog.wu@gmail.com>
2025-03-08 00:20:37 +0800

    leetcode 2234. Maximum Total Beauty of the Gardens

--- leetcode/2234-MaximumTotalBeautyOfTheGardens.go
+++ leetcode/2234-MaximumTotalBeautyOfTheGardens.go
@@ -119,6 +119,47 @@ func maximumBeauty1(flowers []int, newFlowers int64, target, full, partial int) 
     return int64(res)
 }
 
+func maximumBeauty2(flowers []int, newFlowers int64, target, full, partial int) int64 {
+    n := len(flowers)
+    min := func (x, y int) int { if x < y { return x; }; return y; }
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    // 如果全部种满，还剩下多少朵花？
+    leftFlowers := int(newFlowers) - target*n // 先减掉
+    for i, flower := range flowers {
+        flowers[i] = min(flower, target)
+        leftFlowers += flowers[i] // 把已有的加回来
+    }
+    // 没有种花，所有花园都已种满
+    if leftFlowers == int(newFlowers) {
+        return int64(n * full) // 答案只能是 n*full（注意不能减少花的数量）
+    }
+    // 可以全部种满
+    if leftFlowers >= 0 {
+        // 两种策略取最大值：留一个花园种 target-1 朵花，其余种满；或者，全部种满
+        return int64(max((target - 1) * partial + (n - 1) * full, n * full))
+    }
+    sort.Ints(flowers) // 时间复杂度的瓶颈在这，尽量写在后面
+    res, prefix, j := 0, 0, 0
+    // 枚举 i，表示后缀 [i, n-1] 种满（i=0 的情况上面已讨论）
+    for i := 1; i <= n; i++ {
+        // 撤销，flowers[i-1] 不变成 target
+        leftFlowers += target - flowers[i-1]
+        if leftFlowers < 0 { // 花不能为负数，需要继续撤销
+            continue
+        }
+        // 满足以下条件说明 [0, j] 都可以种 flowers[j] 朵花
+        for j < i && flowers[j]*j <= prefix + leftFlowers {
+            prefix += flowers[j]
+            j++
+        }
+        // 计算总美丽值
+        // 在前缀 [0, j-1] 中均匀种花，这样最小值最大
+        avg := (leftFlowers + prefix) / j // 由于上面特判了，这里 avg 一定小于 target
+        res = max(res, avg * partial + (n - i) * full)
+    }
+    return int64(res)
+}
+
 func main() {
     // Example 1:
     // Input: flowers = [1,3,1,1], newFlowers = 7, target = 6, full = 12, partial = 1
@@ -152,4 +193,7 @@ func main() {
 
     fmt.Println(maximumBeauty1([]int{1,3,1,1}, 7, 6, 12, 1)) // 14
     fmt.Println(maximumBeauty1([]int{2,4,5,3}, 10, 5, 2, 6)) // 30
+
+    fmt.Println(maximumBeauty2([]int{1,3,1,1}, 7, 6, 12, 1)) // 14
+    fmt.Println(maximumBeauty2([]int{2,4,5,3}, 10, 5, 2, 6)) // 30
 }
