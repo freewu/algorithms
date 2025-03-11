d72743162109ecb1bb3266488c0d0f76ea1b06b8
bluefrog <bluefrog.wu@gmail.com>
2025-03-11 09:24:15 +0800

    leetcode 159. Longest Substring with At Most Two Distinct Characters

--- leetcode/159-LongestSubstringWithAtMostTwoDistinctCharacters.go
+++ leetcode/159-LongestSubstringWithAtMostTwoDistinctCharacters.go
@@ -14,74 +14,72 @@ package main
 // Explanation: The substring is "aabbb" which its length is 5.
  
 // Constraints:
-// 	1 <= s.length <= 105
-// 	s consists of English letters.
+//     1 <= s.length <= 10^5
+//     s consists of English letters.
 
 import "fmt"
 
 func lengthOfLongestSubstringTwoDistinct(s string) int {
-	m := make(map[byte]int,58)
-	start, end := 0, 0
-	counter, l := 0, 0
-
-	max := func (a, b int) int {
-		if a <= b {
-			return b
-		}
-		return a
-	}
-
-	for end < len(s) {
-		if _, ok := m[s[end]]; ok {
-			m[s[end]] += 1
-		} else {
-			m[s[end]] = 1
-		}
-		if m[s[end]] == 1 {
-			counter += 1 // 出现新的字符了
-		}
-		for counter > 2  {
-			m[s[start]] -= 1
-			if m[s[start]] == 0 {
-				counter -= 1
-			}
-			start += 1
-		}
-		l = max(end - start + 1, l)
-		end += 1
-	} 
-	return l
+    mp := make(map[byte]int,58)
+    res, count, start, end := 0, 0, 0, 0
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    for end < len(s) {
+        mp[s[end]]++
+        if mp[s[end]] == 1 {
+            count++ // 出现新的字符了
+        }
+        for count > 2  {
+            mp[s[start]]--
+            if mp[s[start]] == 0 {
+                count--
+            }
+            start++
+        }
+        res = max(res, end - start + 1)
+        end++
+    } 
+    return res
 }
 
-const CHARSET int = 58
 // best solution
 func lengthOfLongestSubstringTwoDistinct1(s string) int {
-	n := len(s)
-	cnts := make([]int, CHARSET)
-	diffCnt := 0
-	i, j := 0, 0
-	ans := 0
-	for ; j < n; j++ {
-		if cnts[s[j]-'A'] == 0 {
-			diffCnt++
-		}
-		cnts[s[j]-'A']++
-		for diffCnt > 2 {
-			if cnts[s[i]-'A'] == 1 {
-				diffCnt--
-			}
-			cnts[s[i]-'A']--
-			i++
-		}
-		ans = max(ans, j-i+1)
-	}
-	return ans
+    res, diff, i, j, n := 0, 0, 0, 0, len(s)
+    count := make([]int, 58)
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    for ; j < n; j++ {
+        if count[s[j] - 'A'] == 0 {
+            diff++
+        }
+        count[s[j] - 'A']++
+        for diff > 2 {
+            if count[s[i] - 'A'] == 1 {
+                diff--
+            }
+            count[s[i] - 'A']--
+            i++
+        }
+        res = max(res, j - i + 1)
+    }
+    return res
 }
 
 func main() {
-	fmt.Println(lengthOfLongestSubstringTwoDistinct("eceba")) // 3
-	fmt.Println(lengthOfLongestSubstringTwoDistinct("ccaabbb")) // 5
+    // Example 1:
+    // Input: s = "eceba"
+    // Output: 3
+    // Explanation: The substring is "ece" which its length is 3.
+    fmt.Println(lengthOfLongestSubstringTwoDistinct("eceba")) // 3
+    // Example 2:
+    // Input: s = "ccaabbb"
+    // Output: 5
+    // Explanation: The substring is "aabbb" which its length is 5.
+    fmt.Println(lengthOfLongestSubstringTwoDistinct("ccaabbb")) // 5
 
-	fmt.Println(lengthOfLongestSubstringTwoDistinct1("eceba")) // 3
-	fmt.Println(lengthOfLongestSubstringTwoDistinct1("ccaabbb")) // 5
+    fmt.Println(lengthOfLongestSubstringTwoDistinct("bluefrog")) // 2
+    fmt.Println(lengthOfLongestSubstringTwoDistinct("leetcode")) // 3
+
+    fmt.Println(lengthOfLongestSubstringTwoDistinct1("eceba")) // 3
+    fmt.Println(lengthOfLongestSubstringTwoDistinct1("ccaabbb")) // 5
+    fmt.Println(lengthOfLongestSubstringTwoDistinct1("bluefrog")) // 2
+    fmt.Println(lengthOfLongestSubstringTwoDistinct1("leetcode")) // 3
 }
