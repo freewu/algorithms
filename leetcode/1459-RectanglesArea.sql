e8aae02d59bd98e52d73852ff40d1abc18a540e6
bluefrog <bluefrog.wu@gmail.com>
2025-03-17 00:27:49 +0800

    leetcode 1459. Rectangles Area

--- leetcode/1459-RectanglesArea.sql
+++ leetcode/1459-RectanglesArea.sql
@@ -42,6 +42,12 @@
 -- The rectangle formed by p1 = 1 and p2 = 2 has an area equal to |2-4| * |7-8| = 2.
 -- Note that the rectangle formed by p1 = 1 and p2 = 3 is invalid because the area is 0.
 
+-- Create table If Not Exists Points (id int, x_value int, y_value int)
+-- Truncate table Points
+-- insert into Points (id, x_value, y_value) values ('1', '2', '7')
+-- insert into Points (id, x_value, y_value) values ('2', '4', '8')
+-- insert into Points (id, x_value, y_value) values ('3', '2', '10')
+
 -- Write your MySQL query statement below
 SELECT 
     po1.id AS P1,
