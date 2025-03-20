2590f952d313583271b14e31480fc97691ce6594
bluefrog <bluefrog.wu@gmail.com>
2025-03-20 14:01:36 +0800

    leetcode 2691. Immutability Helper

--- leetcode/2691-ImmutabilityHelper.js
+++ leetcode/2691-ImmutabilityHelper.js
@@ -81,33 +81,90 @@
 //         mutators is an array of functions
 //         total calls to produce() < 10^5
 
+const isObj = o => typeof o === 'object' && o !== null;
+
 var ImmutableHelper = function(obj) {
     this.obj = obj;
 };
 
-/** 
- * @param {Function} mutator
- * @return {JSON} clone of obj
- */
-ImmutableHelper.prototype.produce = function(mutator) {
-    var res = {};
-    mutator(createProxy(this.obj, res));
-    return res;
-};
+const setMapConstructor = () => {
+    return {
+        hasValue: false,
+        value: null,
+        map: new Map(),
+    }
+}
 
-var createProxy = function(obj,res) {
-    return new Proxy(obj, {
-        get(x, prop) {
-            if (typeof obj[prop] === 'object' && obj[prop] !== null) {
-                res[prop] = res[prop]? res[prop] : {};
-                return createProxy(obj[prop], res[prop]);
+
+ImmutableHelper.prototype.produce = function(mutator) {
+    function createProxy(obj, setMap) {
+        return new Proxy(obj, {
+              set(_, prop, value) {
+                  if (!setMap.map.has(prop)) {
+                      setMap.map.set(prop, setMapConstructor());
+                  }
+                  setMap.map.get(prop).hasValue = true;
+                  setMap.map.get(prop).value = value;
+              },
+              get(_, prop, value) {
+                  if (setMap.hasValue) {
+                      return setMap.value;
+                  }
+
+                  if (!setMap.map.has(prop)) {
+                      setMap.map.set(prop, setMapConstructor());
+                  }
+                  if (setMap.map.get(prop).hasValue) {
+                      return setMap.map.get(prop).value;
+                  }
+                  if (isObj(obj[prop])) {
+                      return createProxy(obj[prop], setMap.map.get(prop));
+                  }
+                  return obj[prop];
+              },
+        });
+    }
+    
+    function simplify(setMap) {
+        if (setMap.hasValue) return true
+        let hasMut = false;
+        for (const [key, value] of [...setMap.map]) {
+            if (!simplify(value)) {
+                setMap.map.delete(key)
+            } else {
+                hasMut = true
             }
-            return res[prop]?  res[prop] : x[prop];
-        },
-        set(x, prop, value) {
-            res[prop] = value;
         }
-    });
+        return hasMut;
+    }
+    
+    function transform(obj, setMap) {
+        if (setMap.hasValue) {
+            return setMap.value
+        }
+        if (setMap.map.size === 0) return obj;
+        let clone;
+        if (isObj(obj)) {
+            if (Array.isArray(obj)) {
+                clone = [...obj]
+            } else {
+                clone = {...obj};
+            }
+        } else {
+            return obj;
+        }
+        for (const [key, value] of [...setMap.map]) {
+            clone[key] = transform(obj[key], value);
+        }
+        return clone
+    }
+    
+    const setMap = setMapConstructor();
+    const proxy = createProxy(this.obj, setMap);
+   
+    mutator(proxy);
+    simplify(setMap)
+    return transform(this.obj, setMap);
 };
 
 /**
