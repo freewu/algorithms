fa98acedaf81016983c4b3942247fec30fcdaa18
bluefrog <bluefrog.wu@gmail.com>
2025-05-03 09:10:50 +0800

    leetcode 1007. Minimum Domino Rotations For Equal Row

--- leetcode/1007-MinimumDominoRotationsForEqualRow.go
+++ leetcode/1007-MinimumDominoRotationsForEqualRow.go
@@ -54,6 +54,55 @@ func minDominoRotations(tops []int, bottoms []int) int {
     return res
 }
 
+func minDominoRotations1(tops []int, bottoms []int) int {
+    n, x, y := len(tops), tops[0], bottoms[0]
+    cnt_x1, cnt_x2, cnt_y1, cnt_y2 := 0, 0, 0, 0
+    // top = x
+    for i := 0; i < n; i++ {
+        if tops[i] == x { continue }
+        if bottoms[i] == x {
+            cnt_x1++
+        } else {
+            cnt_x1 = n + 1
+            break
+        }
+    }
+    for i := 0; i < n; i++ {
+        if bottoms[i] == x { continue }
+        if tops[i] == x {
+            cnt_x2++
+        } else {
+            cnt_x2 = n + 1
+            break
+        }
+    }
+    min := func (x, y int) int { if x < y { return x; }; return y; }
+    res1 := min(cnt_x1, cnt_x2)
+    for i := 0; i < n; i++ {
+        if tops[i] == y { continue }
+        if bottoms[i] == y {
+            cnt_y1++
+        } else {
+            cnt_y1 = n + 1
+            break
+        }
+    }
+    for i := 0; i < n; i++ {
+        if bottoms[i] == y { continue }
+        if tops[i] == y {
+            cnt_y2 ++
+        } else {
+            cnt_y2 = n + 1
+            break
+        }
+    }
+    res2 := min(cnt_y1, cnt_y2)
+    if res1 == n + 1 && res2 == n + 1 {
+        return -1
+    }
+    return min(res1, res2)
+}
+
 func main() {
     // Example 1:
     // Input: tops = [2,1,2,4,2,2], bottoms = [5,2,6,2,3,2]
@@ -68,4 +117,16 @@ func main() {
     // Explanation: 
     // In this case, it is not possible to rotate the dominoes to make one row of values equal.
     fmt.Println(minDominoRotations([]int{3,5,1,2,3}, []int{3,6,3,3,4})) // -1
+
+    fmt.Println(minDominoRotations([]int{1,2,3,4,5,6}, []int{1,2,3,4,5,6})) // -1
+    fmt.Println(minDominoRotations([]int{1,2,3,4,5,6}, []int{6,5,4,3,2,1})) // -1
+    fmt.Println(minDominoRotations([]int{6,5,4,3,2,1}, []int{6,5,4,3,2,1})) // -1
+    fmt.Println(minDominoRotations([]int{6,5,4,3,2,1}, []int{1,2,3,4,5,6})) // -1
+
+    fmt.Println(minDominoRotations1([]int{2,1,2,4,2,2}, []int{5,2,6,2,3,2})) // 2
+    fmt.Println(minDominoRotations1([]int{3,5,1,2,3}, []int{3,6,3,3,4})) // -1
+    fmt.Println(minDominoRotations1([]int{1,2,3,4,5,6}, []int{1,2,3,4,5,6})) // -1
+    fmt.Println(minDominoRotations1([]int{1,2,3,4,5,6}, []int{6,5,4,3,2,1})) // -1
+    fmt.Println(minDominoRotations1([]int{6,5,4,3,2,1}, []int{6,5,4,3,2,1})) // -1
+    fmt.Println(minDominoRotations1([]int{6,5,4,3,2,1}, []int{1,2,3,4,5,6})) // -1
 }
