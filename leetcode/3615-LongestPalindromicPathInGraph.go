d32ff8df3914e093e7dff0bd889149ef490a1391
bluefrog <bluefrog.wu@gmail.com>
2025-07-15 22:36:29 +0800

    leetcode 3615. Longest Palindromic Path in Graph

--- leetcode/3615-LongestPalindromicPathInGraph.go
+++ leetcode/3615-LongestPalindromicPathInGraph.go
@@ -98,6 +98,72 @@ func maxLen(n int, edges [][]int, label string) int {
     return res
 }
 
+func maxLen1(n int, edges [][]int, label string) int {
+    res := 0
+    if len(edges) == n*(n-1)/2 { // 完全图
+        count := [26]int{}
+        for _, v := range label {
+            count[v - 'a']++
+        }
+        odd := 0
+        for _, c := range count {
+            res += c - c % 2
+            odd |= c % 2
+        }
+        return res + odd
+    }
+    g := make([][]int, n)
+    for _, e := range edges {
+        x, y := e[0], e[1]
+        g[x] = append(g[x], y)
+        g[y] = append(g[y], x)
+    }
+    memo := make([][][]int, n)
+    for i := range memo {
+        memo[i] = make([][]int, n)
+        for j := range memo[i] {
+            memo[i][j] = make([]int, 1<<n)
+            for p := range memo[i][j] {
+                memo[i][j][p] = -1
+            }
+        }
+    }
+    min := func (x, y int) int { if x < y { return x; }; return y; }
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    // 计算从 x 和 y 向两侧扩展，最多还能访问多少个节点（不算 x 和 y）
+    var dfs func(x, y, vis int)  int
+    dfs = func(x, y, vis int) int {
+        res, p := 0, &memo[x][y][vis]
+        if *p >= 0 { return *p } // 之前计算过
+        for _, v := range g[x] {
+            if vis >> v & 1 > 0 { continue } // v 在路径中
+            for _, w := range g[y] {
+                if vis>>w&1 == 0 && w != v && label[w] == label[v] {
+                    // 保证 v < w，减少状态个数和计算量
+                    r := dfs(min(v, w), max(v, w), vis|1<<v|1<<w)
+                    res = max(res, r+2)
+                }
+            }
+        }
+        *p = res // 记忆化
+        return res
+    }
+    for x, to := range g {
+        // 奇回文串，x 作为回文中心
+        res = max(res, dfs(x, x, 1<<x) + 1)
+        if res == n { return res }
+        // 偶回文串，x 和 x 的邻居 y 作为回文中心
+        for _, y := range to {
+            // 保证 x < y，减少状态个数和计算量
+            if x < y && label[x] == label[y] {
+                res = max(res, dfs(x, y, 1<<x|1<<y)+2)
+                if res == n { return res }
+            }
+        }
+    }
+    return res
+}
+
 func main() {
     // Example 1:
     // Input: n = 3, edges = [[0,1],[1,2]], label = "aba"
@@ -123,4 +189,8 @@ func main() {
     // The longest palindromic path is from node 0 to node 1, following the path 0 → 3 → 1, forming string "bcb".
     // This is a valid palindrome of length 3.
     fmt.Println(maxLen(4, [][]int{{0,2},{0,3},{3,1}}, "bbac")) // 3
+
+    fmt.Println(maxLen1(3, [][]int{{0,1},{1,2}}, "aba")) // 3
+    fmt.Println(maxLen1(3, [][]int{{0,1},{0,2}}, "abc")) // 1
+    fmt.Println(maxLen1(4, [][]int{{0,2},{0,3},{3,1}}, "bbac")) // 3
 }
