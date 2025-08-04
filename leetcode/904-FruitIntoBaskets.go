81a14c24f92e80adc18cfc988c2b4b5865b4cbfb
bluefrog <bluefrog.wu@gmail.com>
2025-08-04 08:31:30 +0800

    leetcode 904. Fruit Into Baskets

--- leetcode/904-FruitIntoBaskets.go
+++ leetcode/904-FruitIntoBaskets.go
@@ -98,10 +98,14 @@ func main() {
     // If we had started at the first tree, we would only pick from trees [1,2].
     fmt.Println(totalFruit([]int{1,2,3,2,2})) // 4 {2,3,2,2}
 
-    fmt.Println(totalFruit([]int{3,3,3,1,2,1,1,2,3,3,4})) // 5 {1,2,1,1,2}
+    fmt.Println(totalFruit([]int{3,3,3,1,2,1,1,2,3,3,4})) // 5
+    fmt.Println(totalFruit([]int{0,1,2,3,4,5,6,7,8,9})) // 2
+    fmt.Println(totalFruit([]int{9,8,7,6,5,4,3,2,1,0})) // 2
 
     fmt.Println(totalFruit1([]int{1,2,1})) // 3 {1,2,1}
     fmt.Println(totalFruit1([]int{0,1,2,2})) // 3 {1,2,2}
     fmt.Println(totalFruit1([]int{1,2,3,2,2})) // 4 {2,3,2,2}
-    fmt.Println(totalFruit1([]int{3,3,3,1,2,1,1,2,3,3,4})) // 5 {1,2,1,1,2}
+    fmt.Println(totalFruit1([]int{3,3,3,1,2,1,1,2,3,3,4})) // 5 
+    fmt.Println(totalFruit1([]int{0,1,2,3,4,5,6,7,8,9})) // 2
+    fmt.Println(totalFruit1([]int{9,8,7,6,5,4,3,2,1,0})) // 2
 }
