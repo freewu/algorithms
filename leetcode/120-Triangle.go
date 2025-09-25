7b769bc0b2d4e34212ba44e702b1f4c47b5d249d
bluefrog <bluefrog.wu@gmail.com>
2025-09-25 08:27:42 +0800

    leetcode 120. Triangle

--- leetcode/120-Triangle.go
+++ leetcode/120-Triangle.go
@@ -92,6 +92,9 @@ func minimumTotal2(triangle [][]int) int {
 }
 
 func main() {
+    // Example 1:
+    // Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+    // Output: 11
     // Explanation: The triangle looks like:
     //    2
     //   3 4
@@ -99,6 +102,9 @@ func main() {
     // 4 1 8 3
     // The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
     fmt.Printf("minimumTotal([][]int{{2}, {3,4},{6,5,7},{4,1,8,3}}) = %v\n",minimumTotal([][]int{{2}, {3,4},{6,5,7},{4,1,8,3}})) // 11
+    // Example 2:
+    // Input: triangle = [[-10]]
+    // Output: -10
     fmt.Printf("minimumTotal([][]int{{-10}}) = %v\n",minimumTotal([][]int{{-10}})) // -10
 
     fmt.Printf("minimumTotal1([][]int{{2}, {3,4},{6,5,7},{4,1,8,3}}) = %v\n",minimumTotal1([][]int{{2}, {3,4},{6,5,7},{4,1,8,3}})) // 11
