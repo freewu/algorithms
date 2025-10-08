bb7331fd86037e8aec021aa7e9cdfdaf600ef1f1
bluefrog <bluefrog.wu@gmail.com>
2025-10-08 10:27:55 +0800

    leetcode  2300. Successful Pairs of Spells and Potions

--- leetcode/2300-SuccessfulPairsOfSpellsAndPotions.go
+++ leetcode/2300-SuccessfulPairsOfSpellsAndPotions.go
@@ -102,6 +102,22 @@ func main() {
     // Thus, [2,0,2] is returned.
     fmt.Println(successfulPairs([]int{3,1,2}, []int{8,5,8}, 17)) // [2,0,2]
 
+    fmt.Println(successfulPairs([]int{1,2,3,4,5,6,7,8,9}, []int{1,2,3,4,5,6,7,8,9}, 17)) // [0 1 4 5 6 7 7 7 8]
+    fmt.Println(successfulPairs([]int{1,2,3,4,5,6,7,8,9}, []int{9,8,7,6,5,4,3,2,1}, 17)) // [0 1 4 5 6 7 7 7 8]
+    fmt.Println(successfulPairs([]int{9,8,7,6,5,4,3,2,1}, []int{1,2,3,4,5,6,7,8,9}, 17)) // [8 7 7 7 6 5 4 1 0]
+    fmt.Println(successfulPairs([]int{9,8,7,6,5,4,3,2,1}, []int{9,8,7,6,5,4,3,2,1}, 17)) // [8 7 7 7 6 5 4 1 0]
+
     fmt.Println(successfulPairs1([]int{5,1,3}, []int{1,2,3,4,5}, 7)) // [4,0,3]
     fmt.Println(successfulPairs1([]int{3,1,2}, []int{8,5,8}, 17)) // [2,0,2]
+    fmt.Println(successfulPairs1([]int{1,2,3,4,5,6,7,8,9}, []int{1,2,3,4,5,6,7,8,9}, 17)) // [0 1 4 5 6 7 7 7 8]
+    fmt.Println(successfulPairs1([]int{1,2,3,4,5,6,7,8,9}, []int{9,8,7,6,5,4,3,2,1}, 17)) // [0 1 4 5 6 7 7 7 8]
+    fmt.Println(successfulPairs1([]int{9,8,7,6,5,4,3,2,1}, []int{1,2,3,4,5,6,7,8,9}, 17)) // [8 7 7 7 6 5 4 1 0]
+    fmt.Println(successfulPairs1([]int{9,8,7,6,5,4,3,2,1}, []int{9,8,7,6,5,4,3,2,1}, 17)) // [8 7 7 7 6 5 4 1 0]
+
+    fmt.Println(successfulPairs2([]int{5,1,3}, []int{1,2,3,4,5}, 7)) // [4,0,3]
+    fmt.Println(successfulPairs2([]int{3,1,2}, []int{8,5,8}, 17)) // [2,0,2]
+    fmt.Println(successfulPairs2([]int{1,2,3,4,5,6,7,8,9}, []int{1,2,3,4,5,6,7,8,9}, 17)) // [0 1 4 5 6 7 7 7 8]
+    fmt.Println(successfulPairs2([]int{1,2,3,4,5,6,7,8,9}, []int{9,8,7,6,5,4,3,2,1}, 17)) // [0 1 4 5 6 7 7 7 8]
+    fmt.Println(successfulPairs2([]int{9,8,7,6,5,4,3,2,1}, []int{1,2,3,4,5,6,7,8,9}, 17)) // [8 7 7 7 6 5 4 1 0]
+    fmt.Println(successfulPairs2([]int{9,8,7,6,5,4,3,2,1}, []int{9,8,7,6,5,4,3,2,1}, 17)) // [8 7 7 7 6 5 4 1 0]
 }
