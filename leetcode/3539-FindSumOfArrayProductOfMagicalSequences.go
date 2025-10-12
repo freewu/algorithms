9feef2715b0bba0f63a2c576d652d96d65060ced
bluefrog <bluefrog.wu@gmail.com>
2025-10-12 17:33:24 +0800

    leetcode 3539. Find Sum of Array Product of Magical Sequences

--- leetcode/3539-FindSumOfArrayProductOfMagicalSequences.go
+++ leetcode/3539-FindSumOfArrayProductOfMagicalSequences.go
@@ -40,7 +40,7 @@ package main
 //     1 <= nums[i] <= 10^8
 
 import "fmt"
-// import "math/bits"
+import "math/bits"
 
 // func magicalSum(m int, k int, nums []int) int {
 //     n, mod := len(nums), 1_000_000_007
@@ -203,6 +203,89 @@ func magicalSum(m int, k int, nums []int) int {
     return res
 }
 
+
+const mod = 1_000_000_007
+const mx = 31
+
+var fac [mx]int  // fac[i] = i!
+var invF [mx]int // invF[i] = i!^-1
+
+func init() {
+    fac[0] = 1
+    for i := 1; i < mx; i++ {
+        fac[i] = fac[i-1] * i % mod
+    }
+
+    invF[mx-1] = pow(fac[mx-1], mod-2)
+    for i := mx - 1; i > 0; i-- {
+        invF[i-1] = invF[i] * i % mod
+    }
+}
+
+func pow(x, n int) int {
+    res := 1
+    for ; n > 0; n /= 2 {
+        if n%2 > 0 {
+            res = res * x % mod
+        }
+        x = x * x % mod
+    }
+    return res
+}
+
+func magicalSum1(m, k int, nums []int) int {
+    n := len(nums)
+    powV := make([][]int, n)
+    for i, v := range nums {
+        powV[i] = make([]int, m+1)
+        powV[i][0] = 1
+        for j := 1; j <= m; j++ {
+            powV[i][j] = powV[i][j-1] * v % mod
+        }
+    }
+
+    memo := make([][][][]int, n)
+    for i := range memo {
+        memo[i] = make([][][]int, m+1)
+        for j := range memo[i] {
+            memo[i][j] = make([][]int, m/2+1)
+            for p := range memo[i][j] {
+                memo[i][j][p] = make([]int, k+1)
+                for q := range memo[i][j][p] {
+                    memo[i][j][p][q] = -1
+                }
+            }
+        }
+    }
+    var dfs func(int, int, int, int) int
+    dfs = func(i, leftM, x, leftK int) (res int) {
+        c1 := bits.OnesCount(uint(x))
+        if c1+leftM < leftK { // 可行性剪枝
+            return
+        }
+        if i == n || leftM == 0 || leftK == 0 { // 无法继续选数字
+            if leftM == 0 && c1 == leftK {
+                return 1
+            }
+            return
+        }
+        p := &memo[i][leftM][x][leftK]
+        if *p != -1 {
+            return *p
+        }
+        for j := range leftM + 1 { // 枚举 I 中有 j 个下标 i
+            // 这 j 个下标 i 对 S 的贡献是 j * pow(2, i)
+            // 由于 x = S >> i，转化成对 x 的贡献是 j
+            bit := (x + j) & 1 // 取最低位，提前从 leftK 中减去，其余进位到 x 中
+            r := dfs(i+1, leftM-j, (x+j)>>1, leftK-bit)
+            res = (res + r*powV[i][j]%mod*invF[j]) % mod
+        }
+        *p = res
+        return
+    }
+    return dfs(0, m, 0, k) * fac[m] % mod
+}
+
 func main() {
     // Example 1:
     // Input: m = 5, k = 5, nums = [1,10,100,10000,1000000]
@@ -226,5 +309,12 @@ func main() {
     fmt.Println(magicalSum(2, 1, []int{63})) // 3969
     fmt.Println(magicalSum(2, 2, []int{9,8,7,6,5,4,3,2,1})) // 1740
     fmt.Println(magicalSum(2, 2, []int{1,2,3,4,5,6,7,8,9})) // 1740
+
+    fmt.Println(magicalSum1(5, 5, []int{1,10,100,10000,1000000})) // 991600007
+    fmt.Println(magicalSum1(2, 2, []int{5,4,3,2,1})) // 170
+    fmt.Println(magicalSum1(1, 1, []int{28})) // 28
+    fmt.Println(magicalSum1(2, 1, []int{63})) // 3969
+    fmt.Println(magicalSum1(2, 2, []int{9,8,7,6,5,4,3,2,1})) // 1740
+    fmt.Println(magicalSum1(2, 2, []int{1,2,3,4,5,6,7,8,9})) // 1740
 }
 
