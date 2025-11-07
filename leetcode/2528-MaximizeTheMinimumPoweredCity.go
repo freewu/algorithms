b5595043b033049e24b50a77a182d23235127d49
bluefrog <bluefrog.wu@gmail.com>
2025-11-07 08:23:34 +0800

    leetcode 2528. Maximize the Minimum Powered City

--- leetcode/2528-MaximizeTheMinimumPoweredCity.go
+++ leetcode/2528-MaximizeTheMinimumPoweredCity.go
@@ -158,6 +158,11 @@ func main() {
     // It can be proved that we cannot make the minimum power of a city greater than 4.
     fmt.Println(maxPower([]int{4,4,4,4}, 0, 3)) // 4
 
+    fmt.Println(maxPower([]int{1,2,3,4,5,6,7,8,9}, 1, 2)) // 5
+    fmt.Println(maxPower([]int{9,8,7,6,5,4,3,2,1}, 1, 2)) // 5
+
     fmt.Println(maxPower1([]int{1,2,4,5,0}, 1, 2)) // 5
     fmt.Println(maxPower1([]int{4,4,4,4}, 0, 3)) // 4
+    fmt.Println(maxPower1([]int{1,2,3,4,5,6,7,8,9}, 1, 2)) // 5
+    fmt.Println(maxPower1([]int{9,8,7,6,5,4,3,2,1}, 1, 2)) // 5
 }
