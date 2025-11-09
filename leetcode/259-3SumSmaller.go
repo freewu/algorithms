875c32ca91a93ad7a73ebb2f3603d8d41dcda539
bluefrog <bluefrog.wu@gmail.com>
2025-11-09 11:48:51 +0800

    leetcode 259. 3Sum Smaller

--- leetcode/259-3SumSmaller.go
+++ leetcode/259-3SumSmaller.go
@@ -49,6 +49,24 @@ func threeSumSmaller(nums []int, target int) int {
     return res
 }
 
+func threeSumSmaller1(nums []int, target int) int {
+    res, n := 0, len(nums)
+    if n < 3 { return res }
+    sort.Ints(nums)
+    for i := 0; i < n; i++ {
+        j, k := i + 1, n - 1
+        for j < k {
+            if nums[j] + nums[k] < target - nums[i] {
+                res += k - j
+                j++
+            } else {
+                k--
+            }
+        }
+    }
+    return res
+}
+
 func main() {
     // Example 1:
     // Input: nums = [-2,0,1,3], target = 2
@@ -65,4 +83,13 @@ func main() {
     // Input: nums = [0], target = 0
     // Output: 0
     fmt.Println(threeSumSmaller([]int{0}, 0)) // 0
+
+    fmt.Println(threeSumSmaller([]int{1,2,3,4,5,6,7,8,9}, 2)) // 0
+    fmt.Println(threeSumSmaller([]int{9,8,7,6,5,4,3,2,1}, 2)) // 0
+
+    fmt.Println(threeSumSmaller1([]int{-2,0,1,3}, 2)) // 2
+    fmt.Println(threeSumSmaller1([]int{ }, 0)) // 0
+    fmt.Println(threeSumSmaller1([]int{0}, 0)) // 0
+    fmt.Println(threeSumSmaller1([]int{1,2,3,4,5,6,7,8,9}, 2)) // 0
+    fmt.Println(threeSumSmaller1([]int{9,8,7,6,5,4,3,2,1}, 2)) // 0
 }
