829905693f4df214d703a49499ce79a5cde018cc
bluefrog <bluefrog.wu@gmail.com>
2025-11-14 08:20:24 +0800

    leetcode  2536. Increment Submatrices by One

--- leetcode/2536-IncrementSubmatricesByOne.go
+++ leetcode/2536-IncrementSubmatricesByOne.go
@@ -89,6 +89,9 @@ func main() {
     // - In the first query we add 1 to every element in the matrix.
     fmt.Println(rangeAddQueries(2, [][]int{{0,0,1,1}})) // [[1,1],[1,1]]
 
+    fmt.Println(rangeAddQueries(9, [][]int{{1,2,3,4,5,6,7,8,9}})) // [[0 0 0 0 0 0 0 0 0] [0 0 1 1 1 0 0 0 0] [0 0 1 1 1 0 0 0 0] [0 0 1 1 1 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0]]
+
     fmt.Println(rangeAddQueries1(3, [][]int{{1,1,2,2},{0,0,1,1}})) // [[1,1,0],[1,2,1],[0,1,1]]
     fmt.Println(rangeAddQueries1(2, [][]int{{0,0,1,1}})) // [[1,1],[1,1]]
+    fmt.Println(rangeAddQueries1(9, [][]int{{1,2,3,4,5,6,7,8,9}})) // [[0 0 0 0 0 0 0 0 0] [0 0 1 1 1 0 0 0 0] [0 0 1 1 1 0 0 0 0] [0 0 1 1 1 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0]]
 }
