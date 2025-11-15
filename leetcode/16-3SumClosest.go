e985c5d57d9760b5919ae7d16baca09641da8b35
bluefrog <bluefrog.wu@gmail.com>
2025-11-15 21:18:18 +0800

    leetcode 16. 3Sum Closest

--- leetcode/16-3SumClosest.go
+++ leetcode/16-3SumClosest.go
@@ -1,111 +1,114 @@
 package main
 
-/**
-16. 3Sum Closest
+// 16. 3Sum Closest
+// Given an array nums of n integers and an integer target,
+// find three integers in nums such that the sum is closest to target.
+// Return the sum of the three integers. You may assume that each input would have exactly one solution.
 
-Given an array nums of n integers and an integer target,
-find three integers in nums such that the sum is closest to target.
-Return the sum of the three integers. You may assume that each input would have exactly one solution.
+// Example 1:
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
-Example 1:
+// Example 2:
+// Input: nums = [0,0,0], target = 1
+// Output: 0
 
-	Input: nums = [-1,2,1,-4], target = 1
-	Output: 2
-	Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+// 解题思路:
+// 	最优解 O(n^2)
 
-Example 2:
-
-	Input: nums = [0,0,0], target = 1
-	Output: 0
-
-解题思路:
-	最优解 O(n^2)
- */
-
-import (
-	"fmt"
-	"math"
-	"sort"
-)
+import "fmt"
+import "sort"
 
 // 解法一 O(n^2)
 func threeSumClosest(nums []int, target int) int {
-	n, res, diff := len(nums), 0, math.MaxInt32
-	if n > 2 {
-		sort.Ints(nums)
-		for i := 0; i < n-2; i++ {
-			if i > 0 && nums[i] == nums[i-1] {
-				continue
-			}
-			for j, k := i+1, n-1; j < k; {
-				sum := nums[i] + nums[j] + nums[k]
-				if abs(sum-target) < diff {
-					res, diff = sum, abs(sum-target)
-				}
-				if sum == target {
-					return res
-				} else if sum > target {
-					k--
-				} else {
-					j++
-				}
-			}
-		}
-	}
-	return res
+    n, res, diff := len(nums), 0, 1 << 31
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
+    if n > 2 {
+        sort.Ints(nums)
+        for i := 0; i < n-2; i++ {
+            if i > 0 && nums[i] == nums[i-1] {
+                continue
+            }
+            for j, k := i+1, n-1; j < k; {
+                sum := nums[i] + nums[j] + nums[k]
+                if abs(sum-target) < diff {
+                    res, diff = sum, abs(sum-target)
+                }
+                if sum == target {
+                    return res
+                } else if sum > target {
+                    k--
+                } else {
+                    j++
+                }
+            }
+        }
+    }
+    return res
 }
 
 // 解法二 暴力解法 O(n^3)
 func threeSumClosest1(nums []int, target int) int {
-	res, difference := 0, math.MaxInt16
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			for k := j + 1; k < len(nums); k++ {
-				if abs(nums[i]+nums[j]+nums[k]-target) < difference {
-					difference = abs(nums[i] + nums[j] + nums[k] - target)
-					res = nums[i] + nums[j] + nums[k]
-				}
-			}
-		}
-	}
-	return res
+    res, diff := 0, 1 << 31
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            for k := j + 1; k < len(nums); k++ {
+                if abs(nums[i]+nums[j]+nums[k]-target) < diff {
+                    diff = abs(nums[i] + nums[j] + nums[k] - target)
+                    res = nums[i] + nums[j] + nums[k]
+                }
+            }
+        }
+    }
+    return res
 }
 
-func abs(a int) int {
-	if a > 0 {
-		return a
-	}
-	return -a
-}
-
-// best solution
-func threeSumClosestBest(nums []int, target int) int {
-	sort.Ints(nums)
-	var ret int
-	diff := 1 << 32
-	for i := range nums {
-		curr := nums[i]
-		j := i+1
-		k := len(nums) - 1
-		for k > j {
-			sum := curr + nums[k] + nums[j]
-			if abs(target-sum) < diff {
-				ret = sum
-				diff = abs(target-sum)
-			} else if sum == target {
-				return sum
-			} else if sum > target {
-				k--
-			} else if sum < target {
-				j++
-			}
-		}
-	}
-	return ret
+func threeSumClosest2(nums []int, target int) int {
+    sort.Ints(nums)
+    res, diff := 0, 1 << 31
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
+    for i := range nums {
+        curr, j,  k := nums[i],i + 1, len(nums) - 1
+        for k > j {
+            sum := curr + nums[k] + nums[j]
+            if abs(target - sum) < diff {
+                res = sum
+                diff = abs(target - sum)
+            } else if sum == target {
+                return sum
+            } else if sum > target {
+                k--
+            } else if sum < target {
+                j++
+            }
+        }
+    }
+    return res
 }
 
 func main() {
-	fmt.Printf("threeSumClosest([]int{-1, 2, 1, -4},1) = %v\n",threeSumClosest([]int{-1, 2, 1, -4},1))
-	fmt.Printf("threeSumClosest1([]int{-1, 2, 1, -4},1) = %v\n",threeSumClosest([]int{-1, 2, 1, -4},1))
-	fmt.Printf("threeSumClosestBest([]int{-1, 2, 1, -4},1) = %v\n",threeSumClosestBest([]int{-1, 2, 1, -4},1))
+    // Example 1:
+    // Input: nums = [-1,2,1,-4], target = 1
+    // Output: 2
+    // Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+    fmt.Println(threeSumClosest([]int{-1, 2, 1, -4},1)) // 2
+    // Example 2:
+    // Input: nums = [0,0,0], target = 1
+    // Output: 0
+    fmt.Println(threeSumClosest([]int{0,0,0},1)) // 0
+
+    fmt.Println(threeSumClosest([]int{1,2,3,4,5,6,7,8,9},2)) // 6
+    fmt.Println(threeSumClosest([]int{9,8,7,6,5,4,3,2,1},2)) // 6
+
+    fmt.Println(threeSumClosest1([]int{-1, 2, 1, -4},1)) // 2
+    fmt.Println(threeSumClosest1([]int{0,0,0},1)) // 0
+    fmt.Println(threeSumClosest1([]int{1,2,3,4,5,6,7,8,9},2)) // 6
+    fmt.Println(threeSumClosest1([]int{9,8,7,6,5,4,3,2,1},2)) // 6
+
+    fmt.Println(threeSumClosest2([]int{-1, 2, 1, -4},1)) // 2
+    fmt.Println(threeSumClosest2([]int{0,0,0},1)) // 0
+    fmt.Println(threeSumClosest2([]int{1,2,3,4,5,6,7,8,9},2)) // 6
+    fmt.Println(threeSumClosest2([]int{9,8,7,6,5,4,3,2,1},2)) // 6
 }
