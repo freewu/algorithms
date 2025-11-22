4566cf2ce6c7673ef4579f2165d4a8caa49acad7
bluefrog <bluefrog.wu@gmail.com>
2025-11-22 15:52:28 +0800

    leetcode 3190. Find Minimum Operations to Make All Elements Divisible by Three

--- leetcode/3190-FindMinimumOperationsToMakeAllElementsDivisibleByThree.go
+++ leetcode/3190-FindMinimumOperationsToMakeAllElementsDivisibleByThree.go
@@ -47,4 +47,7 @@ func main() {
     // Input: nums = [3,6,9]
     // Output: 0
     fmt.Println(minimumOperations([]int{3,6,9})) // 0
+
+    fmt.Println(minimumOperations([]int{1,2,3,4,5,6,7,8,9})) // 6
+    fmt.Println(minimumOperations([]int{9,8,7,6,5,4,3,2,1})) // 6
 }
