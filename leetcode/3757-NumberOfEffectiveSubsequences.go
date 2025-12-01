d213277249fdaea104fb6c468a517fbf865c2cce
bluefrog <bluefrog.wu@gmail.com>
2025-12-01 08:38:14 +0800

    leetcode 3757. Number of Effective Subsequences

--- leetcode/3757-NumberOfEffectiveSubsequences.go
+++ leetcode/3757-NumberOfEffectiveSubsequences.go
@@ -68,40 +68,47 @@ const MX = 100_001
 var pow2 = [MX]int{1}
 
 func init() {
-    // 预处理 2 的幂
     for i := 1; i < MX; i++ {
         pow2[i] = pow2[i-1] * 2 % MOD
     }
 }
 
 func countEffective(nums []int) int {
-    or := 0
+    or, same := 0, true
     for _, x := range nums {
         or |= x
+        if x != nums[0] {
+            same = false
+        }
     }
-    mx := bits.Len(uint(or))
-    f := make([]int, 1<<mx)
+    if same {
+        return 1
+    }
+    w := bits.Len(uint(or))
+    f := make([]int, 1<<w)
     for _, x := range nums {
         f[x]++
     }
-    for i := range mx {
-        for s := 0; s < 1<<mx; s++ {
+    for i := range w {
+        if or>>i&1 == 0 {
+            continue
+        }
+        for s := 0; s < 1<<w; s++ {
             s |= 1 << i
             f[s] += f[s^1<<i]
         }
     }
-    // 计算完毕后，f[s] 表示 nums 中的是 s 的子集的元素个数
-    res := pow2[len(nums)] // 所有子序列的个数
-    // 枚举 or 的所有子集（包括空集）
+    res := pow2[len(nums)]
     for sub, ok := or, true; ok; ok = sub != or {
-        sign := 1 - bits.OnesCount(uint(or^sub)) % 2 * 2
+        sign := 1 - bits.OnesCount(uint(or^sub))%2*2
         res -= sign * pow2[f[sub]]
         sub = (sub - 1) & or
     }
-    return (res % MOD + MOD) % MOD // 保证结果非负    
+    return (res % MOD + MOD) % MOD
 }
 
 func countEffective1(nums []int) int {
+    const MOD = 1_000_000_007
     curr, n := 0, len(nums)
     for _, v := range nums {
         curr |= v
