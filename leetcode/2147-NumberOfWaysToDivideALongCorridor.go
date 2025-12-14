e2e186f1aadf213169dd960c32438d9ee119df90
bluefrog <bluefrog.wu@gmail.com>
2025-12-14 13:21:47 +0800

    leetcode 2147. Number of Ways to Divide a Long Corridor

--- leetcode/2147-NumberOfWaysToDivideALongCorridor.go
+++ leetcode/2147-NumberOfWaysToDivideALongCorridor.go
@@ -58,6 +58,23 @@ func numberOfWays(corridor string) int {
     return res
 }
 
+func numberOfWays1(corridor string) int {
+    res, count, last := 1, 0, 0
+    for i, ch := range corridor {
+        if ch == 'S' {
+            count++
+            if count >= 3 && count % 2 > 0 { // 对于第 3,5,7,... 个座位，可以在其到其左侧最近座位之间的任意空隙放置屏风
+                res = res * (i - last) % 1_000_000_007
+            }
+            last = i // 记录上一个座位的位置
+        }
+    }
+    if count == 0 || count%2 > 0 { // 座位个数不能为 0 或奇数
+        return 0
+    }
+    return res
+}
+
 func main() {
     // Example 1:
     // <img src="https://assets.leetcode.com/uploads/2021/12/04/1.png" />
@@ -80,4 +97,21 @@ func main() {
     // Output: 0
     // Explanation: There is no way to divide the corridor because there will always be a section that does not have exactly two seats.
     fmt.Println(numberOfWays("S")) // 0
+
+    fmt.Println(numberOfWays("SSSSSSSSSS")) // 0
+    fmt.Println(numberOfWays("PPPPPPPPPP")) // 0
+    fmt.Println(numberOfWays("SSSSSPPPPP")) // 0
+    fmt.Println(numberOfWays("PPPPPSSSSS")) // 0
+    fmt.Println(numberOfWays("SPSPSPSPSP")) // 0
+    fmt.Println(numberOfWays("PSPSPSPSPS")) // 0
+
+    fmt.Println(numberOfWays1("SSPPSPS")) // 3
+    fmt.Println(numberOfWays1("PPSPSP")) // 1
+    fmt.Println(numberOfWays1("S")) // 0
+    fmt.Println(numberOfWays1("SSSSSSSSSS")) // 0
+    fmt.Println(numberOfWays1("PPPPPPPPPP")) // 0
+    fmt.Println(numberOfWays1("SSSSSPPPPP")) // 0
+    fmt.Println(numberOfWays1("PPPPPSSSSS")) // 0
+    fmt.Println(numberOfWays1("SPSPSPSPSP")) // 0
+    fmt.Println(numberOfWays1("PSPSPSPSPS")) // 0
 }
