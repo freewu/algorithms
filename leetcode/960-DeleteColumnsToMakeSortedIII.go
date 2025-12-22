43766e8a4febd77e4d5592ea9d4c4cb64fba6128
bluefrog <bluefrog.wu@gmail.com>
2025-12-22 09:16:53 +0800

    leetcode 960. Delete Columns to Make Sorted III

--- leetcode/960-DeleteColumnsToMakeSortedIII.go
+++ leetcode/960-DeleteColumnsToMakeSortedIII.go
@@ -37,36 +37,32 @@ package main
 import "fmt"
 
 func minDeletionSize(strs []string) int {
-    deletionsToColumn := make([]int, len(strs[0]) + 1) // deletionsToColumn is 1 place longer, to accomodate for the very important End node
-    // this loop simulates going from our placeholder start node to every other node (the cost is just the column index)
-    // also there will always be a path from S to any other node, it translates to where we choose to start
-    for i := 0; i < len(deletionsToColumn); i++ {
-        deletionsToColumn[i] = i
+    n := len(strs[0])
+    dp := make([]int, n)
+    for i := range dp {
+        dp[i] = 1
     }
-    // hasRoute checks that all the chars in the column (c) are in order with the chars in the next column (nc)
-    hasRoute := func(strs []string, c, nc int) bool {
-        if c == -1 || nc == len(strs[0]) { return true } // if nc is out of bounds, that means it's the end node, to which all nodes are free to reach (at a cost!)
-        for si := 0; si < len(strs); si++ {
-            if strs[si][c] > strs[si][nc] {
-                return false
+    for i := n - 2; i >= 0; i-- {
+        for j := i + 1; j < n; j++ {
+            valid := true
+            for _, row := range strs {
+                if row[i] > row[j] {
+                    valid = false
+                    break
+                }
             }
-        }    
-        return true
-    }
-    // I guess there was no real need for me to split to another function, just a remainder of my previous DFS attempt
-    minDeletionsRoute := func (strs []string, deletionsToColumn []int) int {
-        for column := 0; column < len(strs[0]); column++ {
-            for nextColumn := column + 1; nextColumn < len(deletionsToColumn); nextColumn++ {
-                // it's cheaper (computationally) to check if the cost of the edge is cheaper than the previous best way to arrive to it, so that condition comes before the call to hasRoute
-                if deletionsToColumn[nextColumn] >= deletionsToColumn[column] + nextColumn - column - 1 && 
-                   hasRoute(strs, column, nextColumn) {
-                    deletionsToColumn[nextColumn] = deletionsToColumn[column] + nextColumn - column - 1
+            if valid {
+                if dp[i] < 1 + dp[j] {
+                    dp[i] = 1 + dp[j]
                 }
             }
         }
-        return deletionsToColumn[len(deletionsToColumn)-1]
     }
-    return minDeletionsRoute(strs, deletionsToColumn)
+    mx := 0
+    for _, v := range dp {
+        mx = max(mx, v)
+    }
+    return n - mx
 }
 
 func main() {
@@ -87,4 +83,7 @@ func main() {
     // Output: 0
     // Explanation: All rows are already lexicographically sorted.
     fmt.Println(minDeletionSize([]string{"ghi","def","abc"})) // 0
+
+    fmt.Println(minDeletionSize([]string{"bluefrog","leetcode"})) // 6
+    fmt.Println(minDeletionSize([]string{"leetcode","bluefrog"})) // 6
 }
