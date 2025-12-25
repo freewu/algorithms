64d9aa456d92f8632339d77a5c76fbb5b8e02d4a
bluefrog <bluefrog.wu@gmail.com>
2025-12-25 08:37:54 +0800

    leetcode 3074. Apple Redistribution into Boxes

--- leetcode/3074-AppleRedistributionIntoBoxes.go
+++ leetcode/3074-AppleRedistributionIntoBoxes.go
@@ -25,51 +25,64 @@ package main
 //     The input is generated such that it's possible to redistribute packs of apples into boxes.
 
 import "fmt"
-import "slices"
 import "sort"
 
 func minimumBoxes(apple []int, capacity []int) int {
-    s := 0
-    // 累加总和
-	for _, x := range apple {
-		s += x
-	}
-    // 从大到小排序
-	slices.SortFunc(capacity, func(a, b int) int { return b - a })
-    //fmt.Println(capacity)
-	for i, c := range capacity {
-		s -= c
-		if s <= 0 { // 所有苹果都装入了箱子
-			return i + 1 // 0 到 i 有 i + 1 个箱子
-		}
-	}
-	return -1
+    sum := 0
+    for _, v := range apple { // 累加总和
+        sum += v
+    }
+    // 容量由大到小排序
+    sort.Slice(capacity, func(i, j int) bool { return capacity[i] > capacity[j] })
+    for i, c := range capacity {
+        sum -= c
+        if sum <= 0 { // 所有苹果都装入了箱子
+            return i + 1 // 0 到 i 有 i + 1 个箱子
+        }
+    }
+    return -1
 }
 
 func minimumBoxes1(apple []int, capacity []int) int {
-    // 累加苹果数量
-    var apples int
-    for i := range apple {
+    apples := 0
+    for i := range apple { // 累加苹果数量
         apples += apple[i]
     }
-    // 由大到小排序
+    // 箱子容量由大到小排序
     sort.Slice(capacity, func(i, j int) bool { return capacity[i] > capacity[j] })
-    var sum, count int
+    sum, res := 0, 0
     for i := range capacity {
         sum += capacity[i]
-        count++ // 累加箱子个数
-        // 装满了
-        if sum >= apples {
+        res++ // 累加箱子个数
+        if sum >= apples { // 装满了
             break
         }
     }
-    return count
-  }
+    return res
+}
 
 func main() {
+    // Example 1:
+    // Input: apple = [1,3,2], capacity = [4,3,1,5,2]
+    // Output: 2
+    // Explanation: We will use boxes with capacities 4 and 5.
+    // It is possible to distribute the apples as the total capacity is greater than or equal to the total number of apples.
     fmt.Println(minimumBoxes([]int{1,3,2},[]int{4,3,1,5,2})) // 2
+    // Example 2:
+    // Input: apple = [5,5,5], capacity = [2,4,2,7]
+    // Output: 4
+    // Explanation: We will need to use all the boxes.
     fmt.Println(minimumBoxes([]int{5,5,5},[]int{2,4,2,7})) // 4
+
+    fmt.Println(minimumBoxes([]int{1,2,3,4,5,6,7,8,9},[]int{1,2,3,4,5,6,7,8,9})) // 9
+    fmt.Println(minimumBoxes([]int{1,2,3,4,5,6,7,8,9},[]int{9,8,7,6,5,4,3,2,1})) // 9
+    fmt.Println(minimumBoxes([]int{9,8,7,6,5,4,3,2,1},[]int{1,2,3,4,5,6,7,8,9})) // 9
+    fmt.Println(minimumBoxes([]int{9,8,7,6,5,4,3,2,1},[]int{9,8,7,6,5,4,3,2,1})) // 9
 
     fmt.Println(minimumBoxes1([]int{1,3,2},[]int{4,3,1,5,2})) // 2
     fmt.Println(minimumBoxes1([]int{5,5,5},[]int{2,4,2,7})) // 4
+    fmt.Println(minimumBoxes1([]int{1,2,3,4,5,6,7,8,9},[]int{1,2,3,4,5,6,7,8,9})) // 9
+    fmt.Println(minimumBoxes1([]int{1,2,3,4,5,6,7,8,9},[]int{9,8,7,6,5,4,3,2,1})) // 9
+    fmt.Println(minimumBoxes1([]int{9,8,7,6,5,4,3,2,1},[]int{1,2,3,4,5,6,7,8,9})) // 9
+    fmt.Println(minimumBoxes1([]int{9,8,7,6,5,4,3,2,1},[]int{9,8,7,6,5,4,3,2,1})) // 9
 }
