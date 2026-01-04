cf6df713aa0bc1c195a7f0745dc4d6899aa5c8ee
bluefrog <bluefrog.wu@gmail.com>
2026-01-05 00:46:51 +0800

    leetcode 3796. Find Maximum Value in a Constrained Sequence

--- leetcode/3796-FindMaximumValueInAConstrainedSequence.go
+++ leetcode/3796-FindMaximumValueInAConstrainedSequence.go
@@ -58,6 +58,25 @@ func findMaxVal(n int, restrictions [][]int, diff []int) int {
     return slices.Max(arr)
 }
 
+func findMaxVal1(n int, restrictions [][]int, diff []int) int {
+    arr := make([]int, n)
+    for _, r := range restrictions {
+        arr[r[0]] = r[1]
+    }
+    for i := 1; i < n; i++ {
+        t := arr[i - 1] + diff[i-1]
+        if arr[i] > 0 {
+            arr[i] = min(t, arr[i])
+        } else {
+            arr[i] = t
+        }
+    }
+    for i := n - 2; i >= 0; i-- {
+        arr[i] = min(arr[i], arr[i + 1] + diff[i])
+    }
+    return slices.Max(arr)
+}
+
 func main() {
     // Example 1:
     // Input: n = 10, restrictions = [[3,1],[8,1]], diff = [2,2,3,1,4,5,1,1,2]
@@ -73,4 +92,12 @@ func main() {
     // The sequence a = [0, 3, 3, 2, 6, 8, 11, 12] satisfies the given constraints (a[3] <= 2).
     // The maximum value in the sequence is 12. 
     fmt.Println(findMaxVal(8, [][]int{{3,2}}, []int{3,5,2,4,2,3,1})) // 12
+
+    fmt.Println(findMaxVal(10, [][]int{{3,2}}, []int{1,2,3,4,5,6,7,8,9})) // 41
+    fmt.Println(findMaxVal(10, [][]int{{3,2}}, []int{9,8,7,6,5,4,3,2,1})) // 23
+
+    fmt.Println(findMaxVal1(10, [][]int{{3,1},{8,1}}, []int{2,2,3,1,4,5,1,1,2})) // 6
+    fmt.Println(findMaxVal1(8, [][]int{{3,2}}, []int{3,5,2,4,2,3,1})) // 12
+    fmt.Println(findMaxVal1(10, [][]int{{3,2}}, []int{1,2,3,4,5,6,7,8,9})) // 41
+    fmt.Println(findMaxVal1(10, [][]int{{3,2}}, []int{9,8,7,6,5,4,3,2,1})) // 23
 }
