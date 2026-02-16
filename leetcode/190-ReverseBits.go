577993288b3e6759d28f02214e1c801e1afb0063
bluefrog <bluefrog.wu@gmail.com>
2026-02-16 09:50:54 +0800

    leetcode 190. Reverse Bits

--- leetcode/190-ReverseBits.go
+++ leetcode/190-ReverseBits.go
@@ -30,8 +30,8 @@ package main
 import "fmt"
 import "strconv"
 
-func reverseBits(num uint32) uint32 {
-    var res uint32
+func reverseBits(num int) int {
+    res := 0
     for i := 0; i < 32; i++ {
         // 把 num 往右移动，不断的消灭右边最低位的 1，将这个 1 给 res，
         // res 不断的左移即可实现反转二进制位的目的
@@ -41,7 +41,7 @@ func reverseBits(num uint32) uint32 {
     return res
 }
 
-func reverseBits1(num uint32) uint32 {
+func reverseBits1(num int) int {
     const (
         m1 = 0x55555555 // 01010101010101010101010101010101
         m2 = 0x33333333 // 00110011001100110011001100110011
@@ -64,7 +64,7 @@ func main() {
     decimal, _ := strconv.ParseInt("00000010100101000001111010011100", 2, 64)
     fmt.Printf("%v\n",decimal)
     // 00000010100101000001111010011100
-    fmt.Println(reverseBits(uint32(43261596))) // 964176192 (00111001011110000010100101000000)
+    fmt.Println(reverseBits(int(43261596))) // 964176192 (00111001011110000010100101000000)
     // Example 2:
     // Input: n = 11111111111111111111111111111101
     // Output:   3221225471 (10111111111111111111111111111111)
@@ -73,8 +73,15 @@ func main() {
     decimal, _ = strconv.ParseInt("11111111111111111111111111111101", 2, 64)
     fmt.Printf("%v\n",decimal)
     // 11111111111111111111111111111101
-    fmt.Println(reverseBits(uint32(4294967293))) // 3221225471 (10111111111111111111111111111111)
+    fmt.Println(reverseBits(int(4294967293))) // 3221225471 (10111111111111111111111111111111)
 
-    fmt.Println(reverseBits1(uint32(43261596))) // 964176192 (00111001011110000010100101000000)
-    fmt.Println(reverseBits1(uint32(4294967293))) // 3221225471 (10111111111111111111111111111111)
+    fmt.Println(reverseBits(int(1))) // 3221225471 (10111111111111111111111111111111)
+    fmt.Println(reverseBits(int(1024))) // 2097152
+    fmt.Println(reverseBits(int(1 << 31))) // 1
+
+    fmt.Println(reverseBits1(int(43261596))) // 964176192 (00111001011110000010100101000000)
+    fmt.Println(reverseBits1(int(4294967293))) // 3221225471 (10111111111111111111111111111111)
+    fmt.Println(reverseBits1(int(1))) // 2147483648
+    fmt.Println(reverseBits1(int(1024))) // 2097152
+    fmt.Println(reverseBits1(int(1 << 31))) // 4294967297
 }
