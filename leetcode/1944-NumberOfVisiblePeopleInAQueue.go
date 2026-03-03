b355f7da848cf7214edb7187e429d2cdcbc0fea5
bluefrog <bluefrog.wu@gmail.com>
2026-03-03 09:42:50 +0800

    leetcode 1944. Number of Visible People in a Queue

--- leetcode/1944-NumberOfVisiblePeopleInAQueue.go
+++ leetcode/1944-NumberOfVisiblePeopleInAQueue.go
@@ -68,4 +68,7 @@ func main() {
     // Input: heights = [5,1,2,3,10]
     // Output: [4,1,1,1,0]
     fmt.Println(canSeePersonsCount([]int{5,1,2,3,10})) // [4,1,1,1,0]
+
+    fmt.Println(canSeePersonsCount([]int{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 0]
+    fmt.Println(canSeePersonsCount([]int{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 0]
 }
