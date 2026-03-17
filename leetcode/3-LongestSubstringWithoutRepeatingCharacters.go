7ee0800ce8bbf0aaf8e60e815fcfa88560a52d95
bluefrog <bluefrog.wu@gmail.com>
2026-03-18 00:03:30 +0800

    leetcode 3. Longest Substring Without Repeating Characters

--- leetcode/3-LongestSubstringWithoutRepeatingCharacters.go
+++ leetcode/3-LongestSubstringWithoutRepeatingCharacters.go
@@ -143,13 +143,23 @@ func lengthOfLongestSubstring4(s string) int {
 }
 
 func main() {
+    // Example 1:
+    // Input: s = "abcabcbb"
+    // Output: 3
     // Explanation: The answer is "abc", with the length of 3.
     fmt.Println(lengthOfLongestSubstring("abcabcbb")) // 3
+    // Example 2:
+    // Input: s = "bbbbb"
+    // Output: 1
     // Explanation: The answer is "b", with the length of 1.
     fmt.Println(lengthOfLongestSubstring("bbbbb")) // 1
+    // Example 3:
+    // Input: s = "pwwkew"
+    // Output: 3
     // Explanation: The answer is "wke", with the length of 3.
     // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
     fmt.Println(lengthOfLongestSubstring("pwwkew")) // 3
+
 
     fmt.Println(lengthOfLongestSubstring1("abcabcbb")) // 3
     fmt.Println(lengthOfLongestSubstring1("bbbbb")) // 1
