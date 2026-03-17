45142a9ae4505749c1c7f52b56da63121bccbd33
bluefrog <bluefrog.wu@gmail.com>
2026-03-17 09:14:57 +0800

    leetcode 42. Trapping Rain Water

--- leetcode/42-TrappingRainWater.go
+++ leetcode/42-TrappingRainWater.go
@@ -114,11 +114,18 @@ func trap3(height []int) int {
     return res
 }
 
-
 func main() {
-    // The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
-    // In this case, 6 units of rain water (blue section) are being trapped.
+    // Example 1:
+    // <img src="https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png" />
+    // Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    // Output: 6
+    // Explanation: 
+    //     The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
+    //     In this case, 6 units of rain water (blue section) are being trapped.
     fmt.Println(trap([]int{0,1,0,2,1,0,1,3,2,1,2,1})) // 6
+    // Example 2:
+    // Input: height = [4,2,0,3,2,5]
+    // Output: 9
     fmt.Println(trap([]int{4,2,0,3,2,5})) // 9
 
     fmt.Println(trap1([]int{0,1,0,2,1,0,1,3,2,1,2,1})) // 6
