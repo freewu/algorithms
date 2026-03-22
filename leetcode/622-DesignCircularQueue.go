0181adf47e88748796ae151237cca4d2fdf4f5d6
bluefrog <bluefrog.wu@gmail.com>
2026-03-22 10:58:19 +0800

    leetcode 622. Design Circular Queue

--- leetcode/622-DesignCircularQueue.go
+++ leetcode/622-DesignCircularQueue.go
@@ -47,59 +47,59 @@ package main
 import "fmt"
 
 type MyCircularQueue struct {
-	data    []int // 数据存放
-	front   int
+    data    []int // 数据存放
+    front   int
     rear    int
     size    int // 初始化队列长度
 }
 
 func Constructor(k int) MyCircularQueue {
-	return MyCircularQueue{
-		size:  k,
-		data: make([]int, k),
-		front: 0,
-		rear:  -1,
-	}
+    return MyCircularQueue{
+        size:  k,
+        data: make([]int, k),
+        front: 0,
+        rear:  -1,
+    }
 }
 
 func (q *MyCircularQueue) EnQueue(value int) bool {
     // 队列满了,不能再加入队列了
-	if q.IsFull() {
-		return false
-	}
-	q.rear++
-	q.data[q.rear % q.size] = value
-	return true
+    if q.IsFull() {
+        return false
+    }
+    q.rear++
+    q.data[q.rear % q.size] = value
+    return true
 }
 
 func (q *MyCircularQueue) DeQueue() bool {
-	if q.IsEmpty() {
-		return false
-	}
-	q.front++
-	return true
+    if q.IsEmpty() {
+        return false
+    }
+    q.front++
+    return true
 }
 
 func (q *MyCircularQueue) Front() int {
-	if q.IsEmpty() {
-		return -1
-	}
-	return q.data[q.front % q.size]
+    if q.IsEmpty() {
+        return -1
+    }
+    return q.data[q.front % q.size]
 }
 
 func (q *MyCircularQueue) Rear() int {
-	if q.IsEmpty() {
-		return -1
-	}
-	return q.data[q.rear % q.size]
+    if q.IsEmpty() {
+        return -1
+    }
+    return q.data[q.rear % q.size]
 }
 
 func (q *MyCircularQueue) IsEmpty() bool {
-	return q.rear < q.front
+    return q.rear < q.front
 }
 
 func (q *MyCircularQueue) IsFull() bool {
-	return q.rear - q.front == q.size - 1
+    return q.rear - q.front == q.size - 1
 }
 
 func main() {
