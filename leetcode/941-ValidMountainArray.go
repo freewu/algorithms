c5585a73dcc0a474dd706681f981ec414c5efcd9
bluefrog <bluefrog.wu@gmail.com>
2026-03-22 20:16:27 +0800

    leetcode 941. Valid Mountain Array

--- leetcode/941-ValidMountainArray.go
+++ leetcode/941-ValidMountainArray.go
@@ -92,7 +92,12 @@ func main() {
     // Output: true
     fmt.Println(validMountainArray([]int{0,3,2,1})) // true
 
+    fmt.Println(validMountainArray([]int{1,2,3,4,5,6,7,8,9})) // false
+    fmt.Println(validMountainArray([]int{9,8,7,6,5,4,3,2,1})) // false
+
     fmt.Println(validMountainArray1([]int{2,1})) // false
     fmt.Println(validMountainArray1([]int{3,5,5})) // false
     fmt.Println(validMountainArray1([]int{0,3,2,1})) // true
+    fmt.Println(validMountainArray1([]int{1,2,3,4,5,6,7,8,9})) // false
+    fmt.Println(validMountainArray1([]int{9,8,7,6,5,4,3,2,1})) // false
 }
