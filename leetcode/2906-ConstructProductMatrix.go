ee355d01a38dac4deca992642edbd7857d48d36d
bluefrog <bluefrog.wu@gmail.com>
2026-03-24 08:35:32 +0800

    leetcode 2906. Construct Product Matrix

--- leetcode/2906-ConstructProductMatrix.go
+++ leetcode/2906-ConstructProductMatrix.go
@@ -35,25 +35,45 @@ package main
 import "fmt"
 
 func constructProductMatrix(grid [][]int) [][]int {
-    n, m := len(grid), len(grid[0])
+    n, m, mod := len(grid), len(grid[0]), 12345
     // Generate product from left top element to right bottom element
-    res, prevProd := make([][]int, n), 1
+    res, prod := make([][]int, n), 1
     for i := range res {
         res[i] = make([]int, m)
         for j := range res[i] {
-            res[i][j] = prevProd
-            prevProd = (prevProd * grid[i][j]) % 12345
+            res[i][j] = prod
+            prod = (prod * grid[i][j]) % mod
         }
     }
     // Generate product from right bottom element to left top element
-    prevProd = 1
+    prod = 1
     for i := n - 1 ; i >= 0 ; i-- {
         for j := m - 1 ; j >= 0 ; j-- {
-            res[i][j] = (res[i][j] * prevProd) % 12345
-            prevProd = (prevProd * grid[i][j]) % 12345
+            res[i][j] = (res[i][j] * prod) % mod
+            prod = (prod * grid[i][j]) % mod
         }
     }
     return res
+}
+
+func constructProductMatrix1(grid [][]int) [][]int {
+    prefix, suffix, shift, mod := 1, 1, 32, 12345
+    mask := 1 << shift - 1
+    for i, row := range grid {
+        for j := range row {
+            val := grid[i][j]
+            grid[i][j] = (prefix << shift) | (val % mod)
+            prefix = (prefix * (val % mod)) % mod
+        }
+    }
+    for i := len(grid) - 1; i >= 0; i-- {
+        for j := len(grid[i]) - 1; j >= 0; j-- {
+            prefix, val := grid[i][j] >> shift, grid[i][j] & mask
+            grid[i][j] = (prefix * suffix) % mod
+            suffix = (suffix * val) % mod
+        }
+    }
+    return grid
 }
 
 func main() {
@@ -74,4 +94,16 @@ func main() {
     // p[0][2] = grid[0][0] * grid[0][1] = 12345 * 2 = 24690. 24690 % 12345 = 0. So p[0][2] = 0.
     // So the answer is [[2],[0],[0]].
     fmt.Println(constructProductMatrix([][]int{{12345},{2},{1}})) // [[2],[0],[0]]
+
+    fmt.Println(constructProductMatrix([][]int{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}})) // [[1500 750 8730 375 300 4365 5505 6360 2910] [1500 750 8730 375 300 4365 5505 6360 2910]]
+    fmt.Println(constructProductMatrix([][]int{{1,2,3,4,5,6,7,8,9},{9,8,7,6,5,4,3,2,1}})) // [[1500 750 8730 375 300 4365 5505 6360 2910] [2910 6360 5505 4365 300 375 8730 750 1500]]
+    fmt.Println(constructProductMatrix([][]int{{9,8,7,6,5,4,3,2,1},{1,2,3,4,5,6,7,8,9}})) // [[2910 6360 5505 4365 300 375 8730 750 1500] [1500 750 8730 375 300 4365 5505 6360 2910]]
+    fmt.Println(constructProductMatrix([][]int{{9,8,7,6,5,4,3,2,1},{9,8,7,6,5,4,3,2,1}})) // [[2910 6360 5505 4365 300 375 8730 750 1500] [2910 6360 5505 4365 300 375 8730 750 1500]]
+
+    fmt.Println(constructProductMatrix1([][]int{{1,2},{3,4}})) // [[24,12],[8,6]]
+    fmt.Println(constructProductMatrix1([][]int{{12345},{2},{1}})) // [[2],[0],[0]]
+    fmt.Println(constructProductMatrix1([][]int{{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}})) // [[1500 750 8730 375 300 4365 5505 6360 2910] [1500 750 8730 375 300 4365 5505 6360 2910]]
+    fmt.Println(constructProductMatrix1([][]int{{1,2,3,4,5,6,7,8,9},{9,8,7,6,5,4,3,2,1}})) // [[1500 750 8730 375 300 4365 5505 6360 2910] [2910 6360 5505 4365 300 375 8730 750 1500]]
+    fmt.Println(constructProductMatrix1([][]int{{9,8,7,6,5,4,3,2,1},{1,2,3,4,5,6,7,8,9}})) // [[2910 6360 5505 4365 300 375 8730 750 1500] [1500 750 8730 375 300 4365 5505 6360 2910]]
+    fmt.Println(constructProductMatrix1([][]int{{9,8,7,6,5,4,3,2,1},{9,8,7,6,5,4,3,2,1}})) // [[2910 6360 5505 4365 300 375 8730 750 1500] [2910 6360 5
 }
