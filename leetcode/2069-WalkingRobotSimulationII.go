28bd103fccdd78d4047b5a7fb799995dfd3cd754
bluefrog <bluefrog.wu@gmail.com>
2026-04-07 08:47:38 +0800

    leetcode 2069. Walking Robot Simulation II

--- leetcode/2069-WalkingRobotSimulationII.go
+++ leetcode/2069-WalkingRobotSimulationII.go
@@ -52,17 +52,17 @@ package main
 
 import "fmt"
 
-type Robot struct {
+type Robot1 struct {
     Height, Width, Equation int
     Position []int
     Direction string
 }
 
-func Constructor(width int, height int) Robot {
-    return Robot{ height, width, width * 2 + height * 2 - 4, []int{0, 0}, "East" }
+func Constructor1(width int, height int) Robot1 {
+    return Robot1{ height, width, width * 2 + height * 2 - 4, []int{0, 0}, "East" }
 }
 
-func (this *Robot) Step(num int) {
+func (this *Robot1) Step(num int) {
     num %= this.Equation
     if num == 0 { num = this.Equation }
     for i := 0; i < num; i++ {
@@ -85,12 +85,82 @@ func (this *Robot) Step(num int) {
     }
 }
 
-func (this *Robot) GetPos() []int {
+func (this *Robot1) GetPos() []int {
     return this.Position
 }
 
-func (this *Robot) GetDir() string {
+func (this *Robot1) GetDir() string {   
     return this.Direction
+}
+
+
+type Robot struct {
+    x int
+    y int
+    dir int
+    width int
+    height int
+    step int
+}
+
+func Constructor(width int, height int) Robot {
+    return Robot{
+        x: 0,
+        y: 0,
+        dir: 0,
+        width: width,
+        height: height,
+    }
+}
+func (this *Robot) Step(num int) {
+    this.step += num
+}
+
+func (this *Robot) Solve()  {
+    dirs := [][]int{ {1,0},{0,1},{-1,0},{0,-1} }
+    num := this.step
+    this.step = 0
+    num = (num - 1) % ((this.width + this.height - 2) * 2) + 1
+    for num > 0 {
+        curx := this.x + dirs[this.dir][0] * num
+        cury := this.y + dirs[this.dir][1] * num  
+        if curx >= this.width {
+            num -= this.width - 1 - this.x
+            this.x = this.width - 1
+            this.dir = (this.dir+1) % 4
+        } else if curx < 0 {
+            num -= this.x
+            this.x = 0
+            this.dir = (this.dir + 1) % 4
+        } else if cury >= this.height {
+            num -= this.height - 1 - this.y
+            this.y=this.height-1
+            this.dir = (this.dir + 1) % 4
+        } else if cury < 0 {
+            num -= this.y
+            this.y = 0
+            this.dir = (this.dir + 1) % 4 
+        } else {
+            num = 0
+            this.x = curx
+            this.y = cury
+        }
+    }
+}
+
+func (this *Robot) GetPos() []int {
+    if this.step > 0 {
+        this.Solve()
+    }
+    return []int{ this.x, this.y }
+}
+
+func (this *Robot) GetDir() string {
+    if this.step > 0 {
+        this.Solve()
+    }
+    mp := []string{ "East", "North", "West", "South" }
+    return mp[this.dir]
 }
 
 /**
@@ -131,4 +201,35 @@ func main() {
     fmt.Println(obj.GetPos()) // [1, 2]
     // robot.getDir(); // return "West"
     fmt.Println(obj.GetDir()) // West
+
+
+    // Robot robot = new Robot(6, 3); // Initialize the grid and the robot at (0, 0) facing East.
+    obj11 := Constructor1(6,3)
+    fmt.Println(obj11)
+    // robot.step(2);  // It moves two steps East to (2, 0), and faces East.
+    obj11.Step(2) 
+    fmt.Println(obj11) // (2, 0, East)
+    // robot.step(2);  // It moves two steps East to (4, 0), and faces East.
+    obj11.Step(2) 
+    fmt.Println(obj11) // (4, 0, East)
+    // robot.getPos(); // return [4, 0]
+    fmt.Println(obj11.GetPos) // [4, 0]
+    // robot.getDir(); // return "East"
+    fmt.Println(obj11.GetDir) // "East"
+    // robot.step(2);  // It moves one step East to (5, 0), and faces East.
+    //                 // Moving the next step East would be out of bounds, so it turns and faces North.
+    //                 // Then, it moves one step North to (5, 1), and faces North.
+    obj11.Step(2) 
+    fmt.Println(obj11) // (5, 1, North)
+    // robot.step(1);  // It moves one step North to (5, 2), and faces North (not West).
+    obj11.Step(1) 
+    fmt.Println(obj11) // (5, 2, North)
+    // robot.step(4);  // Moving the next step North would be out of bounds, so it turns and faces West.
+    //                 // Then, it moves four steps West to (1, 2), and faces West.
+    obj11.Step(4) 
+    fmt.Println(obj11) // (1, 2, West)
+    // robot.getPos(); // return [1, 2]
+    fmt.Println(obj11.GetPos()) // [1, 2]
+    // robot.getDir(); // return "West"
+    fmt.Println(obj11.GetDir()) // West
 }
