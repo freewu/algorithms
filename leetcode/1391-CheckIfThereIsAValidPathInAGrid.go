b59fc4b283632d7f739046d95b487cb7f48fb89e
bluefrog <bluefrog.wu@gmail.com>
2026-04-27 08:39:42 +0800

    leetcode 1391. Check if There is a Valid Path in a Grid

--- leetcode/1391-CheckIfThereIsAValidPathInAGrid.go
+++ leetcode/1391-CheckIfThereIsAValidPathInAGrid.go
@@ -44,9 +44,8 @@ package main
 
 import "fmt"
 
-
 func hasValidPath(grid [][]int) bool {
-    type cell struct {
+    type Cell struct {
         x int
         y int
     }
@@ -58,13 +57,13 @@ func hasValidPath(grid [][]int) bool {
         5: [][]int{{0,-1}, {-1,0}},
         6: [][]int{{0,1}, {-1,0}},
     }
-    accept := map[cell]map[int]bool{
-        cell{x:0, y:1}: map[int]bool{1:true,3:true, 5:true},
-        cell{x:1, y:0}: map[int]bool{2:true,5:true, 6:true},
-        cell{x:0, y:-1}: map[int]bool{4:true,1:true, 6:true},
-        cell{x:-1, y:0}: map[int]bool{2:true,4:true, 3:true},
+    accept := map[Cell]map[int]bool{
+        Cell{x:0, y:1}: map[int]bool{1:true,3:true, 5:true},
+        Cell{x:1, y:0}: map[int]bool{2:true,5:true, 6:true},
+        Cell{x:0, y:-1}: map[int]bool{4:true,1:true, 6:true},
+        Cell{x:-1, y:0}: map[int]bool{2:true,4:true, 3:true},
     }
-    res, visited := false, make(map[cell]bool)
+    res, visited := false, make(map[Cell]bool)
     var dfs func(i, j int)
     dfs = func(i, j int){
         if i == len(grid)-1 && j == len(grid[0])-1 {
@@ -75,17 +74,54 @@ func hasValidPath(grid [][]int) bool {
             for _, d := range ds {
                 nx, ny :=i + d[0], j + d[1]
                 if nx >= 0 && ny >= 0 && nx < len(grid) && ny <len(grid[0]) {
-                    if !visited[cell{x:nx, y:ny}] && accept[cell{x:d[0],y:d[1]}][grid[nx][ny]] {
-                        visited[cell{x:nx, y:ny}] = true
+                    if !visited[Cell{x:nx, y:ny}] && accept[Cell{x:d[0],y:d[1]}][grid[nx][ny]] {
+                        visited[Cell{x:nx, y:ny}] = true
                         dfs(nx,ny)
                     }
                 }
             }
         }
     }
-    visited[cell{x:0,y:0}] = true
+    visited[Cell{x:0,y:0}] = true
     dfs(0,0)
     return res
+}
+
+type UnionFind struct {
+    fa    []int
+    rank  []int
+    count int
+}
+
+func newUnionFind(n int) *UnionFind {
+    u := &UnionFind{
+        fa:    make([]int, n),
+        rank:  make([]int, n),
+        count: n,
+    }
+    for i := 0; i < n; i++ {
+        u.fa[i] = i
+    }
+    return u
+}
+
+func (u *UnionFind) find(x int) int {
+    if u.fa[x] == x { return x }
+    u.fa[x] = u.find(u.fa[x])
+    return u.fa[x]
+}
+
+func (u *UnionFind) union(x, y int) {
+    if x == y { return }
+    if u.rank[x] <= u.rank[y] {
+        u.fa[x] = y
+    } else {
+        u.fa[y] = x
+    }
+    if u.rank[x] == u.rank[y] {
+        u.rank[x]++
+    }
+    u.count--
 }
 
 // union find
@@ -157,42 +193,95 @@ func hasValidPath1(grid [][]int) bool {
     return u.find(0) == u.find(k-1)
 }
 
-
-type UnionFind struct {
-    fa    []int
-    rank  []int
-    count int
+func hasValidPath2(grid [][]int) bool {
+    m, n := len(grid), len(grid[0])
+    if m == 1 && n == 1 {
+        return true
+    }
+    visited := make([][]bool, m)
+    for i := range visited {
+        visited[i] = make([]bool, n)
+    }
+    var q [][]int
+    q = append(q, []int{0, 0})
+    visited[0][0] = true
+    add := make([][]int, 7)
+    add[1] = []int{0, -1, 0, 1}
+    add[2] = []int{-1, 0, 1, 0}
+    add[3] = []int{0, -1, 1, 0}
+    add[4] = []int{1, 0, 0, 1}
+    add[5] = []int{-1, 0, 0, -1}
+    add[6] = []int{-1, 0, 0, 1}
+    var valid func(i, j int) bool
+    valid = func(i, j int) bool {
+        return i >= 0 && i < m && j >= 0 && j < n
+    }
+    for len(q) > 0 {
+        i, j := q[0][0], q[0][1]
+        q = q[1:]
+        now := grid[i][j]
+        for idx := 0; idx < len(add[now]); idx += 2 {
+            ii, jj := i+add[now][idx], j+add[now][idx+1]
+            if valid(ii, jj) && !visited[ii][jj] {
+                nxt := grid[ii][jj]
+                ok := false
+                for idx2 := 0; idx2 < len(add[nxt]); idx2 += 2 {
+                    preI, preJ := ii+add[nxt][idx2], jj+add[nxt][idx2+1]
+                    if preI == i && preJ == j {
+                        ok = true
+                    }
+                }
+                // 真正意义上的合法
+                if ok {
+                    if ii == m-1 && jj == n-1 {
+                        return true
+                    }
+                    visited[ii][jj] = true
+                    q = append(q, []int{ii, jj})
+                }
+            }
+        }
+    }
+    return false
 }
 
-func newUnionFind(n int) *UnionFind {
-    u := &UnionFind{
-        fa:    make([]int, n),
-        rank:  make([]int, n),
-        count: n,
+func hasValidPath3(grid [][]int) bool {
+    startdirs := [6]uint8{1, 2, 2, 2, 0, 1}
+    nextpos := [5][2]int{ {500, 500}, {0, 1}, {1, 0}, {0, -1}, {-1, 0},  }
+    states := [5][7]uint8{
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 2, 0, 4, 0},
+        {0, 0, 2, 0, 0, 3, 1},
+        {0, 3, 0, 0, 2, 0, 4},
+        {0, 0, 4, 3, 1, 0, 0},
     }
-    for i := 0; i < n; i++ {
-        u.fa[i] = i
+    move := func(dir uint8, i, j, street int) (uint8, int, int) {
+        ndir := states[dir][street]
+        ni := i + nextpos[ndir][0]
+        nj := j + nextpos[ndir][1]
+        return ndir, ni, nj
     }
-    return u
-}
-
-func (u *UnionFind) find(x int) int {
-    if u.fa[x] == x { return x }
-    u.fa[x] = u.find(u.fa[x])
-    return u.fa[x]
-}
-
-func (u *UnionFind) union(x, y int) {
-    if x == y { return }
-    if u.rank[x] <= u.rank[y] {
-        u.fa[x] = y
-    } else {
-        u.fa[y] = x
+    m, n := len(grid), len(grid[0])
+    extra := grid[0][0] == 4
+    target := grid[m-1][n-1]
+    inBounds := func(i, j int) bool {
+        return i >= 0 && i < m && j >= 0 && j < n
     }
-    if u.rank[x] == u.rank[y] {
-        u.rank[x]++
+    inTarget := func(i, j int) bool {
+        return i == m-1 && j == n-1
     }
-    u.count--
+    simulate := func(dir uint8, i, j int) bool {
+        grid[m-1][n-1] = target
+        for inBounds(i, j) && !inTarget(i, j) {
+            street := grid[i][j]
+            grid[i][j] = 0 
+            dir, i, j = move(dir, i, j, street)
+        }
+        return inTarget(i, j) && (extra || states[dir][grid[i][j]] != 0)
+    }
+    dir := startdirs[grid[0][0]-1]
+    i, j := nextpos[dir][0], nextpos[dir][1]
+    return m * n == 1 || simulate(dir, i, j) || extra && simulate(1, 0, 0)
 }
 
 func main() {
@@ -222,4 +311,16 @@ func main() {
     fmt.Println(hasValidPath1([][]int{{1,1,2}})) // false
     fmt.Println(hasValidPath1([][]int{{1,1,3}})) // true
     fmt.Println(hasValidPath1([][]int{{1,1,1}})) // true
+
+    fmt.Println(hasValidPath2([][]int{{2,4,3},{6,5,2}})) // true
+    fmt.Println(hasValidPath2([][]int{{1,2,1},{1,2,1}})) // false
+    fmt.Println(hasValidPath2([][]int{{1,1,2}})) // false
+    fmt.Println(hasValidPath2([][]int{{1,1,3}})) // true
+    fmt.Println(hasValidPath2([][]int{{1,1,1}})) // true
+
+    fmt.Println(hasValidPath3([][]int{{2,4,3},{6,5,2}})) // true
+    fmt.Println(hasValidPath3([][]int{{1,2,1},{1,2,1}})) // false
+    fmt.Println(hasValidPath3([][]int{{1,1,2}})) // false
+    fmt.Println(hasValidPath3([][]int{{1,1,3}})) // true
+    fmt.Println(hasValidPath3([][]int{{1,1,1}})) // true
 }
