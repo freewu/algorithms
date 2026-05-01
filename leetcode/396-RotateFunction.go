ad172d5146d07b840d11f1beacba1683518c82ba
bluefrog <bluefrog.wu@gmail.com>
2026-05-01 09:18:12 +0800

    leetcode 396. Rotate Function

--- leetcode/396-RotateFunction.go
+++ leetcode/396-RotateFunction.go
@@ -62,4 +62,7 @@ func main() {
     // Input: nums = [100]
     // Output: 0
     fmt.Println(maxRotateFunction([]int{100})) // 0
+
+    fmt.Println(maxRotateFunction([]int{1,2,3,4,5,6,7,8,9})) // 240
+    fmt.Println(maxRotateFunction([]int{9,8,7,6,5,4,3,2,1})) // 210
 }
