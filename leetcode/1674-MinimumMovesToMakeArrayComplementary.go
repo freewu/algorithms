966abbf5382896a388ca9c1e401ab2c203f10e3b
bluefrog <bluefrog.wu@gmail.com>
2026-05-13 08:27:54 +0800

    leetcode 1674. Minimum Moves to Make Array Complementary

--- leetcode/1674-MinimumMovesToMakeArrayComplementary.go
+++ leetcode/1674-MinimumMovesToMakeArrayComplementary.go
@@ -46,7 +46,7 @@ func minMoves(nums []int, limit int) int {
     // line sweep , moves needed goes from 2->1->0->1->2
     // here the starting value is 0, so minimum value will go to -n in case of 2 moves for all n/2 pairs
     // so initial sum value need to be kept equal to n
-    for i := 0; i < n/2; i++ {
+    for i := 0; i < n / 2; i++ {
         a, b := nums[i], nums[n - i - 1]
         arr[min(a, b) + 1]--
         arr[a + b]--
@@ -59,6 +59,29 @@ func minMoves(nums []int, limit int) int {
     for i := 2; i <= limit * 2; i++{
         moves += arr[i]
         res = min(res, moves)
+    }
+    return res
+}
+
+func minMoves1(nums []int, limit int) int {
+    n := len(nums)
+    delta := make([]int, limit * 2 + 2) // 差分数组，大小为 limit*2 + 2（防止越界）
+    min := func (x, y int) int { if x < y { return x; }; return y; }
+    max := func (x, y int) int { if x > y { return x; }; return y; }
+    for i := 0; i < n/2; i++ {
+        a, b := nums[i], nums[n-1-i]
+        low, high, sum := 1 + min(a, b), limit + max(a, b), a + b   // 最小可能互补和, 最大可能互补和, 当前互补和
+        // 差分数组更新（核心逻辑）
+        delta[low]--
+        delta[sum]--
+        delta[sum+1]++
+        delta[high+1]++
+    }
+    res, now := n, n    // 最小操作次数, 当前操作次数
+    // 遍历所有可能的互补和 [2, 2*limit]
+    for i := 2; i <= limit * 2; i++ {
+        now += delta[i]
+        res = min(res, now)
     }
     return res
 }
@@ -84,4 +107,13 @@ func main() {
     // Output: 0
     // Explanation: nums is already complementary.
     fmt.Println(minMoves([]int{1,2,1,2}, 2)) // 0
+
+    fmt.Println(minMoves([]int{1,2,3,4,5,6,7,8,9}, 9)) // 1
+    fmt.Println(minMoves([]int{9,8,7,6,5,4,3,2,1}, 9)) // 1
+
+    fmt.Println(minMoves1([]int{1,2,4,3}, 4)) // 1
+    fmt.Println(minMoves1([]int{1,2,2,1}, 2)) // 2
+    fmt.Println(minMoves1([]int{1,2,1,2}, 2)) // 0
+    fmt.Println(minMoves1([]int{1,2,3,4,5,6,7,8,9}, 9)) // 1
+    fmt.Println(minMoves1([]int{9,8,7,6,5,4,3,2,1}, 9)) // 1
 }
