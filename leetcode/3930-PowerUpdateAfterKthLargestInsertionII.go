9b8029dac27a449943bfe7dd68b03c2e53be56ca
bluefrog <bluefrog.wu@gmail.com>
2026-05-18 11:31:09 +0800

    leetcode 3930. Power Update After K-th Largest Insertion II

--- leetcode/3930-PowerUpdateAfterKthLargestInsertionII.go
+++ leetcode/3930-PowerUpdateAfterKthLargestInsertionII.go
@@ -39,7 +39,10 @@ package main
 //     1 <= ki <= n + i + 1​​​​​​​
 
 import "fmt"
+import "sort"
+import "slices"
 
+// 超出时间限制 913 / 924 个通过的测试用例
 func powerUpdate(nums []int, p int, queries [][]int) []int {
     const MOD = 1_000_000_007
     // Pair { 数值, 唯一索引 }
@@ -97,6 +100,81 @@ func powerUpdate(nums []int, p int, queries [][]int) []int {
     return res
 }
 
+type BinaryIndexedTree []int
+
+func (t BinaryIndexedTree) Add(index, inc int) {
+    n := len(t)
+    for i := max(1, index); i < n; i += i & -i {
+        t[i] += inc
+    }
+}
+
+func (t BinaryIndexedTree) Pre(index int) int {
+    s := 0
+    for i := min(len(t) - 1, index); i > 0; i -= i & -i {
+        s += t[i]
+    }
+    return s
+}
+
+func (t BinaryIndexedTree) Sum(l, r int) int {
+    if l > r {
+        return 0
+    }
+    return t.Pre(r) - t.Pre(l - 1)
+}
+
+func powerUpdate1(nums []int, p int, queries [][]int) []int {
+    const MOD = 1_000_000_007
+    pow := func (x, y int) int {
+        p := 1
+        for y > 0 {
+            if y & 1 == 1 {
+                p = p * x % MOD
+            }
+            x = x * x % MOD
+            y >>= 1
+        }
+        return p
+    }
+    disperse := func(nums []int) (map[int]int, []int) {
+        i2v := slices.Clone(nums)
+        slices.Sort(i2v)
+        slices.Compact(i2v)
+        v2i := make(map[int]int)
+        i := 0
+        for _, num := range i2v {
+            i++
+            v2i[num] = i
+        }
+        return v2i, i2v
+    }
+    arr := slices.Clone(nums)
+    for _, q := range queries {
+        arr = append(arr, q[0])
+    }
+    v2i, i2v := disperse(arr)
+    t := make(BinaryIndexedTree, len(v2i) + 1)
+    for _, num := range nums {
+        t.Add(v2i[num], 1)
+    }
+    res := make([]int, 0, len(queries))
+    total := len(nums)
+    for _, q := range queries {
+        val, k := q[0], q[1]
+        t.Add(v2i[val], 1)
+        total++
+        right := total - k + 1
+        i := sort.Search(len(v2i), func (a int) bool {
+            a++
+            return t.Pre(a) >= right
+        })
+        p = pow(p, i2v[i])
+        res = append(res, p)
+    }
+    return res
+}
+
 func main() {
     // Example 1:
     // Input: nums = [2], p = 4, queries = [[3,1],[1,2]]
@@ -119,4 +197,10 @@ func main() {
 
     fmt.Println(powerUpdate([]int{1,2,3,4,5,6,7,8,9}, 6, [][]int{{4,3},{7,2}})) // [279936 592081930]
     fmt.Println(powerUpdate([]int{9,8,7,6,5,4,3,2,1}, 6, [][]int{{4,3},{7,2}})) // [279936 592081930]
+
+    fmt.Println(powerUpdate1([]int{2}, 4, [][]int{{3,1},{1,2}})) // [64,4096]
+    fmt.Println(powerUpdate1([]int{7,5}, 6, [][]int{{4,3},{7,2}})) // [1296,220296870]
+    fmt.Println(powerUpdate1([]int{1,2,3,4,5,6,7,8,9}, 6, [][]int{{4,3},{7,2}})) // [279936 592081930]
+    fmt.Println(powerUpdate1([]int{9,8,7,6,5,4,3,2,1}, 6, [][]int{{4,3},{7,2}})) // [279936 592081930]
 }
+
