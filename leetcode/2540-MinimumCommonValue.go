ab5a1d4ba9a857559c3d63db7f8af37d088f81b4
bluefrog <bluefrog.wu@gmail.com>
2026-05-19 08:32:59 +0800

    leetcode  2540. Minimum Common Value

--- leetcode/2540-MinimumCommonValue.go
+++ leetcode/2540-MinimumCommonValue.go
@@ -15,21 +15,19 @@ package main
 // Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
  
 // Constraints:
-//         1 <= nums1.length, nums2.length <= 10^5
-//         1 <= nums1[i], nums2[j] <= 10^9
-//         Both nums1 and nums2 are sorted in non-decreasing order.
+//     1 <= nums1.length, nums2.length <= 10^5
+//     1 <= nums1[i], nums2[j] <= 10^9
+//     Both nums1 and nums2 are sorted in non-decreasing order.
 
 import "fmt"
 
 func getCommon(nums1 []int, nums2 []int) int {
-    i, j,l1,l2 := 0, 0, len(nums1) - 1, len(nums2) - 1
+    i, j, l1, l2 := 0, 0, len(nums1) - 1, len(nums2) - 1
     for {
-        // 已经有一个结束了
-        if i > l1 || j > l2 {
+        if i > l1 || j > l2 { // 已经有一个结束了
             break
         }
-        // 小一边的要追上才能比对
-        if nums1[i] > nums2[j] { 
+        if nums1[i] > nums2[j] { // 小一边的要追上才能比对
             j++
         } else if nums1[i] < nums2[j]{
             i++
@@ -40,17 +38,43 @@ func getCommon(nums1 []int, nums2 []int) int {
     return -1
 }
 
+func getCommon1(nums1 []int, nums2 []int) int {
+    i, j := 0, 0
+    for i < len(nums1) && j < len(nums2) {
+        if nums1[i] == nums2[j] {
+            return nums1[i]
+        } else if nums1[i] < nums2[j] {
+            i++
+        } else {
+            j++
+        }
+    }
+    return -1
+}
+
 func main() {
-    fmt.Println(getCommon(
-        []int{1,2,3},
-        []int{2,4},
-    )) // 2
-    fmt.Println(getCommon(
-        []int{1,2,3,6},
-        []int{2,3,4,5},
-    )) // 2
-    fmt.Println(getCommon(
-        []int{1,3},
-        []int{3,4},
-    )) // 3
+    // Example 1:
+    // Input: nums1 = [1,2,3], nums2 = [2,4]
+    // Output: 2
+    // Explanation: The smallest element common to both arrays is 2, so we return 2.
+    fmt.Println(getCommon([]int{1,2,3}, []int{2,4})) // 2
+    // Example 2:
+    // Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+    // Output: 2
+    // Explanation: There are two common elements in the array 2 and 3 out of which 2 is the smallest, so 2 is returned.
+    fmt.Println(getCommon( []int{1,2,3,6},[]int{2,3,4,5} )) // 2
+
+    fmt.Println(getCommon( []int{1,3}, []int{3,4})) // 3
+    fmt.Println(getCommon( []int{1,2,3,4,5,6,7,8,9}, []int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(getCommon( []int{1,2,3,4,5,6,7,8,9}, []int{9,8,7,6,5,4,3,2,1})) // 9
+    fmt.Println(getCommon( []int{9,8,7,6,5,4,3,2,1}, []int{1,2,3,4,5,6,7,8,9})) // 9
+    fmt.Println(getCommon( []int{9,8,7,6,5,4,3,2,1}, []int{9,8,7,6,5,4,3,2,1})) // 9
+
+    fmt.Println(getCommon1([]int{1,2,3}, []int{2,4})) // 2
+    fmt.Println(getCommon1( []int{1,2,3,6},[]int{2,3,4,5} )) // 2
+    fmt.Println(getCommon1( []int{1,3}, []int{3,4})) // 3
+    fmt.Println(getCommon1( []int{1,2,3,4,5,6,7,8,9}, []int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(getCommon1( []int{1,2,3,4,5,6,7,8,9}, []int{9,8,7,6,5,4,3,2,1})) // 9
+    fmt.Println(getCommon1( []int{9,8,7,6,5,4,3,2,1}, []int{1,2,3,4,5,6,7,8,9})) // 9
+    fmt.Println(getCommon1( []int{9,8,7,6,5,4,3,2,1}, []int{9,8,7,6,5,4,3,2,1})) // 9
 }
