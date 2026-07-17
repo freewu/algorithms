1afc0a61c46d4acb3323bc3d0b19264162153a3e
bluefrog <bluefrog.wu@gmail.com>
2026-07-17 08:40:35 +0800

    leetcode 3312. Sorted GCD Pair Queries

--- leetcode/3312-SortedGCDPairQueries.go
+++ leetcode/3312-SortedGCDPairQueries.go
@@ -40,6 +40,7 @@ package main
 import "fmt"
 import "slices"
 import "sort"
+import "unsafe"
 
 func gcdValues(nums []int, queries []int64) []int {
     mx := slices.Max(nums)
@@ -66,6 +67,81 @@ func gcdValues(nums []int, queries []int64) []int {
     return res
 }
 
+func gcdValues1(nums []int, queries []int64) []int {
+    mx := nums[0]
+    for _, v := range nums {
+        if v > mx {
+            mx = v
+        }
+    }
+    count := make([]int64, mx + 1)
+    for _, v := range nums {
+        count[v]++
+    }
+    for i := 1; i <= mx; i++ {
+        for j := i * 2; j <= mx; j += i {
+            count[i] += count[j]
+        }
+    }
+    for i := 1; i <= mx; i++ {
+        count[i] = count[i] * (count[i] - 1) / 2
+    }
+    for i := mx; i >= 1; i-- {
+        for j := i * 2; j <= mx; j += i {
+            count[i] -= count[j]
+        }
+    }
+    for i := 1; i <= mx; i++ {
+        count[i] += count[i - 1]
+    }
+    res := make([]int, len(queries))
+    for k, q := range queries {
+        q++
+        left, right := 1, mx
+        for left < right {
+            mid := (left + right) / 2
+            if count[mid] >= q {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        res[k] = left
+    }
+    return res
+}
+
+var freq, gcd [50001]uint32
+
+func castSlice[T1, T2 any](arr []T1) []T2 {
+    return unsafe.Slice((*T2)(unsafe.Pointer(&arr[0])), len(arr))
+}
+
+func gcdValues2(nums []int, queries []int64) []int {
+    mx := slices.Max(nums)
+    for _, v := range nums {
+        freq[v]++
+    }
+    for i := mx; i > 0; i-- {
+        var count uint32
+        for j := i; j <= mx; j += i {
+            count += freq[j]
+            gcd[i] -= gcd[j]
+        }
+        gcd[i] += count * (count - 1) / 2
+    }
+    for i := 2; i <= mx; i++ {
+        gcd[i] += gcd[i-1]
+    }
+    res := castSlice[int64, int](queries)
+    for i, q := range queries {
+        res[i], _ = slices.BinarySearch(gcd[:mx+1], uint32(q)+1)
+    }
+    clear(freq[:mx + 1])
+    clear(gcd[:mx + 1])
+    return res
+}
+
 func main() {
     // Example 1:
     // Input: nums = [2,3,4], queries = [0,2,2]
@@ -87,4 +163,25 @@ func main() {
     // Explanation:
     // gcdPairs = [2].
     fmt.Println(gcdValues([]int{2,2}, []int64{0,0})) // [2,2]
+
+    fmt.Println(gcdValues([]int{1,2,3,4,5,6,7,8,9}, []int64{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues([]int{1,2,3,4,5,6,7,8,9}, []int64{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues([]int{9,8,7,6,5,4,3,2,1}, []int64{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues([]int{9,8,7,6,5,4,3,2,1}, []int64{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 1]
+
+    fmt.Println(gcdValues1([]int{2,3,4}, []int64{0,2,2})) // [1,2,2]
+    fmt.Println(gcdValues1([]int{4,4,2,1}, []int64{5,3,1,0})) // [4,2,1,1]
+    fmt.Println(gcdValues1([]int{2,2}, []int64{0,0})) // [2,2]
+    fmt.Println(gcdValues1([]int{1,2,3,4,5,6,7,8,9}, []int64{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues1([]int{1,2,3,4,5,6,7,8,9}, []int64{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues1([]int{9,8,7,6,5,4,3,2,1}, []int64{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues1([]int{9,8,7,6,5,4,3,2,1}, []int64{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 1]
+
+    fmt.Println(gcdValues2([]int{2,3,4}, []int64{0,2,2})) // [1,2,2]
+    fmt.Println(gcdValues2([]int{4,4,2,1}, []int64{5,3,1,0})) // [4,2,1,1]
+    fmt.Println(gcdValues2([]int{2,2}, []int64{0,0})) // [2,2]
+    fmt.Println(gcdValues2([]int{1,2,3,4,5,6,7,8,9}, []int64{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues2([]int{1,2,3,4,5,6,7,8,9}, []int64{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues2([]int{9,8,7,6,5,4,3,2,1}, []int64{1,2,3,4,5,6,7,8,9})) // [1 1 1 1 1 1 1 1 1]
+    fmt.Println(gcdValues2([]int{9,8,7,6,5,4,3,2,1}, []int64{9,8,7,6,5,4,3,2,1})) // [1 1 1 1 1 1 1 1 1]
 }
