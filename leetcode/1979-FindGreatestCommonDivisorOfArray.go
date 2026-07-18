14150e6fafcf3d845cbcf76958b51d9123ffc353
bluefrog <bluefrog.wu@gmail.com>
2026-07-18 12:21:55 +0800

    leetcode  1979. Find Greatest Common Divisor of Array

--- leetcode/1979-FindGreatestCommonDivisorOfArray.go
+++ leetcode/1979-FindGreatestCommonDivisorOfArray.go
@@ -145,38 +145,61 @@ func findGCD5(nums []int) int {
 }
 
 func main() {
+    // Example 1:
+    // Input: nums = [2,5,6,9,10]
+    // Output: 2
     // Explanation:
     // The smallest number in nums is 2.
     // The largest number in nums is 10.
     // The greatest common divisor of 2 and 10 is 2.
     fmt.Println(findGCD([]int{2,5,6,9,10})) // 2
+    // Example 2:
+    // Input: nums = [7,5,6,8,3]
+    // Output: 1
     // Explanation:
     // The smallest number in nums is 3.
     // The largest number in nums is 8.
     // The greatest common divisor of 3 and 8 is 1.
     fmt.Println(findGCD([]int{7,5,6,8,3})) // 1
+    // Example 3:
+    // Input: nums = [3,3]
+    // Output: 3
+    // Explanation:
     // The smallest number in nums is 3.
     // The largest number in nums is 3.
     // The greatest common divisor of 3 and 3 is 3.
     fmt.Println(findGCD([]int{3,3})) // 3
 
+    fmt.Println(findGCD([]int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(findGCD([]int{9,8,7,6,5,4,3,2,1})) // 1
+
     fmt.Println(findGCD1([]int{2,5,6,9,10})) // 2
     fmt.Println(findGCD1([]int{7,5,6,8,3})) // 1
     fmt.Println(findGCD1([]int{3,3})) // 3
+    fmt.Println(findGCD1([]int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(findGCD1([]int{9,8,7,6,5,4,3,2,1})) // 1
 
     fmt.Println(findGCD2([]int{2,5,6,9,10})) // 2
     fmt.Println(findGCD2([]int{7,5,6,8,3})) // 1
     fmt.Println(findGCD2([]int{3,3})) // 3
+    fmt.Println(findGCD2([]int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(findGCD2([]int{9,8,7,6,5,4,3,2,1})) // 1
 
     fmt.Println(findGCD3([]int{2,5,6,9,10})) // 2
     fmt.Println(findGCD3([]int{7,5,6,8,3})) // 1
     fmt.Println(findGCD3([]int{3,3})) // 3
+    fmt.Println(findGCD3([]int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(findGCD3([]int{9,8,7,6,5,4,3,2,1})) // 1
 
     fmt.Println(findGCD4([]int{2,5,6,9,10})) // 2
     fmt.Println(findGCD4([]int{7,5,6,8,3})) // 1
     fmt.Println(findGCD4([]int{3,3})) // 3
+    fmt.Println(findGCD4([]int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(findGCD4([]int{9,8,7,6,5,4,3,2,1})) // 1
 
     fmt.Println(findGCD5([]int{2,5,6,9,10})) // 2
     fmt.Println(findGCD5([]int{7,5,6,8,3})) // 1
     fmt.Println(findGCD5([]int{3,3})) // 3
+    fmt.Println(findGCD5([]int{1,2,3,4,5,6,7,8,9})) // 1
+    fmt.Println(findGCD5([]int{9,8,7,6,5,4,3,2,1})) // 1
 }
