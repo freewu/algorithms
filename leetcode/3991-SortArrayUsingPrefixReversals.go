4f369f513a27700737d4d4a676ab739cd9fc79d2
bluefrog <bluefrog.wu@gmail.com>
2026-07-20 04:20:32 +0800

    leetcode 3991. Sort Array Using Prefix Reversals

--- leetcode/3991-SortArrayUsingPrefixReversals.go
+++ leetcode/3991-SortArrayUsingPrefixReversals.go
@@ -109,6 +109,52 @@ func sortArray(nums []int, pre []int) int {
     return -1
 }
 
+func sortArray1(nums []int, pre []int) int {
+    n := len(nums)
+    target := 0
+    for i := 0; i < n; i++ {
+        target = target*8 + i
+    }
+    start := 0
+    for _, x := range nums {
+        start = start*8 + x
+    }
+    if start == target {
+        return 0
+    }
+    visited := map[int]bool{start: true}
+    type Pair struct {
+        state []int
+        dist  int
+    }
+    q := []Pair{{append([]int(nil), nums...), 0}}
+    for len(q) > 0 {
+        p := q[0]
+        q = q[1:]
+        state := p.state
+        dist := p.dist
+        nd := dist + 1
+        for _, x := range pre {
+            nxt := append([]int(nil), state...)
+            for l, r := 0, x-1; l < r; l, r = l+1, r-1 {
+                nxt[l], nxt[r] = nxt[r], nxt[l]
+            }
+            key := 0
+            for _, v := range nxt {
+                key = key*8 + v
+            }
+            if key == target {
+                return nd
+            }
+            if !visited[key] {
+                visited[key] = true
+                q = append(q, Pair{nxt, nd})
+            }
+        }
+    }
+    return -1
+}
+
 func main() {
     // Example 1:
     // Input: nums = [2,0,1], pre = [2,3]
@@ -135,4 +181,12 @@ func main() {
     fmt.Println(sortArray([]int{0,1,2,3,4,5,6,7}, []int{8,7,6,5,4,3,2,1})) // 0
     fmt.Println(sortArray([]int{7,6,5,4,3,2,1,0}, []int{1,2,3,4,5,6,7,8})) // 1
     fmt.Println(sortArray([]int{7,6,5,4,3,2,1,0}, []int{8,7,6,5,4,3,2,1})) // 1
+
+    fmt.Println(sortArray1([]int{2,0,1}, []int{2,3})) // 2
+    fmt.Println(sortArray1([]int{1,0,2}, []int{1,3})) // -1
+    fmt.Println(sortArray1([]int{0,1}, []int{2})) // 0
+    fmt.Println(sortArray1([]int{0,1,2,3,4,5,6,7}, []int{1,2,3,4,5,6,7,8})) // 0
+    fmt.Println(sortArray1([]int{0,1,2,3,4,5,6,7}, []int{8,7,6,5,4,3,2,1})) // 0
+    fmt.Println(sortArray1([]int{7,6,5,4,3,2,1,0}, []int{1,2,3,4,5,6,7,8})) // 1
+    fmt.Println(sortArray1([]int{7,6,5,4,3,2,1,0}, []int{8,7,6,5,4,3,2,1})) // 1
 }
