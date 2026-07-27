a6720645e4f55cb0ca8b7b828cdfcad78718eab0
bluefrog <bluefrog.wu@gmail.com>
2026-07-27 08:47:49 +0800

    leetcode 1464. Maximum Product of Two Elements in an Array

--- leetcode/1464-MaximumProductOfTwoElementsInAnArray.go
+++ leetcode/1464-MaximumProductOfTwoElementsInAnArray.go
@@ -51,4 +51,7 @@ func main() {
     // Input: nums = [3,7]
     // Output: 12
     fmt.Println(maxProduct([]int{3,7})) // 12
+
+    fmt.Println(maxProduct([]int{1,2,3,4,5,6,7,8,9})) // 56
+    fmt.Println(maxProduct([]int{9,8,7,6,5,4,3,2,1})) // 56
 }
