f5e1f60d291ac5298008335e29279fe6176c5b63
bluefrog <bluefrog.wu@gmail.com>
2026-08-02 10:04:33 +0800

    leetcode 877. Stone Game

--- leetcode/877-StoneGame.go
+++ leetcode/877-StoneGame.go
@@ -75,4 +75,7 @@ func main() {
     // Input: piles = [3,7,2,3]
     // Output: true
     fmt.Println(stoneGame([]int{3,7,2,3})) // true
+
+    fmt.Println(stoneGame([]int{1,2,3,4,5,6,7,8})) // true
+    fmt.Println(stoneGame([]int{8,7,6,5,4,3,2,1})) // true
 }
