bf0a8f32a849a305acbf6f5d4bbaa771649340cd
bluefrog <bluefrog.wu@gmail.com>
2026-08-19 02:27:19 +0800

    leetcode 4030. Elevator Requests IV

--- leetcode/4030-ElevatorRequestsIV.go
+++ leetcode/4030-ElevatorRequestsIV.go
@@ -47,11 +47,9 @@ package main
 //     0 <= start, floori <= n - 1
 
 import "fmt"
+import "slices"
 
-// func elevatorRequests(n int, start int, requests [][]int) int64 {
-    
-// }
-
+// 执行出错 511 / 550 oom
 func elevatorRequests(n int, start int, requests [][]int) int64 {
     m, inf := len(requests), int64(1 << 61)
     dp := make([][]int64, 1 << m)
@@ -92,6 +90,41 @@ func elevatorRequests(n int, start int, requests [][]int) int64 {
     return res
 }
 
+func elevatorRequests1(n int, start int, requests [][]int) int64 {
+    requests = append(requests, []int{0, -1}, []int{0, n}) // 插入两个哨兵
+    slices.SortFunc(requests, func(a, b []int) int { // 按楼层排序
+         return a[1] - b[1] 
+    }) 
+    m := len(requests) // 不含哨兵的下标范围是 [1, m-2]
+    f := make([][2]int, m)
+    for j := range f {
+        f[j] = [2]int{ 1 << 61, 1 << 61 }
+    }
+    abs := func(x int) int { if x < 0 { return -x; }; return x; }
+    for i := 1; i < m-1; i++ {
+        t, x := requests[i][0], requests[i][1]
+        for j := m - 2; j >= i; j-- {
+            t2, y := requests[j][0], requests[j][1]
+            if i == 1 && j == m-2 { // 当前请求是第一个请求
+                // 从 start 到当前楼层
+                f[j][0] = max(abs(x-start), t)
+                f[j][1] = max(abs(y-start), t2)
+                continue
+            }
+            f[j][1] = min(max(f[j][0]+y-requests[i-1][1], t2), // 从 floor[i-1] 到当前楼层
+                        max(f[j+1][1]+requests[j+1][1]-y, t2)) // 从 floor[j+1] 到当前楼层
+            f[j][0] = min(max(f[j][0]+x-requests[i-1][1], t), // 从 floor[i-1] 到当前楼层
+                        max(f[j+1][1]+requests[j+1][1]-x, t)) // 从 floor[j+1] 到当前楼层
+        }
+    }
+    // 枚举最后一个完成的请求
+    res := 1 << 61
+    for i := 1; i < m-1; i++ {
+        res = min(res, f[i][0])
+    }
+    return int64(res)
+}
+
 func main() {
     // Example 1:
     // Input: n = 9, start = 0, requests = [[0,8],[6,5]]
@@ -118,4 +151,12 @@ func main() {
     // Move from floor 1 to floor 3 (requests[2][1]) in 2 seconds, reaching at time 8. Its request arrived at requests[2][0] = 6, so floor 3 is fulfilled at time 8.
     // Thus, all requests are fulfilled by time 8.
     fmt.Println(elevatorRequests(7, 3, [][]int{{0,5},{0,1},{6,3}})) // 8
+
+    //fmt.Println(elevatorRequests(41, 32, [][]int{{11,3},{17,14},{10,5},{0,17},{2,25},{2,28},{16,12},{15,36},{13,22},{6,13},{6,2},{3,16},{12,33},{4,38},{17,9},{8,29},{18,26},{18,37},{7,32},{17,6},{13,10},{2,31},{0,40},{16,39},{9,23},{0,0},{10,34},{20,35},{4,30},{21,19},{10,4},{14,11},{4,18}})) // 55
+
+    fmt.Println(elevatorRequests1(9, 0, [][]int{{0,8},{6,5}})) // 9
+    fmt.Println(elevatorRequests1(8, 5, [][]int{{1,7},{7,3}})) // 7
+    fmt.Println(elevatorRequests1(7, 3, [][]int{{0,5},{0,1},{6,3}})) // 8
+    fmt.Println(elevatorRequests1(41, 32, [][]int{{11,3},{17,14},{10,5},{0,17},{2,25},{2,28},{16,12},{15,36},{13,22},{6,13},{6,2},{3,16},{12,33},{4,38},{17,9},{8,29},{18,26},{18,37},{7,32},{17,6},{13,10},{2,31},{0,40},{16,39},{9,23},{0,0},{10,34},{20,35},{4,30},{21,19},{10,4},{14,11},{4,18}})) // 55
+
 }
