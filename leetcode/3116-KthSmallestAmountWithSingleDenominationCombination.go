86c613d0a60d56f4fcb44a48b818dac45ee4f4e3
bluefrog <bluefrog.wu@gmail.com>
2026-08-21 10:40:24 +0800

    leetcode 3116. Kth Smallest Amount With Single Denomination Combination

--- leetcode/3116-KthSmallestAmountWithSingleDenominationCombination.go
+++ leetcode/3116-KthSmallestAmountWithSingleDenominationCombination.go
@@ -69,6 +69,62 @@ func findKthSmallest(coins []int, k int) int64 {
     return int64(res)
 }
 
+func findKthSmallest1(coins []int, k int) int64 {
+    sort.Ints(coins)
+    newCoins := make([]int, 0)
+    for _, v := range coins {
+        flag := true
+        for _, y := range newCoins {
+            if v % y == 0 {
+                flag = false
+                break
+            }
+        }
+        if flag {
+            newCoins = append(newCoins, v)
+        }
+    }
+    coins = newCoins
+    n := len(coins)
+    m, l, r := 1 << n, int64(k), int64(coins[0])*int64(k) + 1
+    lcm := make([]int64, m)
+    lcm[0] = 1
+    gcd := func (x, y int64) int64 { for y != 0 { x, y = y, x % y; }; return x; }
+    for mask := 1; mask < m; mask++ {
+        preMask := mask & (mask - 1)
+        i := bits.TrailingZeros(uint(mask))
+        tmp := lcm[preMask] / gcd(lcm[preMask], int64(coins[i]))
+        if tmp <= r/int64(coins[i]) {
+            lcm[mask] = tmp * int64(coins[i])
+        } else {
+            lcm[mask] = r + 1
+        }
+    }
+    count := func(x int64) int64 {
+        var res int64 = 0
+        for mask := 1; mask < m; mask++ {
+            if lcm[mask] > x {
+                continue
+            }
+            if bits.OnesCount(uint(mask))&1 == 1 {
+                res += x / lcm[mask]
+            } else {
+                res -= x / lcm[mask]
+            }
+        }
+        return res
+    }
+    for l < r {
+        x := l + (r-l)/2
+        if count(x) >= int64(k) {
+            r = x
+        } else {
+            l = x + 1
+        }
+    }
+    return l
+}
+
 func main() {
     // Example 1:
     // Input: coins = [3,6,9], k = 3
@@ -87,4 +143,12 @@ func main() {
     // Coin 2 produces multiples of 2: 2, 4, 6, 8, 10, 12, etc.
     // All of the coins combined produce: 2, 4, 5, 6, 8, 10, 12, 14, 15, etc.
     fmt.Println(findKthSmallest([]int{5,2}, 7)) // 12
+
+    fmt.Println(findKthSmallest([]int{1,2,3,4,5,6,7,8,9}, 3)) // 3
+    fmt.Println(findKthSmallest([]int{9,8,7,6,5,4,3,2,1}, 3)) // 3
+
+    fmt.Println(findKthSmallest1([]int{3,6,9}, 3)) // 9
+    fmt.Println(findKthSmallest1([]int{5,2}, 7)) // 12
+    fmt.Println(findKthSmallest1([]int{1,2,3,4,5,6,7,8,9}, 3)) // 3
+    fmt.Println(findKthSmallest1([]int{9,8,7,6,5,4,3,2,1}, 3)) // 3
 }
