76d73404299498eab12862b76f0bf744a4003cbf
bluefrog <bluefrog.wu@gmail.com>
2026-08-25 08:23:12 +0800

    leetcode 3718. Smallest Missing Multiple of K

--- leetcode/3718-SmallestMissingMultipleOfK.go
+++ leetcode/3718-SmallestMissingMultipleOfK.go
@@ -37,6 +37,21 @@ func missingMultiple(nums []int, k int) int {
     return 101
 }
 
+func missingMultiple1(nums []int, k int) int {
+    const MASK = 255
+    for i := range nums {
+        v := nums[i] & MASK
+        if v%k == 0 && v/k-1 < len(nums) {
+            nums[v/k-1] |= MASK + 1
+        }
+    }
+    i := 0
+    for i < len(nums) && nums[i] > MASK {
+        i++
+    }
+    return (i + 1) * k
+}
+
 func main() {
     // Example 1:
     // Input: nums = [8,2,3,4,6], k = 2
@@ -55,4 +70,11 @@ func main() {
     fmt.Println(missingMultiple([]int{9,8,7,6,5,4,3,2,1}, 2)) // 10
     fmt.Println(missingMultiple([]int{9,8,7,6,5,4,3,2,1}, 2)) // 10
     fmt.Println(missingMultiple([]int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100}, 1)) // 101
+
+    fmt.Println(missingMultiple1([]int{8,2,3,4,6}, 2)) // 10
+    fmt.Println(missingMultiple1([]int{1,4,7,10,15}, 5)) // 5
+    fmt.Println(missingMultiple1([]int{1,2,3,4,5,6,7,8,9}, 2)) // 10
+    fmt.Println(missingMultiple1([]int{9,8,7,6,5,4,3,2,1}, 2)) // 10
+    fmt.Println(missingMultiple1([]int{9,8,7,6,5,4,3,2,1}, 2)) // 10
+    fmt.Println(missingMultiple1([]int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100}, 1)) // 101
 }
